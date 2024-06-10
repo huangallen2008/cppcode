053db71aa83aa55da946c515b74d04c144ff4081
huangallen2008 <huangallen2008@gamil.con>
2024-06-10 05:27:57 +0000

    116210ing

--- code/AAhomework/Level2/DP/F/F.cpp
+++ code/AAhomework/Level2/DP/F/F.cpp
@@ -47,7 +47,7 @@ int rd(int l,int r) {
 signed main() {
     IOS();
     string s1="123",s2="12";
-    ope(min(s1,s2))
+    ope((char)('z'+1))
     int n,k;
     cin>>n>>k;
     vector<string> a(n+1);
@@ -56,7 +56,7 @@ signed main() {
     vector<vector<string>> dp(n+1,vector<string>(k+1));
     REP1(i,n) {
         dp[i][0]=dp[i-1][0];
-        REP1(j,k) dp[i][j]=min(a[i]+dp[i-1][j-1],dp[i-1][j]);
+        REP1(j,i) dp[i][j]=min(a[i]+dp[i-1][j-1],j<=i-1?dp[i-1][j]:"");
     }
     cout<<dp[n][k]<<'\n';
     return 0;
