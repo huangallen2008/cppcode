643423704b9d062b0e70059c4f2b5ec88823bb77
huangallen2008 <huangallen2008@gamil.con>
2024-06-19 12:49:17 +0000

    317119ing

--- code/Atcoder/ABC123D/ABC123D.cpp
+++ code/Atcoder/ABC123D/ABC123D.cpp
@@ -61,9 +61,9 @@ signed main() {
     REP(i,y) cin>>b[i];sort(ALL(b),so);
     REP(i,z) cin>>c[i];sort(ALL(c),so);
     priority_queue<E,vector<E>,so_E> pq;
-    pq.push({a[1]+b[0]+c[0],1,0});
-    if(y>1)pq.push({a[0]+b[1]+c[0],1,1});
-    if(z>1)pq.push({a[0]+b[0]+c[1],1,2});
+    pq.push({a[0]+b[0]+c[0],0,0});
+    // if(y>1)pq.push({a[0]+b[1]+c[0],1,1});
+    // if(z>1)pq.push({a[0]+b[0]+c[1],1,2});
     REP(rd,k) {
         auto [v,id,now]=pq.top();
         op(v)op(id)ope(now)
