93fae30c8dd78c9ef525f411e8f4c824f7eca97d
huangallen2008 <huangallen2008@gamil.con>
2024-06-19 12:41:48 +0000

    317119ing

--- code/Atcoder/ABC123D/ABC123D.cpp
+++ code/Atcoder/ABC123D/ABC123D.cpp
@@ -64,10 +64,10 @@ signed main() {
     pq.push({a[0]+b[0]+c[0],0,0});
     if(y>1)pq.push({a[0]+b[0]+c[0],0,1});
     if(z>1)pq.push({a[0]+b[0]+c[0],0,2});
-    REP(rd,k) {
+    REP(rd,k+2) {
         auto [v,id,now]=pq.top();
         op(v)op(id)ope(now)
-        cout<<v<<'\n';
+        if(rd>1)cout<<v<<'\n';
         pq.pop();
         // if(now<2)pq.push({v,0,now+1});
         if(now==0&&id<x-1)pq.push({v-a[id]+a[id+1],id+1,now});
