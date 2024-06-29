126dddbca62c38c5ad0dde02e4891f1f398dc0e6
huangallen2008 <huangallen2008@gamil.con>
2024-06-29 17:26:22 +0000

    718230ing

--- code/競賽/AA/AATOI2024/初II/E.cpp
+++ code/競賽/AA/AATOI2024/初II/E.cpp
@@ -68,11 +68,11 @@ signed main() {
         auto [sum,fix,cntr,cnta,ls]=*pq.begin();
         pq.erase(pq.begin());
         an+=sum;
-        if(fix+cntr<n) pq.insert({sum+a[fix+cntr+1]-a[fix+cntr],fix+cntr,1,fix+cntr});
+        if(fix+cntr<n) pq.insert({sum+a[fix+cntr+1]-a[fix+cntr],1,cnta,fix+cntr});
         for(;fix>ls;) {
             sum+=a[fix]-a[fix-1],cntr++,fix--;
             pq.insert({sum,fix,cntr,cnta,ls});
-            if(pq.size()+rd>k) pq.erase(prev(pq.end()));
+            // if(pq.size()+rd>k) pq.erase(prev(pq.end()));
         }
     }
     cout<<an<<'\n';
