0758eea163903fb9ad148e6b3ac59d8944ccbbdb
huangallen2008 <huangallen2008@gamil.con>
2024-06-29 09:29:38 +0000

    618129ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -25,7 +25,7 @@ using namespace std;
 #define chmax(x,y) x=max(x,y)
 #define chmin(x,y) x=min(x,y)
 #define addmod(x,y) x=((x+(y))%mod)
-#ifdef LOCAL
+#ifdef LOCAL_
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
 #define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
