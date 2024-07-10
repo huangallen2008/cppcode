29d40debd85f55920bd51855144f0b597d78d13d
huangallen2008 <huangallen2008@gamil.con>
2024-07-10 13:24:39 +0000

    319210ing

---
+++ code/AAhomework/競程養成level5/F/F.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=300+5;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+Graph g;
+vector<int> stk,v,p,l;
+void dfs(int u) {
+    while(stk.back()<=v[u]) stk.pop_back();
+    l[u]=stk.back();
+    stk.pb(v[u]);
+    for(int v:g[u]) {
+        dfs(v);
+    }
+}
+void solve() {
+    int n;
+    cin>>n;
+    p=vector<int>(n);
+    v=vector<int>(n);
+    l=vector<int>(n);
+    g=Graph(n);
+    REP(i,n) cin>>p[i];
+    REP(i,n) cin>>v[i];
+    REP(i,n) g[p[i]].pb(i);
+    REP(i,n) sort(ALL(g));
+    stk={n};
+    dfs(0);
+    int an=0;
+    REP(i,n) addmod(an,l[i]*(i+1));
+    cout<<an<<'\n';
+}
+signed main() {
+    IOS();
+    int T;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
