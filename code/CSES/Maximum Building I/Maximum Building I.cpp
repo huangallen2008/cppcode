42f8baa00780a3a2092c95b87af7f769c41bc741
huangallen2008 <huangallen2008@gamil.con>
2024-07-13 17:44:38 +0000

    719614ing

--- code/CSES/Maximum Building I/Maximum Building I.cpp
+++ code/CSES/Maximum Building I/Maximum Building I.cpp
@@ -61,7 +61,8 @@ signed main() {
     }
     ope("ok")
     int an=0;
-    for(auto v:a) {
+    REP1(rd,n) {
+        auto &v=a[rd];
         ope("ok0")
         vector<int> pre(m),nxt(m);
         vector<pii> stk;
