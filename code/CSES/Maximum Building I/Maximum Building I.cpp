53dd708a26b8f433c7062181f3706320ebc6d75d
huangallen2008 <huangallen2008@gamil.con>
2024-07-13 17:49:07 +0000

    719614ing

--- code/CSES/Maximum Building I/Maximum Building I.cpp
+++ code/CSES/Maximum Building I/Maximum Building I.cpp
@@ -72,13 +72,13 @@ signed main() {
         vector<int> pre(m),nxt(m);
         vector<pii> stk;
         stk.pb({0,0});
-        // REP1(i,m) {
-        //     while(stk.back().f>=v[i]) {
-        //         stk.pop_back();
-        //     }
-        //     pre[i]=stk.back().s;
-        //     stk.pb({v[i],i});
-        // }
+        REP1(i,m) {
+            while(stk.back().f>=v[i]) {
+                stk.pop_back();
+            }
+            pre[i]=stk.back().s;
+            stk.pb({v[i],i});
+        }
         // stk={{0,m+1}};
         // ope("ok1")
         // RREP1(i,m) {
