3642bc4f3ee8412799ffaab5236d601541be0fbe
huangallen2008 <huangallen2008@gamil.con>
2024-07-14 16:57:16 +0000

    119715ing

--- code/codeforces/edu/two_pointer/Step2/E/E.cpp
+++ code/codeforces/edu/two_pointer/Step2/E/E.cpp
@@ -50,7 +50,7 @@ signed main() {
     vector<int> a(n);
     REP(i,n) cin>>a[i];
     int r=0;
-    map<int,int> mp;
+    unordered_map<int,int> mp;
     int cnt=0,an=0;
     REP(l,n) {
         while(r<n&&cnt<=k) cnt+=(mp[a[r++]]++==0);
