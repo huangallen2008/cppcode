84c55f3e3923fde0b100abed74b45e0e9c649b5e
huangallen2008 <huangallen2008@gamil.con>
2024-07-14 05:33:42 +0000

    719614ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -1,73 +1,95 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
-#pragma GCC optimize("O3,unroll-loops,fast-math")
-#pragma GCC target("avx2,bmi,popcnt")
-// #define int long long
-#define REP(i,n) for(int i=0;i<(n);i++)
-#define REP1(i,n) for(int i=1;i<=(n);i++)
-#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
-#define RREP1(i,n) for(int i=(n);i>=1;i--)
-#define f first
-#define s second
-#define pb push_back
-#define ALL(x) (x).begin(),(x).end()
-#define SZ(x) (int)((x).size())
-#define SQ(x) (x)*(x)
-#define pii pair<int,int>
-#define pipii pair<int,pii>
-#define Graph vector<vector<int>>
-#define Graphw vector<vector<pii>>
-#define IOS() ios::sync_with_stdio(0),cin.tie(0)
-#define md(x) (((x)%(mod)+(mod))%(mod))
-#define MD(x,M) (((x)%(M)+(M))%(M))
-#define ld long double
-#define pdd pair<ld,ld>
-#define chmax(x,y) x=max(x,y)
-#define chmin(x,y) x=min(x,y)
-#define addmod(x,y) x=((x+(y))%mod)
-#ifdef LOCAL
-#define op(x) cout<<(#x)<<"="<<(x)<<", ";
-#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
-#define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
-#define entr cout<<endl;
-#else
-#define op(x) ;
-#define ope(x) ;
-#define oparr(x) ;
-#define entr ;
-#endif
-const int mod=1e9+7;
-const int maxn=2e3+5;
-const int inf=(1ll<<62);
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-int rd(int l,int r) {
-    return uniform_int_distribution<int>(l,r)(rng);
-}
+const int mx = 1e5+5;
+
+int n, m, k;
+int gr[mx];
+int c[mx];
+int g[2][mx];
+long long a[1<<23];
+long long ans;
+
 signed main() {
-    IOS();
-    int n,k;
-    cin>>n>>k;
-    vector<int> a(n+1),ps(n+1),p(n+1);p[0]=maxn;
-    REP1(i,n) {
-        cin>>a[i];
-        ps[i]=ps[i-1]+a[i];
-        if(a[i]&1) p[i]=p[i-1]+1;
-        else p[i]=p[i-1]-1;
-    }
-    int an=ps[n];
-    vector<int> mxid(maxn<<1|1,-1);
-    int lid=0;
-    mxid[p[0]]=0;
-    REP1(i,n) {
-        while(lid<=i&&ps[i]-ps[lid]>=ps[n]-k) {
-            mxid[p[lid]]=lid;
-            lid++;
+    cin >> n >> m >> k;
+    for (int i=1;i<=n;i++) cin >> gr[i];
+    int l = (k+1)/2, r = k-l;
+    for (int i=1;i<=m;i++) {
+        int u, v;
+        cin >> u >> v;
+        u = gr[u]; v = gr[v];
+        if (u != v) {
+            g[v>l][u] |= (1<<(v-l*(v>l)));
+            g[u>l][v] |= (1<<(u-l*(u>l)));
         }
-        int lr=mxid[p[i]-p[n]+maxn];
-        // op(i)op(lid)ope(lr)ope(ps[i]-ps[lr])
-        if(lr>=0&&ps[i]-ps[lr]>=ps[n]-k)chmin(an,ps[i]-ps[lr]);
-        // ope(an)
     }
-    cout<<ps[n]-an<<'\n';
-    return 0;
+    for (int i=1;i<=k;i++) cin >> c[i];
+    if (n==1) {
+        cout << c[1] << "\n";
+        exit(0);
+    }
+//    int l = k, r = k-l;
+
+    for (long long i=1;i<1LL<<l;i++) {
+        int num = 0;
+        for (int j=0;j<l;j++) {
+            if (i&(1LL<<j)) num |= (1<<(j+1));
+        }
+        bool fl = 1;
+        for (int j=1;j<=l;j++) {
+            if (!(num & (1<<j))) continue;
+            if (num & g[0][j]) {
+                fl = 0;
+                break;
+            }
+        }
+        if (!fl) continue;
+        else {
+            a[i] = 0;
+            for (int j=0;j<l;j++) {
+                if (!(i & (1<<j))) continue;
+                a[i] += c[j+1];
+            }
+            ans = max(a[i], ans);
+        }
+    }
+    vector<long long> dp(1<<l);
+    for(int i=0;i<1ll<<l;i++) dp[i]=a[i];
+    for(int j=0;j<l;j++) {
+        vector<long long> ndp(1<<l);
+        for(int i=0;i<1ll<<l;i++) {
+            if((i>>j)&1) {
+                ndp[i]=max(dp[i^(1ll<<j)],dp[i]);
+            }
+            else ndp[i]=dp[i];
+        }
+        swap(dp,ndp);
+    }
+    for (int i=1;i<1<<r;i++) {
+        int num = 0;
+        for (int j=0;j<r;j++) {
+            if (i&(1LL<<j)) num |= (1<<(j+1));
+        }
+        bool fl = 1;
+        int mp = (1<<l) - 1;
+        for (int j=1;j<=r;j++) {
+            if (!(num & (1<<j))) continue;
+            if (num & g[1][j+l]) {
+                fl = 0;
+                break;
+            }
+            mp &= ~(g[0][j+l]>>1);
+        }
+        if (!fl) continue;
+        else {
+            long long s = 0;
+            for (int j=0;j<r;j++) {
+                if (!(i & (1<<j))) continue;
+                s += c[j+1+l];
+            }
+//            cout << i << " " << mp << " " << s << "\n";
+            ans = max(s + dp[mp], ans);
+        }
+    }
+    cout << ans << "\n";
 }
