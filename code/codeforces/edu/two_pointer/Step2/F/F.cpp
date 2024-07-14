9b8477343eebd190d5e9b20910e9193d72a72dc1
huangallen2008 <huangallen2008@gamil.con>
2024-07-14 15:17:23 +0000

    719614ing

--- code/codeforces/edu/two_pointer/Step2/F/F.cpp
+++ code/codeforces/edu/two_pointer/Step2/F/F.cpp
@@ -58,8 +58,8 @@ struct my_stk {
         return x;
     }
     int size() { return stk.size(); }
-    int mx() { return mx.size()?mx.back():0; }
-    int mn() { return mn.size()?mn.back():inf; }
+    int mxe() { return mx.size()?mx.back():0; }
+    int mne() { return mn.size()?mn.back():inf; }
 };
 struct my_que {
     my_stk s1,s2;
@@ -74,8 +74,8 @@ struct my_que {
         }
     }
     int size() { return s1.size()+s2.size(); }
-    int mx() { return max(s1.mx(),s2.mx()); }
-    int mn() { return min(s1.mn(),s2.mn()); }
+    int mx() { return max(s1.mxe(),s2.mxe()); }
+    int mn() { return min(s1.mne(),s2.mne()); }
     int dif() { return mx()-mn(); }  
 }q;
 signed main() {
