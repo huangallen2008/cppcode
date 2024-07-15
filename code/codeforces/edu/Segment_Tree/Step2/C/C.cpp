b1dc637742e5edfc3c3fa725e6a5c864a24934d7
huangallen2008 <huangallen2008@gamil.con>
2024-07-15 13:31:26 +0000

    119715ing

--- code/codeforces/edu/Segment_Tree/Step2/C/C.cpp
+++ code/codeforces/edu/Segment_Tree/Step2/C/C.cpp
@@ -76,7 +76,7 @@ struct SEG {
     }
     int qu(int w,int l,int r,int ql,int qr) {
         if(ql<=l&&r<=qr) return s[w].v;
-        if(ql>r||qr<l) return 0;
+        if(ql>r||qr<l) return -1;
         push(s[w],s[w<<1],s[w<<1|1]);
         int m=l+r>>1;
         return qu(w<<1,l,m,ql,qr)&qu(w<<1|1,m+1,r,ql,qr);
