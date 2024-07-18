7f948b1db6c659b5acfcb89a224ba4a977ccd091
huangallen2008 <huangallen2008@gamil.con>
2024-07-18 11:58:03 +0000

    420018ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -43,8 +43,21 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+bool isp(int n) {
+    for(int i=2;i*i<=n;i++) if(n%i==0) return 0;
+    return 1;
+}
 signed main() {
     IOS();
-
+    int cnt=0;
+    for(int i=2;;i++) {
+        if(isp(i)) {
+            cnt++;
+        }
+        if(cnt==10000) {
+            ope(i)
+            break;
+        }
+    }
     return 0;
 }
