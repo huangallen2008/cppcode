5183c8e08c1d797e514ff270aaaaddba4143fd29
huangallen2008 <huangallen2008@gamil.con>
2024-07-28 15:23:09 +0000

    721028ing

--- code/other/test/tempCodeRunnerFile.cpp
+++ code/other/test/tempCodeRunnerFile.cpp
@@ -1,2 +1,12 @@
-5
-1 2 3 4 5
+
+    int n=100;
+    REP1(i,n) {
+        vector<int> v;
+        REP1(j,i) {
+            if(isp(i^j)) {
+                // cout<<i<<' '<<j<<'\n';
+                v.pb(j);
+            }
+        }
+        op(i)oparr(v)
+    }
