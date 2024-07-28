ccb7c45a67a76f98a4a841e715a51493244c8977
huangallen2008 <huangallen2008@gamil.con>
2024-07-28 16:48:12 +0000

    121129ing

---
+++ code/競賽/codeforces/Pinely Round 4 (Div. 1 + Div. 2)/F.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=1e4+5;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+vector<int> v;
+bool ok2(int a,int b,int c) {
+    return a+b>c&&a+c>b&&c+b>a;
+}
+bool ok(int a,int b,int c,int d,int e,int f) {
+    return ok2(a,b,c)&&ok2(d,e,f);
+}
+void solve() {
+    int n,q;
+    cin>>n>>q;
+    vector<int> a(n+1);
+    REP1(i,n) cin>>a[i];
+    vector<int> c(n);
+    REP1(l,n-5) {
+        int &v1=a[l],&v2=a[l+1],&v3=a[l+2],&v4=a[l+3],&v5=a[l+4],&v6=a[l+5];
+        v={a[l],a[l+1],a[l+2],a[l+3],a[l+4],a[l+5]};
+        c[l]=(ok(v1,v2,v3,v4,v5,v6)||ok(v1,v2,v4,v3,v5,v6)||ok(v1,v2,v5,v3,v4,v6)||ok(v1,v2,v6,v3,v4,v5)||ok(v1,v3,v4,v2,v5,v6)||ok(v1,v3,v5,v2,v4,v6)||ok(v1,v3,v6,v4,v5,v2)||ok(v1,v4,v5,v2,v3,v6)||ok(v1,v4,v6,v2,v3,v5)||ok(v1,v5,v6,v2,v3,v4));
+    }
+    REP1(i,n) c[i]+=c[i-1];
+    REP(i,q) {
+        int l,r;
+        cin>>l>>r;
+        if(c[r-5]!=c[l-1]) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
+signed main() {
+    IOS();
+    int T;
+    cin>>T;
+    while(T--) 
+        solve();
+    return 0;
+}
