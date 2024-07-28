1421063672207f823ca6a75c85f4c3fc6b33cbec
huangallen2008 <huangallen2008@gamil.con>
2024-07-28 17:34:03 +0000

    121129ing

--- code/競賽/codeforces/Pinely Round 4 (Div. 1 + Div. 2)/F.cpp
+++ code/競賽/codeforces/Pinely Round 4 (Div. 1 + Div. 2)/F.cpp
@@ -48,14 +48,17 @@ bool ok(int a,int b,int c) {
     return a+b>c&&a+c>b&&c+b>a;
 }
 struct DS {
-    multiset<pii> s;
-    multiset<int> s1;
+    set<pii> s;
+    set<int> s1;
     vector<int> o;
     void init(int n) {
         o=vector<int>(n+1);
     }
     void push_1(int x) {
         s1.insert(x);
+    }
+    void pop_1(int x) {
+        if(s1.find(x)!=s1.end()) s1.erase(x);
     }
     void push(int x,int id) {
         auto it=s.lower_bound({x,id}),itl=it;
@@ -76,7 +79,7 @@ struct DS {
         if(las.size()==2) {
             bool oo=ok((*las[0]).f,(*las[1]).f,x);
             if(oo) { 
-                if(o[(*las[1]).s]==0) push_1((*las[1]).s);
+                push_1((*las[1]).s);
                 o[(*las[1]).s]=1;
             }
             las.pop_back();
@@ -84,7 +87,7 @@ struct DS {
         if(las.size()+nxt.size()>=2) {
             bool oo=ok((*las[0]).f,(*nxt[0]).f,x);
             if(oo) { 
-                if(o[(*las[0]).s]==0) push_1((*las[0]).s);
+                push_1((*las[0]).s);
                 o[(*las[0]).s]=1;
             }
             las.pop_back();
@@ -92,25 +95,80 @@ struct DS {
         if(nxt.size()==2) {
             bool oo=ok((*nxt[0]).f,(*nxt[1]).f,x);
             if(oo) { 
-                if(o[id]==0) push_1(id);
+                push_1(id);
                 o[id]=1;
             }
         }
         s.insert({x,id});
     }
-};
+    void pop(int x,int id) {
+        auto it=s.find({x,id});
+        if(o[id]==1) {
+            o[id]=0;
+            s1.erase(id);
+        }
+        s.erase(it);
+        auto it=s.lower_bound({x,id}),itl=it;
+        vector<set<pii>::iterator> nxt,las;
+        if(it!=s.end()) {
+            nxt.pb(it);
+            it=next(it);
+            if(it!=s.end()) nxt.pb(it);
+        }
+        if(itl!=s.begin()) {
+            itl=prev(itl);
+            las.pb(itl);
+            if(itl!=s.begin()) {
+                itl=prev(itl);
+                las.pb(itl);
+            }
+        }
+        if(las.size()==2) {
+            bool oo=ok((*las[0]).f,(*las[1]).f,x);
+            if(!oo) { 
+                pop_1((*las[1]).s);
+                o[(*las[1]).s]=0;
+            }
+            las.pop_back();
+        }
+        if(las.size()+nxt.size()>=2) {
+            bool oo=ok((*las[0]).f,(*nxt[0]).f,x);
+            if(!oo) { 
+                pop_1((*las[0]).s);
+                o[(*las[0]).s]=0;
+            }
+            las.pop_back();
+        }
+        if(nxt.size()==2) {
+            bool oo=ok((*nxt[0]).f,(*nxt[1]).f,x);
+            if(!oo) { 
+                pop_1(id);
+                o[id]=0;
+            }
+        }
+    }
+}ds;
+int sn=0;
 void solve() {
     int n,q;
     cin>>n>>q;
+    sn=sqrt(n);
+    ds.init(n);
     vector<int> a(n+1);
     REP1(i,n) cin>>a[i];
     vector<int> c(n);
     REP1(i,n) c[i]+=c[i-1];
+    vector<pipii> qu(q);
     REP(i,q) {
-        int l,r;
-        cin>>l>>r;
-        if(c[r-5]!=c[l-1]) cout<<"YES\n";
-        else cout<<"NO\n";
+        qu[i].f=i;
+        cin>>qu[i].s.f>>qu[i].s.s;
+    }
+    sort(ALL(qu),[&](pipii a,pipii b) { if((int)a.s.f/sn==(int)b.s.f/sn) return a.s.s<b.s.s;return a.s.f/sn<b.s.f/sn; })
+    REP(i,q) {
+        while (l > ql) ds.push(a[--l]);
+        while (r < qr) ds.push(a[++r]);
+        while (l < ql) ds.pop(a[l++]);
+        while (r > qr) ds.pop(a[r--]);
     }
 }
 signed main() {
