05e0ef5d01fe1b195a9249a5fcd7b9b1ca5c1928
huangallen2008 <huangallen2008@gamil.con>
2024-07-28 08:07:52 +0000

    721028ing

--- code/codeforces/edu/two_pointer/Step3/H/H.cpp
+++ code/codeforces/edu/two_pointer/Step3/H/H.cpp
@@ -55,6 +55,7 @@ signed main() {
     int r=m,now=sumb,an=0;
     REP(l,n+1) {
         while(r>=0&&now>s) now-=b[r--];
+        op(l)ope(r)
         chmax(an,(r+1)*wb+l*wa);
         now+=a[l];
     }
