3a8c93eb2a856ba5da8be2ac9bd2c4c5c9446e4a
huangallen2008 <huangallen2008@gamil.con>
2024-07-30 07:38:58 +0000

    221230ing

--- code/競賽/codeforces/Pinely Round 4 (Div. 1 + Div. 2)/F.cpp
+++ code/競賽/codeforces/Pinely Round 4 (Div. 1 + Div. 2)/F.cpp
@@ -73,7 +73,6 @@ void solve() {
             cout<<"YES\n";
             continue;
         }
-        oparr(v)
         REP(i,r-l-5) {
             for(auto tt:t) {
                 if(okt(v[i+tt[0]],v[i+tt[1]],v[i+tt[2]])&&okt(v[i+tt[3]],v[i+tt[4]],v[i+tt[5]])) {
