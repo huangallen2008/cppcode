0a9f8f505d1a2ba28257b8c1e020ae8a8bc5ab5f
huangallen2008 <huangallen2008@gamil.con>
2024-07-31 16:23:27 +0000

    42141ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -48,7 +48,7 @@ signed main() {
     freopen("in.txt","w",stdout);
     int q=5000;
     cout<<q<<'\n';
-    RREP1(i,q) cout<<q<<' ';cout<<'\n';
+    RREP(i,q) cout<<5000-i%50<<' ';cout<<'\n';
 
     return 0;
 }
