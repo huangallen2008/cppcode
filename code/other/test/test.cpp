4a59273c5e32e9edd35f8e73c6b92903a50c9a25
huangallen2008 <huangallen2008@gamil.con>
2024-08-01 05:37:46 +0000

    42141ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -52,7 +52,7 @@ signed main() {
         cin>>n;
         vector<int> f(n+1);
         f[1]=1;
-        for(int i=2;i<=n;i++) f[i]=(f[i]+f[i-1])%mod;
+        for(int i=2;i<=n;i++) f[i]=(f[i-1]+f[i-2])%mod;
         cout<<f[n]<<' ';
     }
     cout<<'\n';
