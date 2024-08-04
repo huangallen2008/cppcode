c0b672554a498bbf8417067c507897d68f891b2c
huangallen2008 <huangallen2008@gamil.con>
2024-08-04 08:01:35 +0000

    72174ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -44,7 +44,7 @@ int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
 int cnt=0;
-void f(int a[5][5]) {
+void f(int a[4][5]) {
     
     REP(i,5) {auto *x=a[i];
         REP(j,5)cout<<x[j]<<' ';entr
