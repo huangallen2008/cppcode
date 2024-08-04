4fab2031918a85cf0933793c6e0abcafb2223ae7
huangallen2008 <huangallen2008@gamil.con>
2024-08-04 16:34:39 +0000

    12185ing

--- code/競賽/codeforces/Codeforces Round 963 (Div. 2)/D.cpp
+++ code/競賽/codeforces/Codeforces Round 963 (Div. 2)/D.cpp
@@ -46,12 +46,12 @@ int rd(int l,int r) {
 int n,k;
 bool check(vector<int> a,int m) {
     REP1(i,n) a[i]=(a[i]>=m?1:-1);
-    vector<int> dp(n+2);
+    vector<int> dp(n+2,-inf);
     vector<int> mp(k,-inf),mk(n+2);
     REP(i,n+2) mk[i]=i%k;
-    mp[0]=0;
+    mp[0]=0,dp[0]=0;
     REP1(i,n+1) {
-        dp[i]=dp[i-1];
+        // dp[i]=dp[i-1];
         chmax(dp[i],mp[mk[i-1]]);
         dp[i]+=a[i];
         chmax(mp[mk[i]],dp[i]);
