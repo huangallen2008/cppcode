46d88a5af491e87c17ddfe27879cdeec0dbe081d
huangallen2008 <huangallen2008@gamil.con>
2024-08-04 15:12:17 +0000

    72174ing

--- code/競賽/codeforces/Codeforces Round 963 (Div. 2)/C.cpp
+++ code/競賽/codeforces/Codeforces Round 963 (Div. 2)/C.cpp
@@ -25,7 +25,7 @@ using namespace std;
 #define chmax(x,y) x=max(x,y)
 #define chmin(x,y) x=min(x,y)
 #define addmod(x,y) x=((x+(y))%mod)
-#ifdef LOCAL_
+#ifdef LOCAL
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
 #define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
@@ -66,7 +66,6 @@ void solve() {
     int r=mx%K;
     int an=inf;
     REP1(i,K-1) p[i]+=p[i-1];
-    oparr(p)
     REP(i,K) {
         if(p[i])continue;
         if(i>=r) chmin(an,mx+i-r);
@@ -74,7 +73,6 @@ void solve() {
     }
     if(an==inf) cout<<"-1\n";
     else cout<<an<<'\n';
-    entr
 }
 signed main() {
     IOS();
