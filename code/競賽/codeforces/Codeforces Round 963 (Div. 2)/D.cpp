e405cb4d4b280cf39a9a526298d055b2ef5d1cf8
huangallen2008 <huangallen2008@gamil.con>
2024-08-04 16:14:32 +0000

    12185ing

--- code/競賽/codeforces/Codeforces Round 963 (Div. 2)/D.cpp
+++ code/競賽/codeforces/Codeforces Round 963 (Div. 2)/D.cpp
@@ -51,7 +51,7 @@ bool check(vector<int> a,int m) {
     mp[0]=0;
     REP1(i,n+1) {
         dp[i]=dp[i-1]+a[i];
-        chmax(dp[i],mp[(i-1)%k]);
+        chmax(dp[i],mp[(i-1)%k]+a[i]);
         chmax(mp[i%k],dp[i]);
     }
     return dp[n+1]>0;
