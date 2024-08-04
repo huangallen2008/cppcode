6f2c9f69b57321a589300bd74d4ace6b6cdd2f5e
huangallen2008 <huangallen2008@gamil.con>
2024-08-04 06:00:44 +0000

    72174ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -44,7 +44,7 @@ int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
 int cnt=0;
-void f(vector<int> &v) {
+void f(vector<int> v) {
     if(cnt++>=10) return;
     f(v);
 }
