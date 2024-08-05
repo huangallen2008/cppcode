328caab771b0897ae6ffa4bf035488cedccd263a
huangallen2008 <huangallen2008@gamil.con>
2024-08-05 04:55:57 +0000

    12185ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -1,38 +1,21 @@
 #include <bits/stdc++.h>
-#include "testlib.h"
- 
 using namespace std;
-struct Mos {
-    map<int,int> mp;
-    void init() {
-        mp.clear();
-        mp[-inf]=0,mp[inf]=inf;
-    }
-    void ins(int x,int y) {
-        auto it=prev(mp.upper_bound(x));
-        if(it->s>=y) return;
-        mp[x]=y;
-        it=mp.find(x);
-        while(next(it)->s<=it->s) {
-            mp.erase(next(it));
-        }
-    }
-    int qu(int k) {
-        auto it=prev(mp.upper_bound(x));
-        return it->s;
-    }
-} ;
 int main() {
     int n,q;
     cin>>n>>q;
-    vector<Mos> m(n);
-    REP(i,n) m[i].init();
-    REP(i,q) {
-        int opt;
-        cin>>opt;
-        if(opt==1) {
-            
-        }
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    int mx=a[0];
+    vector<int> d(n);
+    for(int i=1;i<n;i++) {
+        d[i]=max(d[i-1],mx-a[i]);
+        mx=max(mx,a[i]);
+    }
+    while(q--) {
+        int k;
+        cin>>k;
+        k--;
+        cout<<d[k]<<'\n';
     }
     return 0;
 }
