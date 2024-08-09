2cad1442872a3c7474ec29d6a58a8e92903f7e31
huangallen2008 <huangallen2008@gamil.con>
2024-08-09 02:28:48 +0000

    52229ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3,unroll-loops,fast-math")
-// #pragma GCC target("avx2,bmi,popcnt")
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC target("avx2,bmi,popcnt")
 // #define int long long
 #define ll long long
 #define REP(i,n) for(int i=0;i<(n);i++)
@@ -24,51 +24,65 @@ using namespace std;
 #define ld long double
 #define pdd pair<ld,ld>
 #define chmax(x,y) x=max(x,y)
-#define chmin(x,y) x=min(x,y)
+#define chmin(x,y) mn(x,y)
 #define addmod(x,y) x=((x+(y))%mod)
-#ifdef LOCAL
+//#ifdef LOCAL
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
 #define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
 #define entr cout<<endl;
-#else
-#define op(x) ;
-#define ope(x) ;
-#define oparr(x) ;
-#define entr ;
-#endif
+//#else
+//#define op(x) ;
+//#define ope(x) ;
+//#define oparr(x) ;
+//#define entr ;
+//#endif
 const int mod=1e9+7;
 const int maxn=2e5+5;
 const ll inf=(1ll<<62);
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-int rd(int l,int r) {
-    return uniform_int_distribution<int>(l,r)(rng);
-}
-int n,k,x,y,z;
-ll cost(int v) {
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//int rd(int l,int r) {
+//    return uniform_int_distribution<int>(l,r)(rng);
+//}
+#include<unistd.h>
+char OB[65536]; int OP;
+inline char RC(){static char buf[65536],*p=buf,*q=buf;return p==q&&(q=(p=buf)+read(0,buf,65536))==buf?-1:*p++;}
+inline int R(){static char c;int a;while((c=RC())<'0');a=c^'0';while((c=RC())>='0')a*=10,a+=c^'0';return a;}
+const int n,k,x,y,z;
+inline ll cost(const int v) {
     if(v<0) return (ll)x*(-v);
     else return (ll)y*v;
 }
+inline void mn(ll &a,const ll b) { a=a>b?b:a; }
 signed main(){
     IOS();
-    cin>>n>>k>>x>>y>>z;
+//    cin>>n>>k>>x>>y>>z;
+    n=R(),k=R(),x=R(),y=R(),z=R();
     vector<int> a(n);
-    REP(i,n) cin>>a[i];
+    REP(i,n) a[i]=R();
     vector<int> s(1<<n);
-    for(int i=1;i<1<<n;i++) {
+    const int all=(1<<n)-1;
+    for(int i=1;i<=all;i++) {
         s[i]=s[i^(i&-i)]+a[__lg(i&-i)];
     }
-    vector<pii> f;
-    for(int i=1;i*i<=k;i++) if(k%i==0) f.pb({i,k/i});
     ll an=inf;
-    REP(i,1<<n) {
-        if(i==0||(i&-i)==i) continue;
-        int cnt1=__builtin_popcount(i),v=(cnt1-2)*z;
-        for(int j=(i-1)&i;j>0;j=(j-1)&i) {
-            if(__builtin_popcount(j)>(cnt1>>1)) continue;
-            for(auto &[f1,f2]:f) {
-                chmin(an,min(cost(s[j]-f1)+cost(s[j^i]-f2),cost(s[j]-f2)+cost(s[j^i]-f1))+v);
+    vector<ll> dp1(1<<n);
+    for(int f1=1;f1*f1<=k;f1++) {
+        if(k%f1) continue;
+        int f2=k/f1;
+        dp1[0]=inf;
+        for(int i=1;i<=all;i++) {
+            dp1[i]=cost(s[i]-f1)+(ll)(__builtin_popcount(i))*z;
+        }
+        REP(i,n) {
+            int t=all^(1<<i);
+//            REP(j,1<<n) {
+            for(int j=t;j>0;j=(j-1)&t) {
+                chmin(dp1[j^(1<<i)],dp1[j]);
             }
+        }
+        for(int i=1;i<all;i++) {
+            chmin(an,cost(s[i]-f2)+(ll)(__builtin_popcount(i)-2)*z+dp1[all^i]);
         }
     }
     cout<<an<<'\n';
