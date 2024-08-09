e958eacd11bfccbf98366aef010f5209a8bf2a92
huangallen2008 <huangallen2008@gamil.con>
2024-08-09 03:08:37 +0000

    52229ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -61,7 +61,7 @@ const ll inf=(1ll<<62);
 char OB[65536]; int OP;
 inline char RC(){static char buf[65536],*p=buf,*q=buf;return p==q&&(q=(p=buf)+read(0,buf,65536))==buf?-1:*p++;}
 inline int R(){static char c;int a;while((c=RC())<'0');a=c^'0';while((c=RC())>='0')a*=10,a+=c^'0';return a;}
-inline void W(ll n){static char buf[12],p;if(n==0)OB[OP++]='0';p=0;while(n)buf[p++]='0'+(n%10),n/=10;for(--p;p>=0;--p)OB[OP++]=buf[p];OB[OP++]='\n';if(OP>65516)write(1,OB,OP),OP=0;}
+inline void W(ll n){static char buf[12],p;if(n==0)OB[OP++]='0';p=0;while(n)buf[p++]='0'+(n%10),n/=10;for(--p;p>=0;--p)OB[OP++]=buf[p];OB[OP++]='\n';if(OP>65536)write(1,OB,OP),OP=0;}
 int n,k,x,y,z;
 inline ll cost(const int v) {
     return v<0?(ll)x*(-v):(ll)y*v;
