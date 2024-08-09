dc3ac57bcb83cddf3c95d8a644a5ec26f9a5cfa4
huangallen2008 <huangallen2008@gamil.con>
2024-08-09 03:07:23 +0000

    52229ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -57,8 +57,8 @@ const ll inf=(1ll<<62);
 //     while(ch>='0'&&ch<='9') x=(x<<3)+(x<<1)+ch-'0',ch=GC;
 //     return x;
 // }
-// #include<unistd.h>
-// char OB[65536]; int OP;
+#include<unistd.h>
+char OB[65536]; int OP;
 // inline char RC(){static char buf[65536],*p=buf,*q=buf;return p==q&&(q=(p=buf)+read(0,buf,65536))==buf?-1:*p++;}
 inline int R(){static char c;int a;while((c=RC())<'0');a=c^'0';while((c=RC())>='0')a*=10,a+=c^'0';return a;}
 inline void W(ll n){static char buf[12],p;if(n==0)OB[OP++]='0';p=0;while(n)buf[p++]='0'+(n%10),n/=10;for(--p;p>=0;--p)OB[OP++]=buf[p];OB[OP++]='\n';if(OP>65516)write(1,OB,OP),OP=0;}
