22ef1003fa93c2d66f3d8d6ea0dbc47db9173ba3
huangallen2008 <huangallen2008@gamil.con>
2024-08-09 03:07:47 +0000

    52229ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -35,13 +35,13 @@ using namespace std;
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
 #define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
 #define entr cout<<endl;
-#define RC _getchar_nolock
+// #define RC _getchar_nolock
 #else
 #define op(x) ;
 #define ope(x) ;
 #define oparr(x) ;
 #define entr ;
-#define RC getchar_unlocked
+// #define RC getchar_unlocked
 #endif
 const int mod=1e9+7;
 const int maxn=2e5+5;
@@ -59,7 +59,7 @@ const ll inf=(1ll<<62);
 // }
 #include<unistd.h>
 char OB[65536]; int OP;
-// inline char RC(){static char buf[65536],*p=buf,*q=buf;return p==q&&(q=(p=buf)+read(0,buf,65536))==buf?-1:*p++;}
+inline char RC(){static char buf[65536],*p=buf,*q=buf;return p==q&&(q=(p=buf)+read(0,buf,65536))==buf?-1:*p++;}
 inline int R(){static char c;int a;while((c=RC())<'0');a=c^'0';while((c=RC())>='0')a*=10,a+=c^'0';return a;}
 inline void W(ll n){static char buf[12],p;if(n==0)OB[OP++]='0';p=0;while(n)buf[p++]='0'+(n%10),n/=10;for(--p;p>=0;--p)OB[OP++]=buf[p];OB[OP++]='\n';if(OP>65516)write(1,OB,OP),OP=0;}
 int n,k,x,y,z;
