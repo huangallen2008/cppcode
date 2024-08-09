515b4a0a7baec414e792eb6a0b8788774bff90b7
huangallen2008 <huangallen2008@gamil.con>
2024-08-09 02:28:57 +0000

    52229ing

--- code/other/test/test2.cpp
+++ code/other/test/test2.cpp
@@ -48,7 +48,7 @@ const ll inf=(1ll<<62);
 char OB[65536]; int OP;
 inline char RC(){static char buf[65536],*p=buf,*q=buf;return p==q&&(q=(p=buf)+read(0,buf,65536))==buf?-1:*p++;}
 inline int R(){static char c;int a;while((c=RC())<'0');a=c^'0';while((c=RC())>='0')a*=10,a+=c^'0';return a;}
-const int n,k,x,y,z;
+int n,k,x,y,z;
 inline ll cost(const int v) {
     if(v<0) return (ll)x*(-v);
     else return (ll)y*v;
