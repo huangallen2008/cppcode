06c8bb6bbee2cd3b4ddddc771944c7b1f94324fc
huangallen2008 <huangallen2008@gamil.con>
2024-08-12 14:53:19 +0000

    122512ing

--- code/Atcoder/ABC223G/ABC223G.cpp
+++ code/Atcoder/ABC223G/ABC223G.cpp
@@ -69,7 +69,7 @@ void dfs2(int u,int fa) {
         pd1[u]=0;
     }
     else {
-        pd1[u]=pd1[fa]+dp1[fa]-dp0[u];
+        pd1[u]=pd0[fa]+dp1[fa]-dp0[u];
         pd0[u]=pd1[u]+max(max({pre[u],suf[u],fa==0?-inf:pd1[fa]-pd0[fa]})+1,0ll);
     }
     an[u]=dp1[u]+pd0[u];
