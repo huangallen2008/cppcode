4dec6028f596d66885d3f88efc523a7a097785be
huangallen2008 <huangallen2008@gamil.con>
2024-08-12 18:04:14 +0000

    222613ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -50,7 +50,7 @@ namespace NTT {
     const int mod=75161927681;
     int MA(int a,int b) { int c=a+b; if(c>mod) c-=mod; return c; }
     int MM(int a,int b) { int c=a-b; if(c<0) c+=mod; return c; }
-    int MU(int a,int b) { int c=0; if(a<b) swap(a,b); while(b>0) { if(b&1) c=MA(c,a); c=MA(a,a); b>>=1; } return c; }
+    int MU(int a,int b) { int c=0; if(a<b) swap(a,b); while(b>0) { if(b&1) c=MA(c,a); a=MA(a,a); b>>=1; } return c; }
     int pw(int x,int p) {
         int r=1;
         while(p>0) {
