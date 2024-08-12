2890670bc51e9e2739e7a2ab48bfb3ac8e6e4b4c
huangallen2008 <huangallen2008@gamil.con>
2024-08-12 17:27:04 +0000

    222613ing

--- code/Cpp_template/NTT.cpp
+++ code/Cpp_template/NTT.cpp
@@ -51,7 +51,7 @@ struct NTT {
     const int G=3;
     const int INVG=332748118;
     vector<int> r,c;
-    int n,m,t,lt;
+    int n1,n2,t,lt;
     int pw(int x,int p) {
         int r=1;
         while(p>0) {
@@ -64,24 +64,28 @@ struct NTT {
     int inv(int x) {
         return pw(x,mod-2);
     }
+    int MA(int a,int b) { int c=a+b;if(c>mod) c-=mod;return c; }
+    int MM(int a,int b) { int c=a-b;if(c<0) c+=mod;return c; }
+    int MU(int a,int b) { int c=0; if(a<b) swap(a,b); while(b>0) { if(b&1) c=MA(c,a); a=MA(a,a); b>>=1;} return c;}
     void ntt(vector<int> &a,int opt){
         for (int i=0;i<t;i++) if(i<r[i]) swap(a[i],a[r[i]]);
-        for (int mid = 1;mid < t;mid <<= 1){
-            int OMG = pw(opt == 1 ? G : INVG,(mod - 1) / (mid << 1));
-            for (int R = mid << 1,l = 0;l < t;l += R){
-                int omg = 1;
-                for (int k = 0;k < mid;k ++,(omg *= OMG) %= mod){
-                    int tmp1 = a[l + k],tmp2 = a[l + mid + k] * omg % mod;
-                    a[l + mid + k] = (tmp1 - tmp2 + mod) % mod;
-                    a[l + k] =(tmp1 + tmp2) % mod;
+        for (int m=1;m<t;m<<=1){
+            int OMG=pw(opt==1?G:INVG,(mod-1)/(m<<1));
+            for (int l=0;l<t;l+=m<<1){
+                int omg=1;
+                for (int k=0;k<m;k++,){
+                    int t1=a[l+k],t2=a[l+m+k]*omg%mod;
+                    a[l+m+k]=MM(t1-t2);
+                    a[l+k]=MA(t1+t2);
+                    omg*= OMG,omg %= mod
                 }
             }
         }
     }
     vector<int>& init(vector<int>&a,vector<int>&b){
-        n=a.size(),m=b.size();
+        int n1=a.size(),n2=b.size();
         t=1,lt=0;
-        while(t<n+m) t<<=1,lt++;
+        while(t<n1+n2) t<<=1,lt++;
         while(a.size()<t) a.pb(0);
         while(b.size()<t) b.pb(0);
         r=c=vector<int>(t);
@@ -90,7 +94,7 @@ struct NTT {
         for (int i=0;i<t;i++) c[i]=a[i]*b[i]%mod;
         ntt(c,-1);
         int invn=inv(t);
-        for(int i=0;i<=n+m;i++) c[i]=(c[i]*invn)%mod;
+        for(int i=0;i<=n1+n2;i++) c[i]=c[i]*invn%mod;
         return c;
     }
 }ntt;
