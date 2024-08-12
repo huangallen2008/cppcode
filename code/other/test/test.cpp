12d2d3589f961186d962b5ef1d1280b92c03e4d0
huangallen2008 <huangallen2008@gamil.con>
2024-08-12 18:06:50 +0000

    222613ing

--- code/other/test/test.cpp
+++ code/other/test/test.cpp
@@ -118,7 +118,7 @@ signed main() {
         b[x]++;
     }
     vector<int> c=NTT::ntt(a,b);
-    while(c.size()<k<<1) c.pb(0);
+    while(c.size()<=k<<1) c.pb(0);
     for(int i=2;i<=k<<1;i++) cout<<c[i]<<' ';
     cout<<'\n';
     return 0;
