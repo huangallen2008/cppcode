c21f4ed21541ab3370a1f41f9b1552dfd2f96a47
huangallen2008 <huangallen2008@gamil.con>
2024-08-16 16:04:36 +0000

    623017ing

--- code/codeforces/1646D/1646D.cpp
+++ code/codeforces/1646D/1646D.cpp
@@ -43,8 +43,53 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+pii& operator+(pii &a,pii b) {
+    a.f+=b.f,a.s+=b.s;
+}
+Graph g;
+vector<pii> dp[2];
+vector<bool> use;
+void dfs(int u,int pa) {
+    for(int v:g[u]) {
+        if(v==pa) continue;
+        dfs(v,u);
+        dp[1][u]+=dp[0][v];
+        dp[0][u]+=max(dp[0][v],dp[1][v]);
+    }
+    dp[0][u].s--;
+    dp[1][u].f++,dp[1][u].s-=g[u].size();
+}
+void dfs2(int u,int pa,int ty) {
+    use[u]=ty;
+    for(int v:g[u]) {
+        if(v==pa) continue;
+        if(ty) dfs(v,u,0);
+        else dfs(v,u,dp[1][v]>dp[0][v]);
+    }
+}
 signed main() {
     IOS();
-
+    int n;
+    cin>>n;
+    dp[0]=vector<pii>(n);
+    dp[1]=vector<pii>(n);
+    use=vector<bool>(n);
+    g=Graph(n);
+    REP(i,n-1) {
+        int u,v;
+        cin>>u>>v;
+        u--,v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    dfs(0,-1);
+    pii an=max(dp[0][0],dp[1][0]);
+    cout<<an.f<<' '<<an.s<<'\n';
+    if(dp[0][0]==an) dfs2(0,-1,0);
+    else dfs2(0,-1,1);
+    REP(i,n) {
+        if(use[i]) cout<<g[i].size()<<' ';
+        else cout<<1<<'\n';
+    }
     return 0;
 }
