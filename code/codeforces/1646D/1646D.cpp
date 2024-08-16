d79b5b0309562556e3f81f03d1da03d642a9616c
huangallen2008 <huangallen2008@gamil.con>
2024-08-16 16:06:53 +0000

    623017ing

--- code/codeforces/1646D/1646D.cpp
+++ code/codeforces/1646D/1646D.cpp
@@ -57,6 +57,7 @@ void dfs(int u,int pa) {
         dp[1][u]+=dp[0][v];
         dp[0][u]+=max(dp[0][v],dp[1][v]);
     }
+    ope("uu")
     dp[0][u].s--;
     dp[1][u].f++,dp[1][u].s-=g[u].size();
 }
