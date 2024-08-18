81c41160311da6023e4590d655dd15f4c59380ad
huangallen2008 <huangallen2008@gamil.con>
2024-08-18 17:35:29 +0000

    123219ing

--- code/Atcoder/ABC207F/ABC207F.cpp
+++ code/Atcoder/ABC207F/ABC207F.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 #pragma GCC optimize("O3,unroll-loops,fast-math")
 // #pragma GCC target("avx2,sse4,bmi,popcnt")
-// #define int long long
-#define ll long long
+#define int long long
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
@@ -25,7 +24,7 @@ using namespace std;
 #define pdd pair<ld,ld>
 #define chmax(x,y) x=max(x,y)
 #define chmin(x,y) x=min(x,y)
-#define addmod(x,y) x=((x+(y))%mod)
+// #define addmod(x,y) x=((x+(y))%mod)
 #ifdef LOCAL
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
@@ -44,6 +43,10 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+void addmod(int &a,int b) {
+    a+=b;
+    if(a>=mod) a-=mod;
+}
 int n;
 Graph g;
 vector<vector<int>> dp0,dp1,dp2;
@@ -61,9 +64,9 @@ void dfs(int u,int pa) {
         // op(sz[u])ope(sz[v])
         for(int i=sz[u];i>=0;i--) {
             for(int j=i;j>=i-sz[v]-1&&j>=0;j--) {
-                addmod(t0[i],dp0[u][j]*1ll*(dp0[v][i-j]+dp1[v][i-j]));
-                addmod(t1[i],dp1[u][j]*1ll*((ll)dp0[v][i-j]+dp1[v][i-j]+dp2[v][i-j])+(i>j?dp0[u][j]*dp2[v][i-j-1]:0));
-                addmod(t2[i],dp2[u][j]*1ll*((ll)dp1[v][i-j]+dp2[v][i-j]+(i>j?dp0[v][i-j-1]:0)));
+                addmod(t0[i],dp0[u][j]*(dp0[v][i-j]+dp1[v][i-j]));
+                addmod(t1[i],dp1[u][j]*(dp0[v][i-j]+dp1[v][i-j]+dp2[v][i-j])+(i>j?dp0[u][j]*dp2[v][i-j-1]:0));
+                addmod(t2[i],dp2[u][j]*(dp1[v][i-j]+dp2[v][i-j]+(i>j?dp0[v][i-j-1]:0)));
             }
         }
         // oparr(t0)oparr(t1)oparr(t2)entr
@@ -97,7 +100,11 @@ signed main() {
     // REP(i,n) {REP(j,n+1) cout<<dp0[i][j]<<' ';entr}entr
     // REP(i,n) {REP(j,n+1) cout<<dp1[i][j]<<' ';entr}entr
     // REP(i,n) {REP(j,n+1) cout<<dp2[i][j]<<' ';entr}entr
-    REP(i,n+1) an[i]=((ll)dp0[0][i]+dp1[0][i]+dp2[0][i])%mod;
+    REP(i,n+1) {
+        an[i]=dp0[0][i];
+        addmod(an[i],dp1[0][i]);
+        addmod(an[i],dp2[0][i]);
+    }
     REP(i,n+1) cout<<an[i]<<'\n';
     return 0;
 }
