ff5ea3047989c4666503f97f7ff5b7419c3cf157
huangallen2008 <huangallen2008@gamil.con>
2024-08-21 06:20:03 +0000

    323421ing

--- code/codeforces/1830D/1830D.cpp
+++ code/codeforces/1830D/1830D.cpp
@@ -39,7 +39,7 @@ using namespace std;
 const int mod=1e9+7;
 const int maxn=2e5+5;
 const int sn=6;
-const int inf=(1ll<<30);
+const int inf=(1ll<<60);
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
