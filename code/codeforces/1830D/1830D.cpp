745ca7bf287b3839a60fc55efcb24b1940817573
huangallen2008 <huangallen2008@gamil.con>
2024-08-21 13:45:28 +0000

    323421ing

--- code/codeforces/1830D/1830D.cpp
+++ code/codeforces/1830D/1830D.cpp
@@ -25,7 +25,7 @@ using namespace std;
 #define chmax(x,y) x=max(x,y)
 #define chmin(x,y) x=min(x,y)
 #define addmod(x,y) x=((x+(y))%mod)
-#ifdef LOCAL_
+#ifdef LOCAL
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
 #define oparr(x) cout<<(#x)<<":";for(auto &allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
@@ -81,7 +81,6 @@ void dfs(int u,int pa) {
 }
 void solve() {
     ope(sizeof(dp))
-    ope("ok")
     int n;
     cin>>n;
     // dp0=dp1=vector<vector<int>>(n);
