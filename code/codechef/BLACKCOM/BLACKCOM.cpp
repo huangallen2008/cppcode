1ba9fa283671f322d581f0ffacd3000c71b908aa
huangallen2008 <huangallen2008@gamil.con>
2024-08-22 05:49:36 +0000

    423522ing

--- code/codechef/BLACKCOM/BLACKCOM.cpp
+++ code/codechef/BLACKCOM/BLACKCOM.cpp
@@ -92,7 +92,7 @@ void solve() {
     REP(i,n-1) {
         int u,v;
         cin>>u>>v,u--,v--;
-        op(u)ope(v)
+        op(u)ope(v)ope(g.size())
         g[u].pb(v);
         g[v].pb(u);
     }
