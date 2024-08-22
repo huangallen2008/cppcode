c55668ebd4b331ca36f4ed4330db0f60d1349ef8
huangallen2008 <huangallen2008@gamil.con>
2024-08-22 16:16:11 +0000

    523623ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC optimize("O3,unroll-loops,fast-math")
 #pragma GCC target("avx2,bmi,popcnt")
 // #define int long long
 #define REP(i,n) for(int i=0;i<(n);i++)
