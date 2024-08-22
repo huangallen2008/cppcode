d424d53eabf6a8696afc86ad92dca1f0795d510c
huangallen2008 <huangallen2008@gamil.con>
2024-08-22 16:17:17 +0000

    523623ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -71,7 +71,7 @@ signed main() {
     IOS();
     int a=5;
     cout<<fixed<<setprecision(3)<<(ld)clock()/CLOCKS_PER_SEC<<endl;
-    int c[500000000];
+    int c[5000000000];
     cout<<fixed<<setprecision(3)<<(ld)clock()/CLOCKS_PER_SEC<<endl;
     // ope(c)
     // ope(*c)
