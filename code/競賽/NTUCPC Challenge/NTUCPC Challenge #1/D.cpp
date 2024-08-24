4aba4617d84e6ad33afc4041c25bbdc88888565d
huangallen2008 <huangallen2008@gamil.con>
2024-08-24 06:23:41 +0000

    623724ing

--- code/競賽/NTUCPC Challenge/NTUCPC Challenge #1/D.cpp
+++ code/競賽/NTUCPC Challenge/NTUCPC Challenge #1/D.cpp
@@ -73,8 +73,7 @@ signed main() {
     REP(i,maxc) if(b[i].size()) {
         cl.pb({b[i].back(),i});
     }
-    vector<vector<int>> dp(inc+1,vector<int>(maxc));
-    REP(i,maxc) REP(j,maxc) dp[i][j]=-inf;
+    vector<vector<int>> dp(inc+1,vector<int>(maxc,-inf));
     dp[0][0]=0;
     dp[1][cl[0].s]=cl[0].f;
     REP1(i,SZ(cl)-1) {
