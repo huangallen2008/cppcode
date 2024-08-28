3514263bbf5e54adf418a96ab2f55b24dcac4277
huangallen2008 <huangallen2008@gamil.con>
2024-08-28 13:32:26 +0000

    324128ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段1/C_2.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段1/C_2.cpp
@@ -90,21 +90,21 @@ void solve() {
         vis=vector<bool>(n);
         vector<bool> ch(n),ch2(n);
         REP(i,n) if((co>>i)&1) ch[i]=1;
-        // REP(i,n) cout<<ch[i];entr
+        REP(i,n) cout<<ch[i];entr
         REP(i,n) {
             if(ch[i]) ch2[i]=1;
             for(int v:g2[i]) {
                 if(ch[v]) ch2[i]=1;
             }
         }
-        // REP(i,n) cout<<ch2[i];entr
+        REP(i,n) cout<<ch2[i];entr
         REP(i,n) {
             if(ch2[i]) vis[i]=1;
             for(int v:g2[i]) {
                 if(ch2[v]) vis[i]=1;
             }
         }
-        // REP(i,n) cout<<vis[i];entr
+        REP(i,n) cout<<vis[i];entr
         REP(i,n){
             if(vis[i]) continue;
             dfs(i);
