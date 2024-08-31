4482eb13fbedf371850cba36d0eae8c3c2f2b988
huangallen2008 <huangallen2008@gamil.con>
2024-08-31 12:17:08 +0000

    624431ing

--- code/python/test.py
+++ code/python/test.py
@@ -1,3 +1,5 @@
 from sys import stdin
 from bisect import bisect_right
 h, w, n = map(int, stdin.readline().strip().split())
+
+a = sorted([tuple(map(int, stdin.readline().strip().split())) for i in range(n)] + [h, w])
