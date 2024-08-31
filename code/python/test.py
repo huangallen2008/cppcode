dee53665edc12c2dc2f1c20efd51c2b2a68debf5
huangallen2008 <huangallen2008@gamil.con>
2024-08-31 12:23:23 +0000

    624431ing

--- code/python/test.py
+++ code/python/test.py
@@ -10,11 +10,19 @@ bk = [-1]*n
 for i in range(n):
     x, y = a[i]
     idx = bisect_right(LIS, (y, i))
+    
     if idx == len(LIS):
         LIS.append(y)
+        bk[i] = LIS[-1][1]
     else:
         LIS[idx] = y
+        bk[i] = bk[LIS[idx]][1]
 
 print(len(LIS))
 
-v = LIS
+v = LIS[-1][1]
+
+ans = ""
+
+while v != -1:
+    ans = "D"* + "R"* + ans
