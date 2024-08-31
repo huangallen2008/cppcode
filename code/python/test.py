8f5fca17ab70a8aea74edac2ad35407183e96676
huangallen2008 <huangallen2008@gamil.con>
2024-08-31 12:19:56 +0000

    624431ing

--- code/python/test.py
+++ code/python/test.py
@@ -5,6 +5,7 @@ h, w, n = map(int, stdin.readline().strip().split())
 a = sorted(tuple(map(int, stdin.readline().strip().split())) for i in range(n))
 
 LIS = []
+bk = [-1]*n
 
 for x, y in a:
     idx = bisect_right(LIS, y)
