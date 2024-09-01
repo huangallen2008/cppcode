7be515c6c248e2b3efb70c1c7e488c23b8d49db8
huangallen2008 <huangallen2008@gamil.con>
2024-09-01 07:48:31 +0000

    72451ing

---
+++ code/luogu/P3865/P3865.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=1e5+5;
+const int maxb=18;
+const int inf=(1ll<<50);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+int n;
+int st[maxb][maxn];
+void init() {
+    REP(i,maxb) {
+        REP(j,n) {
+            st[i][j]=max(st[i-1][j],st[i-1][min(n-1,j+(1<<i-1))]);
+        }
+    }
+}
+int qu(int l,int r) {
+    int lg=__lg(r-l+1);
+    return max(st[lg][l],st[lg][r-(1<<lg)+1]);
+}
+signed main() {
+    IOS();
+    int n,m;
+    cin>>n>>m;
+    REP(i,n) cin>>st[0][i];
+    init();
+    REP(i,m) {
+        int l,r;
+        cin>>l>>r,l--,r--;
+        cout<<qu(l,r)<<'\n';
+    }
+    return 0;
+}
