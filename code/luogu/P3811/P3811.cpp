0b50c44499498efa7a8931f27e27de716c380410
huangallen2008 <huangallen2008@gamil.con>
2024-09-01 08:10:04 +0000

    72451ing

--- code/luogu/P3811/P3811.cpp
+++ code/luogu/P3811/P3811.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 // #pragma GCC optimize("O3,unroll-loops,fast-math")
-#pragma GCC target("avx2,sse4,bmi2,popcnt")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
 // #define int long long
 #define ll long long
 #define REP(i,n) for(int i=0;i<(n);i++)
