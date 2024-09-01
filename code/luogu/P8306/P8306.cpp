af1f60fc4fe9d63d475d72e5f8311e50571c6794
huangallen2008 <huangallen2008@gamil.con>
2024-09-01 06:03:31 +0000

    72451ing

--- code/luogu/P8306/P8306.cpp
+++ code/luogu/P8306/P8306.cpp
@@ -69,7 +69,6 @@ struct TRIE {
             if(ch[w][t]==-1) ch[w][t]=node_id++;
             w=ch[w][t];
             cnt[w]++;
-            op(w)ope(cnt[w])
         }
     }
     int qu(string s) {
