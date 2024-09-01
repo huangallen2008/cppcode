8b324a05a8fac21940f5d185071946a707739930
huangallen2008 <huangallen2008@gamil.con>
2024-09-01 06:06:18 +0000

    72451ing

--- code/luogu/P8306/P8306.cpp
+++ code/luogu/P8306/P8306.cpp
@@ -37,7 +37,7 @@ using namespace std;
 #define entr ;
 #endif
 const int mod=1e9+7;
-const int maxn=3e6+5;
+const int maxn=3e6+1e5;
 const int inf=(1ll<<50);
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
