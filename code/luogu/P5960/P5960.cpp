cc26ae42c9c791a63dfda151e5a1c9dbbc86dc41
huangallen2008 <huangallen2008@gamil.con>
2024-09-01 17:01:48 +0000

    12462ing

---
+++ code/luogu/P5960/P5960.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=998244353;
+const int maxn=1e4+5;
+const int maxb=18;
+const int inf=(1ll<<50);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+struct edge {
+    int u,v,w;
+};
+signed main() {
+    IOS();
+    int n,m;
+    cin>>n>>m;
+    vector<int> dis(n+1,inf);
+    dis[0]=0;
+    vector<edge> e(m);
+    REP(i,m) cin>>e[i].v>>e[i].u>>e[i].w;
+    bool nc=0;
+    REP(i,n+1) {
+        bool ok=1;
+        for(auto [u,v,w]:e) {
+            if(dis[u]+w<dis[v]) {
+                dis[v]=dis[u]+w;
+                ok=0;
+            }
+        }
+        if(ok) break;
+        if(i==n) {
+            nc=1;
+        }
+    }
+    if(nc) cout<<"NO\n";
+    else {
+        REP1(i,n) cout<<dis[i]<<' ';
+        cout<<'\n'; 
+    }
+    return 0;
+}
