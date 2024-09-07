77a404235e21aedba49f27199d7f16ef8fe7310d
huangallen2008 <huangallen2008@gamil.con>
2024-09-07 16:48:35 +0000

    72528ing

---
+++ code/luogu/P5431/P5431.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+// const int mod=998244353;
+int mod=998244353;
+const int maxn=1e4+5;
+const int maxb=18;
+const int inf=(1ll<<50);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+int pw(int x,int p) {
+    int r=1;
+    while(p>0) {
+        if(p&1) r=r*x%mod;
+        x=x*x%mod;
+        p>>=1;
+    }
+    return r;
+}
+int inv(int x) { return pw(x,mod-2); }
+signed main() {
+    IOS();
+    int n,k;
+    cin>>n>>mod>>k;
+    vector<int> a(n),f(n),fi(n),in(n);
+    REP(i,n) cin>>a[i];
+    f[0]=a[0];
+    for(int i=1;i<n;i++) f[i]=a[i]*f[i-1]%mod;
+    fi[n-1]=inv(f[n-1]);
+    for(int i=n-2;i>=0;i--) fi[i]=fi[i+1]*a[i+1]%mod;
+    in[0]=fi[0];
+    for(int i=1;i<n;i++) in[i]=f[i]*fi[i-1]%mod;
+    int an=0;
+    RREP(i,n) an=(an+in[i])*k%mod;
+    cout<<an<<'\n';
+    return 0;
+}
