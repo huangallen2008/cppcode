7581600278d7f5b27bec68d2c751289ccdc48df8
huangallen2008 <huangallen2008@gamil.con>
2024-09-11 06:08:01 +0000

    325511ing

--- code/luogu/P10815/P10815.cpp
+++ code/luogu/P10815/P10815.cpp
@@ -44,7 +44,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-#define GC getchar()
+#define GC getchar_unlocked()
 inline int read()
 {
     int x=0;
