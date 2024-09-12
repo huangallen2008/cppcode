db7a2fd4593ff8efd7a1eb46060866a37707c458
huangallen2008 <huangallen2008@gamil.con>
2024-09-12 15:32:37 +0000

    425612ing

--- code/科展程式/draw_graph.py
+++ code/科展程式/draw_graph.py
@@ -93,8 +93,8 @@ with open('F_I_star_data.csv', newline='') as csvfile:
 # 創建一個子圖
 fig, ax = plt.subplots()
 ax.plot(x2, y2,color='blue',markersize=1)
-ax.plot(x, y,color='red',markersize=1)
-ax.plot(x3, y3,color='red',markersize=1)
+# ax.plot(x, y,color='red',markersize=1)
+# ax.plot(x3, y3,color='red',markersize=1)
 
 
 # ax.set_xlim(min(x), max(x))
