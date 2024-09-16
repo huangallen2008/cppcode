0c6b8e5251aca389ea3b352c85921ad1210915b5
huangallen2008 <huangallen2008@gamil.con>
2024-09-16 16:31:58 +0000

    226117ing

--- code/codeforces/833B/833B.cpp
+++ code/codeforces/833B/833B.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3,unroll-loops,fast-math")
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
 #pragma GCC target("avx2,sse4,bmi,popcnt")
 // #define int long long
 #define REP(i,n) for(int i=0;i<(n);i++)
