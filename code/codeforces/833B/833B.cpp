ab4ea709647d7d4b55f66a25895cfceb7bd35970
huangallen2008 <huangallen2008@gamil.con>
2024-09-16 15:22:03 +0000

    126016ing

--- code/codeforces/833B/833B.cpp
+++ code/codeforces/833B/833B.cpp
@@ -38,7 +38,7 @@ using namespace std;
 #define entr ;
 #endif
 const int mod=1e9+7;
-const int maxn=50;
+const int maxn=1e6;
 const int inf=(1ll<<50);
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
@@ -48,16 +48,50 @@ struct SEG {
     int s[maxn];
     int lc[maxn];
     int rc[maxn];
-    int n;
-    void f(){
-        REP(i,10){op(i)ope(lc[i])}
+    // int n;
+    int node_id=1;
+    Vi ver;
+    void pull(int w) {
+        s[w]=s[lc[w]]+s[rc[w]];
+    }
+    int _ud(int w,int l,int r,int u,int v) {
+        int nn=node_id++;
+        if(!w) w=nn;
+        lc[nn]=lc[w],rc[nn]=rc[w];
+        if(l==r) {
+            s[w]-=v;
+            return;
+        }
+        int m=l+r>>1;
+        if(u<=m) lc[nn]=_ud(lc[nn],l,m,u,v);
+        else rc[nn]=_ud(rc[nn],m+1,r,u,v);
+        pull(nn);
+    }
+    int ud(int root,int u,int v) {
+        return _ud(root,0,n-1,u,v);
     }
     void init(int _n,Vi &a) {
-
+        n=_n+1;
+        ver=Vi(_n+1);
+        ver[0]=1;
+        Vi las(n);
+        REP1(i,_n) {
+            ver[i]=ud(ud(ver[i-1],las[a[i]],-1),i,1);
+            las[a[i]]=i;
+        }
+    }
+    int _qu(int w,int l,int r,int ql,int qr) {
+        if(!w) return 0;
+        if(ql<=l&&r<=qr) return s[w];
+        if(ql>r||qr<l) return 0;
+        int m=l+r>>1;
+        return _qu(lc[w],l,m,ql,qr)+_qu(rc[w],m+1,r,ql,qr);
+    }
+    int qu(int l,int r) {
+        return _qu(ver[r],0,n-1,l,r);
     }
 }seg;
 signed main() {
     IOS(); 
-    seg.f();
     return 0;
 }
