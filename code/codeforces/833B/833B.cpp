e8f9c1a6b715ccc5aafe0060e0fa67d75c45f751
huangallen2008 <huangallen2008@gamil.con>
2024-09-16 15:35:59 +0000

    126016ing

--- code/codeforces/833B/833B.cpp
+++ code/codeforces/833B/833B.cpp
@@ -115,6 +115,7 @@ void _solve(int l,int r,int mn,int mx) {
 void solve() {
     _solve(1,n,0,n-1);
     dp.swap(ndp);
+    oparr(dp)
 }
 signed main() {
     IOS(); 
@@ -126,7 +127,6 @@ signed main() {
     sort(ALL(t));
     REP1(i,n) a[i]=lower_bound(ALL(t),a[i])-t.begin();
     seg.init(n,a);
-    ope(seg.qu(1,5))
     dp=ndp=Vi(n+1);
     REP(i,n) solve();
     cout<<dp[n]<<'\n';
