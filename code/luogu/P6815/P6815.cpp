336a942964bb1603e562497f448b5b01db2e7b11
huangallen2008 <huangallen2008@gamil.con>
2024-09-17 03:42:19 +0000

    226117ing

--- code/luogu/P6815/P6815.cpp
+++ code/luogu/P6815/P6815.cpp
@@ -62,8 +62,7 @@ signed main() {
         for(int v:g0[u]) if(g[u].size()<g[v].size()||(g[u].size()==g[v].size()&&u<v)) g[u].pb(v);
     }
     REP(u,n) {
-        op(u)oparr(g[u])
-        for(int v:g[u]) op(v)entr
+        for(int v:g[u]) {cout<<u<<' '<<v<<'\n';}
     }
     int an=0;
     Vi mark(n,-1);
