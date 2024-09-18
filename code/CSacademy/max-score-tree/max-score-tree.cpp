26bce297d54a4ecbf89f55d607082cc6071b60cc
huangallen2008 <huangallen2008@gamil.con>
2024-09-18 06:42:10 +0000

    326218ing

--- code/CSacademy/max-score-tree/max-score-tree.cpp
+++ code/CSacademy/max-score-tree/max-score-tree.cpp
@@ -75,7 +75,7 @@ signed main() {
         g[u].pb(v);
         g[v].pb(u);
     }
-    dp[0]=pd[1]=Vi(n);
+    dp[0]=dp[1]=Vi(n);
     dfs(0,-1);
     int an=*max_element(ALL(dp[0]));
     cout<<an<<'\n';
