d4e480b10c9924d141ccf2b84ccec92daa547c58
huangallen2008 <huangallen2008@gamil.con>
2024-09-18 06:40:00 +0000

    326218ing

---
+++ code/CSacademy/max-score-tree/max-score-tree.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=1e5+1;
+const int maxb=17;
+const int inf=(1ll<<50);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+int n;
+Graph g;
+Vi dp[2],a;
+void dfs(int u,int fa) {
+    Vi t={0};
+    for(int v:g[u]) {
+        if(v==fa) continue;
+        dfs(v,u);
+        t.pb(dp[1][v]);
+    }
+    sort(t.begin()+1,t.end(),greater<int>);
+    REP1(i,t.size()-1) {
+        t[i]+=t[i-1];
+    }
+    REP(i,t.size()) chmax(dp[0][u],t[i]+a[i]);
+    REP(i,t.size()-1) chmax(dp[1][u],t[i]+a[i+1]);
+}
+signed main() {
+    IOS(); 
+    cin>>n;
+    g=Graph(n);
+    a=Vi(n);
+    REP(i,n) cin>>a[i];
+    REP(i,n-1) {
+        int u,v;
+        cin>>u>>v,u--,v--;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    dfs(0,-1);
+    int an=*max_element(ALL(dp[0]));
+    cout<<an<<'\n';
+    return 0;
+}
