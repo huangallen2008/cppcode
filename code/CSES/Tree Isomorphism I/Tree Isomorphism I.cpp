373154875323cda139620b7af3dca9e649a16cb6
huangallen2008 <huangallen2008@gamil.con>
2024-09-21 17:11:20 +0000

    726622ing

--- code/CSES/Tree Isomorphism I/Tree Isomorphism I.cpp
+++ code/CSES/Tree Isomorphism I/Tree Isomorphism I.cpp
@@ -1,30 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
-#define REP(i,n) for(int i=0;i<(n);i++)
+#define L int16_t
+#define I int
+#define REP(i,n) for(I i=0;i<(n);i++)
 #define P push_back
-#define Graph vector<vector<int>>
-const int mod=1e9+9;
-const int maxn=5;
-const int inf=(1ll<<50);
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-int rd(int l,int r) {
-    return uniform_int_distribution<int>(l,r)(rng);
-}
-const int AL=666666;
-int f(Graph &g,int u,int fa) {
-    int ret=AL;
-    for(int v:g[u]) {
+#define Graph vector<vector<I>>
+const I X=666666,M=1e9+9;
+L f(Graph &g,I u,I fa) {
+    L r=X;
+    for(I v:g[u]) {
         if(v==fa) continue;
-        ret=ret*(AL+f(g,v,u))%mod;
+        r=r*(X+f(g,v,u))%M;
     }
-    return ret;
+    return r;
 }
 signed main() {
     int T;
     cin>>T;
     while(T--) {
-        int n;
+        I n;
         cin>>n;
         Graph g(n);
         REP(i,n-1) {
