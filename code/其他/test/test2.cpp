6cbfc1434c377edb61d821b12b67ce56194f2dcc
huangallen2008 <huangallen2008@gamil.con>
2024-09-22 15:27:03 +0000

    726622ing

--- code/其他/test/test2.cpp
+++ code/其他/test/test2.cpp
@@ -39,7 +39,7 @@ using namespace std;
 #define entr ;
 #endif
 const int mod=1e9+7;
-const int maxn=5;
+const int maxn=5e7;
 const int inf=(1ll<<50);
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
