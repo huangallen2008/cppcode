252912e626f55fd1277f3dcbe78c80514dfa0d54
huangallen2008 <huangallen2008@gamil.con>
2024-09-22 05:26:11 +0000

    726622ing

--- code/其他/test/tempCodeRunnerFile.cpp
+++ code/其他/test/tempCodeRunnerFile.cpp
@@ -1,2 +1,40 @@
+#pragma GCC optimize("Ofast,fast-math,unroll-loops,no-stack-protector")
+#include<bits/stdc++.h>
+#define int long long
+#define fastio ios_base::sync_with_stdio(0),cin.tie(0)
+using namespace std;
 
-    cout<<n
+
+int n;
+char c[30];
+void f(int t,int l,int r)
+{
+    if(t==2*n)
+    {
+        for(int i=0;i<2*n;i++)
+        {
+            cout<<c[i];
+        }
+        cout<<'\n';
+        return ;
+    }
+    if(l<n)
+    {
+        c[t]='(';
+        f(t+1,l+1,r);
+    }
+    if(r<l)
+    {
+        c[t]=')';
+        f(t+1,l,r+1);
+    }
+    return ;
+}
+signed main()
+{
+    while(cin>>n)
+    {
+        f(0,0,0);
+        cout<<'\n';
+    }
+}
