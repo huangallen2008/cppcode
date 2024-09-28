2cdb5d2338aa28096fd80094cc3e578eecafe0a7
huangallen2008 <huangallen2008@gamil.con>
2024-09-28 07:16:37 +0000

    627228ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -78,7 +78,7 @@ signed main() {
             cin>>p;
             cout<<a[p]<<'\n';
         }
-        REP1(i,n) cout<<a[i]<<' ';entr
+        // REP1(i,n) cout<<a[i]<<' ';entr
     }
     return 0;
 }
