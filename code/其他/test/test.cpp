0c5f54866c318f1f690ac3727e70fcd209765979
huangallen2008 <huangallen2008@gamil.con>
2024-09-29 17:26:28 +0000

    127430ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -45,6 +45,37 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+#ifdef LOCAL
+#define GC _getchar_nolock()
+#define PC _putchar_nolock
+#else 
+#define GC getchar_unlocked()
+#define PC putchar_unlocked
+#endif
+inline int read()
+{
+    int x=0;
+    bool neg=0;
+    char c=GC;
+    while(c<'0'||c>'9'){if(c=='-') neg=1;c=GC;}
+    while(c>='0'&&c<='9') x=(x<<3)+(x<<1)+(c^48),c=GC;
+    if(neg) x=-x;
+    return x;
+}
+inline void out(int x) {
+    if(x<0) {
+        PC('-');
+        x=-x;
+    }
+    char str[18];
+	auto it=str;
+    do { 
+        *it=x%10+'0',it++;
+        x/=10;
+    } while(x);
+    for(it--;it>=str;it--) PC(*it);
+    PC(',');PC(' ');
+}
 bitset<mod> isp;
 signed main() {
     IOS(); 
@@ -53,7 +84,7 @@ signed main() {
     isp[0]=isp[1]=0;
     for(int i=2;i<mod;i++) {
         if(isp[i]){
-            // cout<<i<<",";
+            out(i);
             for(int j=i+i;j<mod;j+=i) isp[j]=0;
         }
     }
