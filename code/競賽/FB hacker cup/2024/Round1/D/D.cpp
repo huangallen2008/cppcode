3a557fcbd816ac9626c5e9c4304ab15390a28692
huangallen <huangallen2008@gmail.com>
2024-10-05 18:46:14 +0000

    72806ing

--- code/競賽/FB hacker cup/2024/Round1/D/D.cpp
+++ code/競賽/FB hacker cup/2024/Round1/D/D.cpp
@@ -114,7 +114,7 @@ void solve(int C) {
 signed main() {
     IOS(); 
 
-    #ifdef LOCAL_
+    #ifdef LOCAL
         freopen("vin.txt","r",stdin);
         freopen("vout.txt","w",stdout);
     #endif
