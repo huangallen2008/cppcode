32814c74618a15e16bb40c292bedc154bdbeb877
huangallen <huangallen2008@gmail.com>
2024-10-05 17:09:52 +0000

    72806ing

---
+++ code/競賽/FB hacker cup/2024/Round1/A/A.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=5;
+const int maxv=1e6+5;
+const int inf=1e9+1;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+#ifdef LOCAL
+#define GC _getchar_nolock()
+#define PC _putchar_nolock
+#else 
+#define GC getchar_unlocked()
+#define PC putchar_unlocked
+#endif
+void solve() {
+    int n;
+    cin>>n;
+    vector<pii> a(n+1);
+    REP1(i,n) cin>>a[i].f>>a[i].s;
+    ld l=0,r=maxv,m;
+    int time=100;
+    while(time--) {
+        m=(l+r)/2;
+        bool ok=1;
+        REP1(i,n) if((ld)a[i].s<m*i) ok=0;
+        if(ok) l=m;
+        else r=m;
+    }
+    bool ok=1;
+    REP1(i,n) if((ld)a[i].f>l*i) ok=0;
+    if(ok) cout<<l<<'\n';
+    else cout<<"-1\n";
+}
+signed main() {
+    IOS(); 
+    #ifdef LOCAL_
+        freopen("in.txt"."r",stdin);
+        freopen("out.txt"."w",stdout);
+    #endif
+    int T;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
