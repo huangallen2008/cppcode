7618bedf0efdfa2fe8214273fc4f7508e2031ab2
huangallen <huangallen2008@gmail.com>
2024-10-06 15:32:08 +0000

    72806ing

--- code/競賽/IOIC pretest/2025/E.cpp
+++ code/競賽/IOIC pretest/2025/E.cpp
@@ -73,7 +73,7 @@ struct DSU {
         return p[u]==u?u:p[u]=find(p[u]);
     }
     void merge(int a,int b) {
-        op(a)op(b)
+        op(a)ope(b)
         int x=find(a),y=find(b);
         if(x==y) return;
         if(sz[x]>sz[y]) swap(x,y);
