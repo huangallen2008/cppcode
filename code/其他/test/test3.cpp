b4a0d998fbe1e2595e0bf920212fc7bb466d002d
huangallen <huangallen2008@gmail.com>
2024-10-09 17:38:15 +0000

    428410ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -55,13 +55,13 @@ T sum(T a,arg ... args) {
 struct S {
     // template<typename T> cmp(T a,T b) { return a<b; }
     cmp(int a,int b) { return a<b; }
-}ss;
+}*ss;
 signed main() {
     IOS(); 
     int n=10;
     Vi v(n);
     REP(i,n) v[i]=rd(0,n);
-    sort(ALL(v),ss.*cmp);
+    sort(ALL(v),*ss.cmp);
     oparr(v)
     return 0;
 }
