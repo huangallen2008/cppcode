25e1069b4aec110c1b05b892bea661d5290c2cd4
huangallen <huangallen2008@gmail.com>
2024-10-09 18:17:26 +0000

    428410ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-template<int T> int c=(T<=1?T:0ll)+c<max(T-1,0ll)>+c<max(T-2,0ll)>;
+template<int T> int c=(T<=1?T:0ll)+c<max(T-1,0ll)>+c<max(T-3,0ll)>;
 main() {
     cout<<c<3>;
     cout<<c<1><<endl;
