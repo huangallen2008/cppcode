6dd8ad67b11bb8bdaf8f21c31ad3222a151cddcc
huangallen <huangallen2008@gmail.com>
2024-10-09 18:00:39 +0000

    428410ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -1,6 +1,8 @@
-#include<bits/stdc++.h>
-using namespace std;
-template<int T> int *c=&c<max(T-1,0)>;
-main() {
-    c<890>;
-}
+#include __FILE__
+#include __FILE__
+// #include<bits/stdc++.h>
+// using namespace std;
+// template<int T> int *c=&c<max(T-1,0)>;
+// main() {
+//     c<890>;
+// }
