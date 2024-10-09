28f921a9c61e3019532723313fe970aff7a83735
huangallen <huangallen2008@gmail.com>
2024-10-09 17:52:30 +0000

    428410ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -59,7 +59,7 @@ struct S {
     int b=9;
     int *n=&b;
     // template<int T> int c=(T==1?c<T-1>:5);
-    template<int T> int c=(T==1?c<T/2>:5);
+    template<int T> int c=(T==1?c<max(T-1,0)>:5);
 signed main() {
     IOS(); 
     ope(c<0>);
