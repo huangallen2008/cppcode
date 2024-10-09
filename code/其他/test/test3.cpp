dc68d5ef077df72287a9b2cb752c3bf897949c47
huangallen <huangallen2008@gmail.com>
2024-10-09 17:07:16 +0000

    428410ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -1,87 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#define int long long int
+#include<bits/stdc++.h>
 using namespace std;
-
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
 signed main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    
-    map<int, int> v;
-    map<int, int> mp;
-    mp[0] = 0;
-    mp[n+1] = 0;
-    while (m--) {
-        int q, l, r, x;
-        long long sum = 0;
-        cin >> q;
-
-        if (q == 1) {
-            cin >> l >> r >> x;
-            auto it = mp.lower_bound(r+1);
-            if(it->first != r+1)
-                --it;
-            int value = it -> second;
-            //cout<<it->first<<' '<<value<<endl;
-            auto start = mp.upper_bound(l);
-            auto end = mp.lower_bound(r+1);
-            mp.erase(start, end);
-            mp.insert_or_assign(r+1,value);
-            mp.insert_or_assign(l,x);
-            /*for(const auto& pair:mp)
-            {
-                cout<<"("<<pair.first<<","<<pair.second<<"), ";
-            }
-            cout<<endl;*/
-        } else {
-            cin >> l >> r;
-            int index;map<int,int> newmap;
-            auto start = mp.lower_bound(l);
-            if(start->first != r+1){
-                --start;
-                newmap[start->second] = 0;
-                index = start -> second;
-                ++start;
-            }
-            else{
-                newmap[start->second] = 0;
-                index = start -> second;
-            }
-            auto end = mp.upper_bound(r);
-            auto it = mp.lower_bound(r+1);
-            if(it->first != r+1)
-                --it;
-            int value = it -> second;
-            //cout<<it->first<<' '<<value<<endl;
-            
-            for(int i=l;i<=r;i++)
-            {
-                auto search = mp.find(i);
-                if (search != mp.end()) {
-                    index = search->second;
-                    newmap[index]++;
-                } else {
-                    newmap[index]++;
-                }
-            }
-            mp.erase(start, end);
-            mp.insert_or_assign(r+1,value);
-            mp.insert_or_assign(l,0);
-            for(const auto& pair:newmap)
-            {
-                int v = pair.second;
-                if(pair.first)
-                    sum+=v*v;
-            }
-            cout << sum << endl;
-        }
-            for(const auto& pair:mp)
-            {
-                cout<<"("<<pair.first<<","<<pair.second<<"), ";
-            }
-            cout<<endl;
-    }
+    IOS(); 
+    int n=10;
+    Vi v(n);
+    REP(i,n) v[i]=rd(0,n-1);
+    auto cmp=[&](auto a,auto b) {
+        return a<b;
+    };
+    sort(ALL(v),cmp);
+    oparr(v)
     return 0;
 }
