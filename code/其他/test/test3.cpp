23a6b8204629a46d27864bc0278284c30bb17d1a
huangallen <huangallen2008@gmail.com>
2024-10-09 18:13:51 +0000

    428410ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-template<int T> int c=(T==0?1:0)+c<max(T-1,0)>+c<max(T-2,0)>);
+template<int T> int c=(T==0?1:0)+c<max(T-1,0)>+c<max(T-2,0)>;
 main() {
     cout<<c<3>;
 }
