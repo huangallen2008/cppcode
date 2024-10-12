b80fd413ed279c06ce6b6362b7c2270c4ced806b
huangallen <huangallen2008@gmail.com>
2024-10-12 17:36:45 +0000

    728713ing

---
+++ code/TIOJ/2311/2311_small.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,sse4,sse4.1,sse4.2,bmi,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
+const int inf=1ll<<62;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+signed main() {
+    IOS(); 
+    int n,m,q;
+    cin>>n>>q>>m;
+    Vi a(n);
+    REP(i,n) cin>>a[i],a[i]=-a[i];
+    seg.init(n,m,a);
+    REP(i,q) {
+        int opt;
+        cin>>opt;
+        if(opt==1) {
+            int p,x;
+            cin>>p>>x,p--;
+            a[p]=-x;
+        }
+        else {
+            int l,r,y;
+            cin>>l>>r>>y,l--,r--,y=min(y,m);
+            int an=0;
+            for(int j=l;j<=r;j++) {
+                y=min(y+a[j],m);
+                if(y<0) an-=y,y=0;
+            }
+            cout<<an<<' '<<y<<'\n';
+        }
+    }
+    return 0;
+}
