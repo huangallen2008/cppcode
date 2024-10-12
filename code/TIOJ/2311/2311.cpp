48b0d5e293a9568e9bd9c252b26a674904361b49
huangallen <huangallen2008@gmail.com>
2024-10-12 14:51:32 +0000

    628612ing

--- code/TIOJ/2311/2311.cpp
+++ code/TIOJ/2311/2311.cpp
@@ -49,8 +49,8 @@ struct SEG {
     int n,m;
     vector<pii> s;
     pii merge(pii b,pii c) {
-        if(b.s+c.f<0) return pii{a.f+b.s+c.f,c.s};
-        else return {a.f,a.s+b.f+b.s};
+        if(b.s+c.f<0) return {b.f+b.s+c.f,c.s};
+        else return {b.f,b.s+c.f+c.s};
     }
     void pull(pii &a,pii &b,pii &c) {
         a=merge(b,c);
