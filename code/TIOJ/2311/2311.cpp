2b9021356a9f93dbf277812f77b0f8f67c153b5b
huangallen <huangallen2008@gmail.com>
2024-10-12 17:10:34 +0000

    728713ing

--- code/TIOJ/2311/2311.cpp
+++ code/TIOJ/2311/2311.cpp
@@ -93,7 +93,7 @@ struct SEG {
         for(int i=mx2+1;i<=7;i++) a[3]+=d0[i];
         chmin(a[0],m);
         chmin(a[2],m);
-        op(mx1)op(rid)op(mx2)
+        oparr(d0)op(mx1)op(rid)op(mx2)
         oparr(a.x)
         return a;
     }
