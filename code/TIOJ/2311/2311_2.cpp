8b6e90c747df5bcb3be943fc1f84d6a23be02702
huangallen <huangallen2008@gmail.com>
2024-10-13 04:10:48 +0000

    728713ing

--- code/TIOJ/2311/2311_2.cpp
+++ code/TIOJ/2311/2311_2.cpp
@@ -63,11 +63,7 @@ struct SEG {
         pii ret=c.cal(ret0.s);
         a.c0=ret0.f+ret.f,a.y0=ret.s;
         a.x1=b.x1+max(min(b.x2-b.x1,c.x1-ret0.s),0ll);
-        // a.x1=b.x1+max(0ll,c.x1-ret0.s);
         a.x2=b.x2-max(0ll,min(b.x2-b.x1,ret2.s-c.x2));
-        op(b.x1)op(b.x2)op(b.c0)ope(b.y0)
-        op(c.x1)op(c.x2)op(c.c0)ope(c.y0)
-        op(a.x1)op(a.x2)op(a.c0)ope(a.y0)
         return a;
     }
     void pull(Seg &a,Seg &b,Seg &c) {
