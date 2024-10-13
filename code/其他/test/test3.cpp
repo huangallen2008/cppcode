09d76f14fac48857d9487245bb946790043b33c7
huangallen <huangallen2008@gmail.com>
2024-10-13 13:03:12 +0000

    728713ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-// #pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
 // #pragma GCC target("avx,avx2,sse4,sse4.1,sse4.2,bmi,bmi2,popcnt")
 // #define int long long
 // #define ll long long
@@ -46,19 +46,50 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+#ifdef LOCAL
+#define GC _getchar_nolock()
+#define PC _putchar_nolock
+#else 
+#define GC getchar_unlocked()
+#define PC putchar_unlocked
+#endif
+inline int read()
+{
+    int x=0;
+    bool neg=0;
+    char c=GC;
+    while(c<'0'||c>'9'){if(c=='-') neg=1;c=GC;}
+    while(c>='0'&&c<='9') x=(x<<3)+(x<<1)+(c^48),c=GC;
+    if(neg) x=-x;
+    return x;
+}
+inline void out(int x) {
+    if(x<0) {
+        PC('-');
+        x=-x;
+    }
+    char str[18];
+	auto it=str;
+    do { 
+        *it=x%10+'0',it++;
+        x/=10;
+    } while(x);
+    for(it--;it>=str;it--) PC(*it);
+    PC(' ');
+}
 bitset<maxn<<1> b;
 signed main() {
     IOS(); 
-    int n;
-    cin>>n;
+    int n=read();
+    // cin>>n;
     REP(i,n) {
-        int x;
-        cin>>x;
+        int x=read();
+        // cin>>x;
         if(!b[x+maxn]) {
             b[x+maxn]=1;
-            cout<<x<<' ';
+            out(x);
         }
     }
-    cout<<'\n';
+    PC('\n');
     return 0;
 }
