4f8a8fed1cd8ba4ac533b1e4849585299fa1897d
huangallen <huangallen2008@gmail.com>
2024-10-13 13:00:53 +0000

    728713ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -1,9 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
-template<int T> int c=(T<=1?T:0ll)+c<max(T-1,0ll)>+c<max(T-3,0ll)>;
-main() {
-    cout<<c<3>;
-    cout<<c<1><<endl;
-    cout<<c<20>;
+// #pragma GCC optimize("Ofast,unroll-loops,fast-math")
+// #pragma GCC target("avx,avx2,sse4,sse4.1,sse4.2,bmi,bmi2,popcnt")
+// #define int long long
+// #define ll long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=1e6+1;
+const int maxb=20;
+const ll inf=1ll<<62;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+bitset<maxn<<1> b;
+signed main() {
+    IOS(); 
+    int n;
+    cin>>n;
+    REP(i,n) {
+        int x;
+        cin>>x;
+        if(!b[x]) {
+            b[x]=1;
+            cout<<x<<' ';
+        }
+    }
+    cout<<'\n';
+    return 0;
 }
