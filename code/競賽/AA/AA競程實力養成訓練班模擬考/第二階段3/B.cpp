beee00c32a64976420b0f9ea675cb7996f91e2ee
huangallen <huangallen2008@gmail.com>
2024-10-16 11:28:43 +0000

    329016ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/B.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/B.cpp
@@ -85,7 +85,7 @@ signed main() {
         else {
             an=bit.qu(las[a[i]],i);
         }
-        ope(i)
+        ope(i)ope(las[a[i]])
         bit.ud(las[a[i]],-1);
         las[a[i]]=i;
         bit.ud(i,1);
