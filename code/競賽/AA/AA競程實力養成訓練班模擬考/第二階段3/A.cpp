c36fbade90e7437d3e505e134c7d1bab3d916b09
huangallen <huangallen2008@gmail.com>
2024-10-16 13:08:47 +0000

    329016ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/A.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/A.cpp
@@ -93,7 +93,7 @@ void solve() {
         op(f1)op(f2)op(x)ope(y)
         // int r1=(n-x*f1)/f2+1,r2=(n+1-y*f2)/f1+1,rr=min(r1,r2);
         // if(x*f1>n||y*f2>n+1) rr=0;
-        int r21=(n+1-x*f1)/f2+1,r22=(n-y*f2)/f1+1,rr2=min(r21,r22);
+        int r21=((n+1)/f1-x)/f2+1,r22=((n/f2)-y)/f1+1,rr2=min(r21,r22);
         if(x*f1>n+1||y*f2>n) rr2=0;
         an+=rr2;
         ope(rr2)
