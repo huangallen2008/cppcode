2908c0d76f1ac857fb26fd2895ffa6bb7ee313a6
huangallen <huangallen2008@gmail.com>
2024-10-16 11:24:01 +0000

    329016ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/B.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/B.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #include<bits/extc++.h>
 using namespace __gnu_pbds;
-using pbds = tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>;
+using TREE = tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>;
 using namespace std;
 // #pragma GCC optimize("O3,unroll-loops,fast-math")
 // #pragma GCC target("avx2,sse4,bmi,popcnt")
