9632349a5ff3b5a1c036818fa45a5dbd11d08f68
huangallen <huangallen2008@gmail.com>
2024-10-16 13:09:14 +0000

    329016ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/A.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第二階段3/A.cpp
@@ -90,16 +90,16 @@ void solve() {
         }
         y=-y;
         if(x==0||y==0) x+=f2,y+=f1;
-        op(f1)op(f2)op(x)ope(y)
+        // op(f1)op(f2)op(x)ope(y)
         // int r1=(n-x*f1)/f2+1,r2=(n+1-y*f2)/f1+1,rr=min(r1,r2);
         // if(x*f1>n||y*f2>n+1) rr=0;
         int r21=((n+1)/f1-x)/f2+1,r22=((n/f2)-y)/f1+1,rr2=min(r21,r22);
         if(x*f1>n+1||y*f2>n) rr2=0;
         an+=rr2;
-        ope(rr2)
+        // ope(rr2)
     }
     cout<<an<<'\n';
-    entr
+    // entr
 }
 void init() {
     lpf[0]=lpf[1]=1;
