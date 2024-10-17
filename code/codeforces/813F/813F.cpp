20f973c954b6083c6b9c280a9cfd0b730de89e02
huangallen <huangallen2008@gmail.com>
2024-10-17 12:23:51 +0000

    429117ing

--- code/codeforces/813F/813F.cpp
+++ code/codeforces/813F/813F.cpp
@@ -156,7 +156,7 @@ signed main() {
     vector<edge> es;
     REP1(i,q) {
         edge e;
-        cin>>e.f>>e.s;
+        cin>>e.u>>e.v;
         es.pb(e);
         int t=id(e);
         if(las[t]==0) {
