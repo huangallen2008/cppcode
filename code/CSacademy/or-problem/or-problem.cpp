acfedb5d4840e366b84b2a6f6b4c54c8cf9e28ea
huangallen <huangallen2008@gmail.com>
2024-10-19 03:39:58 +0000

    629319ing

--- code/CSacademy/or-problem/or-problem.cpp
+++ code/CSacademy/or-problem/or-problem.cpp
@@ -96,13 +96,12 @@ signed main() {
     a=Vi(n+1);
     REP1(i,n) cin>>a[i];
     st_init();
-    int l=-1e10,r=1e10,mid;
-    REP(i,10) check(i);
-    // while(l<r) {
-    //     mid=l+r+1>>1;
-    //     if(check(mid).s>k) l=mid;
-    //     else r=mid-1;
-    // }
-    // cout<<check(l).f+k*l<<'\n';
+    int l=0,r=1e10,mid;
+    while(l<r) {
+        mid=l+r+1>>1;
+        if(check(mid).s>k) l=mid;
+        else r=mid-1;
+    }
+    cout<<check(l).f+k*l<<'\n';
     return 0;
 }
