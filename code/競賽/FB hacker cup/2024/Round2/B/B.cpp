f3d4500f1281dbf07dce0491786c0ffef184e357
huangallen <huangallen2008@gmail.com>
2024-10-19 18:18:58 +0000

    729420ing

--- code/競賽/FB hacker cup/2024/Round2/B/B.cpp
+++ code/競賽/FB hacker cup/2024/Round2/B/B.cpp
@@ -105,6 +105,7 @@ void solve(int C) {
                     int id=0;
                     for(int k=6;k>=0;k--) id=id*7+x[k];
                     pos[id]=pos[i]^1;
+                    ope(id)
                     x[j]--;
                 }
             }
