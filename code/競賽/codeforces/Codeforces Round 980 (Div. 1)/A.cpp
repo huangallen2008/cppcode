2e3d1b0c384dbf8dde18253a1b710fba3f7db3c0
huangallen <huangallen2008@gmail.com>
2024-10-20 09:18:05 +0000

    729420ing

--- code/競賽/codeforces/Codeforces Round 980 (Div. 1)/A.cpp
+++ code/競賽/codeforces/Codeforces Round 980 (Div. 1)/A.cpp
@@ -50,7 +50,6 @@ void solve() {
     cin>>n;
     vector<pii> a(n);
     REP(i,n) cin>>a[i].f>>a[i].s;
-    // REP(i,n) if(a[i].f>a[i].s) swap(a[i].f,a[i].s);
     sort(ALL(a),[&](pii a,pii b) {
         return (a.f>b.f)+(a.f>b.s)+(a.s>b.f)+(a.s>b.s)<(a.f<b.f)+(a.f<b.s)+(a.s<b.f)+(a.s<b.s);
     });
