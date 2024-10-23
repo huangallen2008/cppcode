2c6e166fd9f0e015c91450cbda75ab8c1c2f725c
huangallen <huangallen2008@gmail.com>
2024-10-23 12:40:50 +0000

    329723ing

---
+++ code/CSacademy/and-or-max/and-or-max.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+struct SEG {
+    struct Seg {
+        int mx,no;
+    };
+    void pull(Seg &a,Seg &b,Seg &c) {
+        a.mx=max(b.mx,c.mx);
+        a.no=b.no&c.no&(b.mx^c.mx^all);
+    }
+    int n;
+    const int all=1<<20;
+    vector<Seg> s;
+    void build(int w,int l,int r,Vi &a) {
+        if(l==r) {
+            s[w]={s[l],0};
+            return;
+        }
+        int m=l+r>>1;
+        build(w<<1,l,m,a);
+        build(w<<1|1,m+1,r,a);
+        pull(s[w],s[w<<1],s[w<<1|1]);
+    }
+    void init(int _n,Vi a) {
+        n=_n;
+        s=vector<Seg>(n<<2);
+        build(1,0,n-1);
+    }
+    void _and(int w,int l,int r,int ql,int qr,int v) {
+        if(ql<=l&&r<=qr&&((s[w].no|(v^all))==s[w].no)) {
+            s[w].mx&=v;
+            return;
+        }
+        if(ql>r||qr<l) return;
+        int m=l+r>>1;
+        _and(w<<1,l,m,ql,qr,v);
+        _and(w<<1|1,m+1,r,ql,qr,v);
+        pull(s[w],s[w<<1],s[w<<1|1]);
+    }
+    void and(int l,int r,int v) {
+        _and(1,0,n-1,l,r,v);
+    }
+    void _or(){}
+};
+signed main() {
+    IOS(); 
+
+    return 0;
+}
