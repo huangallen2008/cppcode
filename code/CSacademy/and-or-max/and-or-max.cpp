e7c448a0e93c0ca57b2b3aa1bfe5ec4756700633
huangallen <huangallen2008@gmail.com>
2024-10-23 12:48:49 +0000

    329723ing

--- code/CSacademy/and-or-max/and-or-max.cpp
+++ code/CSacademy/and-or-max/and-or-max.cpp
@@ -134,7 +134,7 @@ signed main() {
             cin>>l>>r,l--,r--;
             cout<<seg.mx(l,r)<<'\n';
         }
-        REP(j,n) cout<<seg.mx(j,j)<<' ';entr
+        cout<<"a:";REP(j,n) cout<<seg.mx(j,j)<<' ';entr
     }
     return 0;
 }
