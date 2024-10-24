d4385ad285c0b010b697c73a1673c39116c62afc
huangallen <huangallen2008@gmail.com>
2024-10-24 15:56:55 +0000

    429824ing

--- code/CSacademy/subarrays-xor-sum/subarrays-xor-sum.cpp
+++ code/CSacademy/subarrays-xor-sum/subarrays-xor-sum.cpp
@@ -47,6 +47,7 @@ int rd(int l,int r) {
 }
 signed main() {
     IOS(); 
+    ope(5>>1&1)
     int n,L,R;
     cin>>n>>L>>R;
     Vi a(n+1);
@@ -54,7 +55,7 @@ signed main() {
     vector<Vi> x[2],p[2],px[2];
     x[0]=x[1]=p[0]=p[1]=px[0]=px[1]=vector<Vi>(30,Vi(n+1));
     REP1(i,n) {
-        REP(j,30) x[0][j][i]=(a[i]>>j)&1,x[1][j][i]=x[0][j][i]^1,px[0][j][i]=px[0][j][i-1]^x[0][j][i],px[1][j][i]=px[1][j][i-1]^x[1][j][i],p[0][j][i]=p[0][j][i-1]+px[0][j][i],p[1][j][i]=p[1][j][i-1]+(px[0][j][i]^1);
+        REP(j,30) x[0][j][i]=a[i]>>j&1,px[0][j][i]=px[0][j][i-1]^x[0][j][i],p[0][j][i]=p[0][j][i-1]+px[0][j][i],p[1][j][i]=p[1][j][i-1]+(px[0][j][i]^1);
     }
     int an=0;
     REP1(i,n) {
