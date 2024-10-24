e90313fb4e7064df7698263c7ff1da48a54604c1
huangallen <huangallen2008@gmail.com>
2024-10-24 13:27:44 +0000

    429824ing

--- code/CSacademy/increasing_subarrays/increasing_subarrays.cpp
+++ code/CSacademy/increasing_subarrays/increasing_subarrays.cpp
@@ -153,7 +153,9 @@ signed main() {
     RREP1(i,n) {
         int ret=seg2.qu(a[i]);
         seg.ud(i,ret-1,a[i]);
+        op(i)op(ret-1)ope(a[i])
         an+=seg.qu(m)-i;
+        op(i)ope(seg.qu(m))
         seg2.ud(i,a[i]);
     }
     cout<<an<<'\n';
