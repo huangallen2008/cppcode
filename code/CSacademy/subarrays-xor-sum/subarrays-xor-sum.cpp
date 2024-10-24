1435d6eefbed1b8cda67df2e366d204cbd092db5
huangallen <huangallen2008@gmail.com>
2024-10-24 15:54:14 +0000

    429824ing

--- code/CSacademy/subarrays-xor-sum/subarrays-xor-sum.cpp
+++ code/CSacademy/subarrays-xor-sum/subarrays-xor-sum.cpp
@@ -61,10 +61,7 @@ signed main() {
         REP(j,30) {
             int b=px[0][j][i-1];
             an+=p[b][j][min(i+R-1,n)]-p[b][j][min(i+L-2,n)]<<j;
-            // if(j<7){op(i)ope(an)op(b)oparr(p[b][j])}
-            if(j<4){op(b)op(j)oparr(p[b][j])}
         }
-        ope(an)
     }
     cout<<an<<'\n';
     return 0;
