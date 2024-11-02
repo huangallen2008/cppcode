d69ada321b52035959205a8f0e7ea4af64e5fc4c
huangallen <huangallen2008@gmail.com>
2024-11-02 15:36:41 +0000

    63072ing

---
+++ code/CSacademy/substring-restrictions/substring-restrictions.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("Ofast,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define ALL1(x) (x).begin()+1,(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=1e5+5;
+const int maxn2=2e5+5;
+const int maxb=20;
+const int inf=(1ll<<62); 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+struct DSU {
+    int n,cc;
+    Vi p,sz;
+    void init(int _n) {
+        n=_n;
+        cc=n;
+        p=Vi(n);
+        sz=Vi(n,1);
+        REP(i,n) p[i]=i;
+    }
+    int find(int u) {
+        return p[u]==u?u:p[u]=find(p[u]);
+    }
+    void merge(int a,int b) {
+        int x=find(a),y=find(b);
+        if(x==y) return;
+        if(sz[x]>sz[y]) swap(x,y);
+        p[x]=y;
+        sz[y]+=sz[x];
+        cc--;
+    }
+    int get_cc() { return cc; }
+};
+vector<DSU> dsu(maxb);
+signed main() {
+    IOS(); 
+    int n,m;
+    cin>>n>>m;
+    REP(i,maxb) dsu[i].init(n);
+    REP(i,m) {
+        int l,x,y;
+        cin>>l>>x>>y,x--,y--;
+        int lg=__lg(l);
+        dsu[lg].merge(x,y);
+        dsu[lg].merge(x+(1<<lg),y+(1<<lg));
+    }
+    RREP(i,maxb-1) {
+        REP(j,n) if(dsu[i+1].p[j]!=j) {
+            int x=dsu[i+1].p[j],y=j;
+            dsu[i].merge(x,y);
+            dsu[i].merge(x+(1<<i),y+(1<<i));
+        }
+    }
+    cout<<dsu[0].get_cc()<<'\n';
+    return 0;
+}
