f1de6ba7f924cb31c6ed904561db239df4272524
huangallen <huangallen2008@gmail.com>
2024-11-02 16:56:00 +0000

    73083ing

--- code/CSacademy/random_nim_generator/random_nim_generator.cpp
+++ code/CSacademy/random_nim_generator/random_nim_generator.cpp
@@ -73,6 +73,7 @@ signed main() {
     cin>>n>>k;
     int r=0;
     int x=__lg(k+1),y=k-(1<<x);
+    op(x)ope(y)
     for(int i=0;i<=n;i+=2) {
         addmod(r,pw(y+1,i)*(i==n?1:pw(2,x*(n-i-1)))*C(n,i));
     }
