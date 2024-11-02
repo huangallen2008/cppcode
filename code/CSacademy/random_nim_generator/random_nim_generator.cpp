5e340ebae320ccb2cb4728124c47ba037c1e236a
huangallen <huangallen2008@gmail.com>
2024-11-02 17:49:01 +0000

    73083ing

--- code/CSacademy/random_nim_generator/random_nim_generator.cpp
+++ code/CSacademy/random_nim_generator/random_nim_generator.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 // #pragma GCC optimize("Ofast,unroll-loops,fast-math")
 // #pragma GCC target("avx2,sse4,bmi,popcnt")
-#define int long long
+// #define int long long
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
