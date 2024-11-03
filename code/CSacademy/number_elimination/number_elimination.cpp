7ac2ae4cfeed3e744338b55d65ee6e0fb71840ad
huangallen <huangallen2008@gmail.com>
2024-11-03 08:07:42 +0000

    73083ing

--- code/CSacademy/number_elimination/number_elimination.cpp
+++ code/CSacademy/number_elimination/number_elimination.cpp
@@ -93,7 +93,7 @@ signed main() {
     // t.resize(unique(ALL(t))-t.begin());
     for(auto [x,y]:mp) {
         if(y>=2) {
-            an=((an*(t[0]==x?(y-1):H((lower_bound(ALL(t),x)-t.begin())+2,y-1)))%mod)*inv(y-1)%mod;
+            an=((an*(t[0]==x?1:H((lower_bound(ALL(t),x)-t.begin())+2,y-1)*inv(y-1)%mod))%mod);
             op(x)op(y)op(H(lower_bound(ALL(t),x)-t.begin()+2,y-1))ope(an)
         }
     }
