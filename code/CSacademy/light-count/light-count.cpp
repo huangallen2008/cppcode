625e01392c0f1379be9f26b7beead9d2fefe3f35
huangallen <huangallen2008@gmail.com>
2024-11-10 07:39:40 +0000

    731510ing

--- code/CSacademy/light-count/light-count.cpp
+++ code/CSacademy/light-count/light-count.cpp
@@ -111,7 +111,7 @@ int pre(int u) {
     u1>>=maxb;
     // op(u)op(u1)ope(r)
     for(;u1>0;u1-=lowbit(u1)) r+=b[u1];
-    // if(1){op(u)op(u1)ope(r)}
+    if(r<0){op(u)op(u1)ope(r)}
     // ope("op2")
     return r;
 }
