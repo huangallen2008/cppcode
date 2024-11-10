30453257cfd4ed862650ce93f1cbe7ea1db03717
huangallen <huangallen2008@gmail.com>
2024-11-10 07:25:36 +0000

    731510ing

--- code/CSacademy/light-count/light-count.cpp
+++ code/CSacademy/light-count/light-count.cpp
@@ -85,7 +85,7 @@ void flipPosition(int u) {
         cout<<u<<" ty+ ";
         for(;u<=maxnb;u+=lowbit(u)) {
             // cout<<u<<' ';
-            ope(u)
+            // ope(u)
             b[u]++;
         }
     }
