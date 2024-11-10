7fd6bc0fb003dfa01ca383ac0ced65e15c7ff2c5
huangallen <huangallen2008@gmail.com>
2024-11-10 07:43:04 +0000

    731510ing

--- code/CSacademy/light-count/light-count.cpp
+++ code/CSacademy/light-count/light-count.cpp
@@ -121,7 +121,7 @@ int getCount(int st, int fn) {
     // st++,fn++;
     st+=1<<maxb,fn+=1<<maxb;
     int ret=pre(fn)-pre(st-1);
-    if(ret<0)cout<<st<<' '<<fn<<' '<<ret<<'\n';
+    if(ret<0)cout<<st<<' '<<fn<<' '<<ret<<' '<<pre(fn)<<' '<<pre(st-1)<<'\n';
     return ret;
 }
 int main() {
