ef79239fdf48ab318171aa3229f153c0e982118e
huangallen <huangallen2008@gmail.com>
2024-11-10 16:06:29 +0000

    131611ing

--- code/競賽/codeforces/Codeforces Round 986 (Div. 2)/C.cpp
+++ code/競賽/codeforces/Codeforces Round 986 (Div. 2)/C.cpp
@@ -68,7 +68,7 @@ void solve() {
     // REP1(i,n) pdp[i]=pdp[i-1]+dp[i];
     oparr(p)oparr(dp)oparr(rdp)
     // oparr(pdp)
-    int an=0;
+    int an=-1;
     REP1(i,n) {
         int rcnt=rdp[i+1];
         int lid=lower_bound(ALL(dp),m-rcnt)-dp.begin();
