716b8925f3bdfb96ba92daa2640806c58e34c079
huangallen <huangallen2008@gmail.com>
2024-11-11 17:19:08 +0000

    231712ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -75,7 +75,7 @@ signed main() {
         int na=0;
         REP1(i,n-1){
             if(a[i]*(i)>na*n) na++;
-            if(i==300) ope(na)
+            if(i==500) ope(na)
         }
         ope(na)
     }
