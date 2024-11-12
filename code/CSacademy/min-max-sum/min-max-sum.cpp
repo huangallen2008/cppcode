78536c4fee75c6f24e737565f0e39cded76fd59b
huangallen <huangallen2008@gmail.com>
2024-11-12 15:01:18 +0000

    231712ing

--- code/CSacademy/min-max-sum/min-max-sum.cpp
+++ code/CSacademy/min-max-sum/min-max-sum.cpp
@@ -140,7 +140,7 @@ signed main() {
     vector<pii> mx,mn; 
     mx.pb({inf,-1});
     mn.pb({-inf,-1});
-    seg.ud(0,n-1,pw(2,n-1));
+    seg.ud(0,n-1,pw(2,n-3));
     REP1(i,n-2) {
         while(mx.back().f<a[i]) {
             if(mx.size()>=2)seg.ud(mx[mx.size()-2].s+1,mx.back().s,a[i]*inv(mx.back().f)%mod );
