f52dc4be948aa51884f30f7a38370cdbd3c7acb0
huangallen <huangallen2008@gmail.com>
2024-11-13 12:10:20 +0000

    331813ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段1/C.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段1/C.cpp
@@ -75,7 +75,7 @@ signed main() {
     Vi nok;
     REP(i,n) if(bas[i]==0) nok.pb(i);
     int cnt=0;
-    oparr(ok)
+    oparr(ok)oparr(nok)
     REP(i,n) if(!ok[i]) a[i].flip(nok[cnt++]);
     if(T==1) {
         cout<<nok.size()<<'\n';
