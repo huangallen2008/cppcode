1edd3bc57b0e3d122270f8d14232ae721beb5243
huangallen <huangallen2008@gmail.com>
2024-11-19 11:59:12 +0000

    232419ing

--- code/CSES/Planets Queries II/Planets Queries II.cpp
+++ code/CSES/Planets Queries II/Planets Queries II.cpp
@@ -1,57 +1,60 @@
+#include<bits/extc++.h>
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi,popcnt")
 #define int long long
-#define FOR(i,a,b) for(int i=a;i<b;i++)
-#define REP(i,n) FOR(i,0,n)
-#define REP1(i,n) FOR(i,1,(n)+1)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
 #define f first
 #define s second
 #define pb push_back
-#define ALL(x) x.begin(),x.end()
-#define SZ(x) (int)(x.size())
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
 #define SQ(x) (x)*(x)
 #define pii pair<int,int>
+#define pipii pair<int,pii>
 #define Graph vector<vector<int>>
-#define IOS() cin.sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto &mken:(x)) cout<<mken<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
 const int inf=(1ll<<62);
-const int maxn=1e6+5;
-const int mod=1e9+9;
-signed main()
-{
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+signed main() {
     IOS();
     int n,q;
     cin>>n>>q;
-    vector<int> t(n),ind(n);
-    REP(i,n) {
-        cin>>t[i];
-        t[i]--;
-        ind[t[i]]++;
-    }
-    queue<int> qu;
-    REP(i,n) if(ind[i]==0) qu.push(i);
-    while(qu.size()) {
-        int u=qu.front();
-        qu.pop();
-        ind[t[u]]--;
-        if(ind[t[u]]==0) qu.push(t[u]);
-    }
-    vector<bool> inc(n);
-    REP(i,n) if(ind[i]>0) inc[i]=1;
-    vector<int> cid(n,-1);
-    int ccnt=0;
-    REP(i,n) {
-        if(inc[i]&&cid[i]==-1) {
-            cid[i]=ccnt;
-            int x=t[i];
-            while(x!=i) {
-                cid[x]=ccnt;
-                x=t[x];
-            }
-            ccnt++;
-        }
-    }
-
+    Vi f(n);
+    REP(i,n) cin>>f[i],f[i]--;
+    
     return 0;
 }
