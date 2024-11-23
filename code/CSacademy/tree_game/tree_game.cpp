11755dfd8665a412c728f2d584888593be92d9d3
huangallen <huangallen2008@gmail.com>
2024-11-23 15:15:02 +0000

    632823ing

--- code/CSacademy/tree_game/tree_game.cpp
+++ code/CSacademy/tree_game/tree_game.cpp
@@ -84,7 +84,7 @@ signed main() {
         g[v].pb(u);
     }
     dfs(0,-1);
-    oparr(dp[0])oparr(dp[1])oparr(dp[2])
+    // oparr(dp[0])oparr(dp[1])oparr(dp[2])
     //g[0] sz=1->b[0]=0;
     int an=max(dp[1][0],dp[2][0]);
     cout<<an<<'\n';
