a65cb36c3190648f1ce07a964e5c6b33b1d5d715
huangallen <huangallen2008@gmail.com>
2024-11-23 16:11:53 +0000

    732924ing

--- code/CSacademy/tree_game/tree_game_solution.cpp
+++ code/CSacademy/tree_game/tree_game_solution.cpp
@@ -67,7 +67,7 @@ void dfs(int u,int fa) {
     dp[1][u]+=1+mx1+mx2;
     chmax(dp[1][u],dp[2][u]);
     dp[2][u]+=mx;
-    chmax(dp[2][u],-1);
+    chmax(dp[2][u],-1ll);
 }
 signed main() {
     IOS(); 
