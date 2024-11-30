1d6fcbb47ec0916834b41c11e9774733306ed556
huangallen <huangallen2008@gmail.com>
2024-11-30 08:13:58 +0000

    633530ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/二模/B.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/二模/B.cpp
@@ -150,7 +150,7 @@ signed main() {
         if(rr<mx%6)lb++;
         while(lb<rb) {
             mb=lb+rb>>1;
-            ope(mb)
+            // ope(mb)
             // Vi v=a;
             // int m=lb*6+rr;
             // int c2=0;
