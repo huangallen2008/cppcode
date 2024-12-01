ec9094128c7c7c920162b1cdefb78405178db2d8
huangallen <huangallen2008@gmail.com>
2024-12-01 14:55:15 +0000

    73361ing

--- code/Atcoder/ABC178F/ABC178F.cpp
+++ code/Atcoder/ABC178F/ABC178F.cpp
@@ -1,30 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3,unroll-loops,fast-math")
-#pragma GCC target("avx2,bmi,popcnt")
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
 #define int long long
-#define REP(i,n) for(int i=0;i<n;i++)
-#define REP1(i,n) for(int i=1;i<=n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
 #define RREP1(i,n) for(int i=(n);i>=1;i--)
 #define f first
 #define s second
 #define pb push_back
-#define ALL(x) x.begin(),x.end()
-#define SZ(x) (int)(x.size())
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
 #define SQ(x) (x)*(x)
 #define pii pair<int,int>
 #define pipii pair<int,pii>
 #define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
 #define IOS() ios::sync_with_stdio(0),cin.tie(0)
 #define md(x) (((x)%(mod)+(mod))%(mod))
 #define MD(x,M) (((x)%(M)+(M))%(M))
 #define ld long double
 #define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
 #ifdef LOCAL
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
-#define oparr(x) cout<<(#x)<<":";REP(allen,(x).size()) cout<<x[allen]<<" ";cout<<" size="<<(x).size()<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
 #define entr cout<<endl;
 #else
 #define op(x) ;
@@ -32,8 +37,11 @@ using namespace std;
 #define oparr(x) ;
 #define entr ;
 #endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
 const int mod=1e9+7;
-const int maxn=2e5+5;
+const int maxn=5;
+const int maxb=20;
 const int inf=(1ll<<62);
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
@@ -43,37 +51,15 @@ signed main() {
     IOS();
     int n;
     cin>>n;
-    vector<int> a(n),b(n),ca(n+1),cb(n+1),cur(n+1),curb(n+1);
-    vector<vector<int>> ida(n+1),idb(n+1);
-    REP(i,n) cin>>a[i];
-    REP(i,n) cin>>b[i];
-    REP(i,n) ida[a[i]].pb(i),idb[b[i]].pb(i);
-    
-    priority_queue<pii> pqa,pqb;
-    REP1(i,n) {
-        if(SZ(ida[i])>0) pqa.push({SZ(ida[i]),i});
-        if(SZ(idb[i])>0) pqb.push({SZ(idb[i]),i});
+    Vi a(n),b(n),ca(n+1),cb(n+1);
+    REP(i,n) cin>>a[i],ca[a[i]]++;
+    REP(i,n) cin>>b[i],cb[b[i]]++;
+    REP1(i,n) if(ca[i]+cb[i]>n) {
+        cout<<"No\n";
+        return 0;
     }
-    vector<int> an(n);
-    while(pqa.size()) {
-        auto [cta,va]=pqa.top(); pqa.pop();
-        auto [ctb,vb]=pqb.top(); pqb.pop();
-        if(va==vb) {
-            if(pqb.size()==0) {
-                cout<<"No\n";
-                return 0;
-            }
-            auto [ctb2,vb2]=pqb.top(); pqb.pop();
-            pqb.push({ctb,vb});
-            swap(ctb,ctb2),swap(vb,vb2);
-        }
-        op(va)ope(vb)
-        an[ida[va][cur[va]++]]=vb;
-        if(--cta!=0) pqa.push({cta,va});
-        if(--ctb!=0) pqb.push({ctb,vb});
-    }
-    cout<<"Yes\n";
-    REP(i,n) cout<<an[i]<<' ';
-    cout<<'\n';
+    int x=0;
+    REP1(i,n) chmax(x,ca[i]-cb[i-1]);
+    raise exception("235");
     return 0;
 }
