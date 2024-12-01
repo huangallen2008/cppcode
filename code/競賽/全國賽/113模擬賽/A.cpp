49aa3a1fe2d25c0c9bf4a6e6de7d5c9f5d9b236b
huangallen <huangallen2008@gmail.com>
2024-12-01 02:38:00 +0000

    73361ing

--- code/競賽/全國賽/113模擬賽/A.cpp
+++ code/競賽/全國賽/113模擬賽/A.cpp
@@ -71,8 +71,8 @@ signed main() {
     vector<pii> li(n);
     REP(i,n) li[i]={l[i],i};
     sort(ALL(li),greater<pii>());
-    for(auto [x,y,z]:t) cout<<"{"<<x<<","<<y<<","<<z<<"} ";entr
-    for(auto [x,y]:li) cout<<"{"<<x<<","<<y<<"} ";entr
+    // for(auto [x,y,z]:t) cout<<"{"<<x<<","<<y<<","<<z<<"} ";entr
+    // for(auto [x,y]:li) cout<<"{"<<x<<","<<y<<"} ";entr
     Vi an(n);
     REP(i,n) an[li[i].s]=t[i].id;
     REP(i,n) cout<<an[i]+1<<' ';
