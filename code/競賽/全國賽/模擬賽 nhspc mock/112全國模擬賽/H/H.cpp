b004ed45bf0bc644afd86e079beaa1eaee45c780
huangallen <huangallen2008@gmail.com>
2024-12-12 12:28:36 +0000

    434712ing

--- code/競賽/全國賽/模擬賽 nhspc mock/112全國模擬賽/H/H.cpp
+++ code/競賽/全國賽/模擬賽 nhspc mock/112全國模擬賽/H/H.cpp
@@ -1,43 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
 #define int long long
-#define FOR(i,a,b) for(int i=a;i<b;i++)
-#define REP(i,n) FOR(i,0,n)
-#define REP1(i,n) FOR(i,1,(n)+1)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
 #define f first
 #define s second
 #define pb push_back
-#define ALL(x) x.begin(),x.end()
-#define SZ(x) (int)(x.size())
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
 #define SQ(x) (x)*(x)
 #define pii pair<int,int>
 #define pipii pair<int,pii>
 #define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
 #define IOS() ios::sync_with_stdio(0),cin.tie(0)
-#define md(x) (x%mod+mod)%mod
-#define op(x) cout<<#x<<"="<<x<<", ";
-#define ope(x) cout<<#x<<"="<<x<<endl;
-#define oparr(x) cout<<#x<<":";REP(i,x.size()) cout<<x[i]<<" ";cout<<" size="<<x.size()<<endl;
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
 #define entr cout<<endl;
-const int inf=(1ll<<62);
-const int maxn=1e5+5;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
 const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
 signed main() {
     IOS();
     int n,k;
     cin>>n>>k;
-    vector<int> a(n);
+    Vi a(n);
     REP(i,n) cin>>a[i];
-    int an=k;
-    cout<<an<<' ';
-    int mn=a[0];
-    REP1(i,n-1) {
-        if(a[i]>mn) an+=2;
-        if(a[i]==mn) an++;
-        mn=min(mn,a[i]);
-        cout<<an<<' ';
-    }
-    cout<<'\n';
+    
     return 0;
 }
