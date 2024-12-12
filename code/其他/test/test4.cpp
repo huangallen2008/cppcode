7bd2659bc56cd6970775ec65e7b77816bbe02867
huangallen <huangallen2008@gmail.com>
2024-12-12 16:45:04 +0000

    534813ing

--- code/其他/test/test4.cpp
+++ code/其他/test/test4.cpp
@@ -1,48 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long //int變成long long，可依照題目要求加或不加
-struct SEG {
-    int n;
-    vector<int> s;
-    void build(int w,int l,int r,vector<int> &a) {
-        if(l==r) {
-            s[w]=a[l];
-            return;
-        }
-        int m=l+r>>1;
-        build(w<<1,l,m,a);
-        build(w<<1|1,m+1,r,a);
-        s[w]=s[w<<1]+s[w<<1|1];
-    }
-    void init(int _n,vector<int> a) {
-        n=_n;
-        s=vector<int>(n<<2);//n*4
-        build(1,0,n-1,a);
-    }
-    void _ud(int w,int l,int r,int u,int v) {
-        if(l==r) {
-            s[w]+=v;
-            return;
-        }
-        int m=l+r>>1;//(l+r)/2
-        if(u<=m) _ud(w<<1,l,m,u,v);//w*2
-        else _ud(w<<1|1,m+1,r,u,v);//w*2+1
-        s[w]=s[w<<1]+s[w<<1|1];
-    }
-    void ud(int u,int v) {
-        _ud(1,0,n-1,u,v);
-    }
-    int _qu(int w,int l,int r,int ql,int qr) {
-        if(ql<=l&&r<=qr) return s[w];
-        if(ql>r||qr<l) return 0;
-        int m=l+r>>1;
-        return _qu(w<<1,l,m,ql,qr)+_qu(w<<1|1,m+1,r,ql,qr);
-    }
-    int qu(int l,int r) {
-        return _qu(1,0,n-1,l,r);
-    }
-}seg;
-#define ld lonf double 
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+#define ld int double 
 #define sp(x) fixed<<setprecision(x)
 signed main() {//signed = int
     int n=6;
