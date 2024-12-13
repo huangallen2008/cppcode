4081036a6809f28e3186b84063c73d373d73d778
huangallen <huangallen2008@gmail.com>
2024-12-13 14:57:57 +0000

    534813ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -47,9 +47,59 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long //int變成long long，可依照題目要求加或不加
+struct SEG {
+    int n;
+    vector<int> s;
+    void build(int w,int l,int r,vector<int> &a) {
+        if(l==r) {
+            s[w]=a[l];
+            return;
+        }
+        int m=l+r>>1;
+        build(w<<1,l,m,a);
+        build(w<<1|1,m+1,r,a);
+        s[w]=s[w<<1]+s[w<<1|1];
+    }
+    void init(int _n,vector<int> a) {
+        n=_n;
+        s=vector<int>(n<<2);//n*4
+        build(1,0,n-1,a);
+    }
+    void _ud(int w,int l,int r,int u,int v) {
+        if(l==r) {
+            s[w]+=v;
+            return;
+        }
+        int m=l+r>>1;
+        if(u<=m) _ud(w<<1,l,m,u,v);
+        else _ud(w<<1|1,m+1,r,u,v);
+        s[w]=s[w<<1]+s[w<<1|1];
+    }
+    void ud(int u,int v) {
+        _ud(1,0,n-1,u,v);
+    }
+    int _qu(int w,int l,int r,int ql,int qr) {
+        if(ql<=l&&r<=qr) return s[w];
+        if(ql>r||qr<l) return 0;
+        int m=l+r>>1;
+        return _qu(w<<1,l,m,ql,qr)+_qu(w<<1|1,m+1,r,ql,qr);
+    }
+    int qu(int l,int r) {
+        return _qu(1,0,n-1,l,r);
+    }
+}seg;
 signed main() {
     IOS();
-    pii a={1,2},b={3,4};
-    cout<<a<<' '<<b<<'\n';
+    int a=1;
+    int an=0;
+    an+=a;
+    REP(i,99) {
+        (a=a*a*a*a*a+1)%=10;
+        an+=a;
+    }
+    ope(an)
     return 0;
 }
