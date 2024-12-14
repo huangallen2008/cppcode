14575ecb39167dbc8d3ab0260b11a8126e7d8bc5
huangallen <huangallen2008@gmail.com>
2024-12-14 08:14:54 +0000

    634914ing

--- code/USACO/202412/silver/B.cpp
+++ code/USACO/202412/silver/B.cpp
@@ -65,6 +65,7 @@ void solve() {
         a[i]=(lower_bound(ALL(t),a[i])-t.begin())+1;
         cnt[a[i]]++;
     }
+    oparr(a)oparr(t)
     Graphw g(N);
     vector<edge> e;
     for(int i=2;i<N;i++) {
