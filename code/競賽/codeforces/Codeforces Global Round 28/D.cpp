51fd76ee3ebef61ffc081c51c726955679612f26
huangallen <huangallen2008@gmail.com>
2024-12-19 15:06:02 +0000

    435419ing

---
+++ code/競賽/codeforces/Codeforces Global Round 28/D.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops,fast-math")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+const int mod=1e9+7;
+const int maxn=5;
+const int maxb=20;
+const int inf=(1ll<<62);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+struct BIT {
+    int n;
+    vector<int> b;
+    void init(int _n) {
+        n=_n;
+        b=vector<int>(n+1);
+    }
+    void ud(int u,int v) {
+        for(;u<=n;u+=u&-u) b[u]+=v;
+    }
+    int pre(int u) {
+        int r=0;
+        for(;u>0;u-=u&-u) r+=b[u];
+        return r;
+    }
+    int qu(int l,int r) {
+        return pre(r)-pre(l-1);
+    }
+}bit;
+void solve() {
+    int n,m;
+    cin>>n>>m;
+    Vi a(n),b(m);
+    Vi t;
+    t.pb(-inf);
+    REP(i,n) cin>>a[i],t.pb(a[i]);
+    REP(i,m) cin>>b[i],t.pb(b[i]);
+    sort(ALL(t));
+    REP(i,n) a[i]=lower_bound(ALL(t),a[i])-t.begin();
+    REP(i,m) b[i]=lower_bound(ALL(t),a[i])-t.begin();
+    int rat=a[0];
+    bit.init(n+m);
+    REP(i,n) bit.ud(a[i],1);
+    sort(ALL(b));
+    int id0=upper_bound(ALL(b),rat)-b.begin();
+}
+signed main() {
+    IOS();
+    int T;
+    cin>>T;
+    while(T--) solve();
+    return 0;
+}
