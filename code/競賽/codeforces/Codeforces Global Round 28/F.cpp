0465d9b6804289b9a839f945a5fffa025d29426d
huangallen <huangallen2008@gmail.com>
2024-12-19 16:06:52 +0000

    535520ing

--- code/競賽/codeforces/Codeforces Global Round 28/F.cpp
+++ code/競賽/codeforces/Codeforces Global Round 28/F.cpp
@@ -49,7 +49,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-    int n;
+int n;
 Vi a,b;
 int di(int a,int b) { return (a+b-1)/b; }
 pii st[maxb][maxn];
