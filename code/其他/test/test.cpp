02355af8f8349c452c06d3fd92da09d5f0205069
huangallen <huangallen2008@gmail.com>
2024-12-24 15:22:38 +0000

    235924ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -56,6 +56,7 @@ struct SEG {
     Vpii merge(Vpii b,Vpii c) {
         Vpii a(maxb,{0,inf});
         REP(i,maxb) REP(j,maxb) chmin(a[i^j].s,b[i].s+c[j].s);
+        oparr(a)oparr(b)oparr(c)
         REP(i,maxb) REP(j,maxb) if(b[i].s+c[j].s==a[i^j].s) (a[i^j].f+=b[i].f*c[j].f)%=mod;
         return a;
     }
