b76c05b0550b489d5ca709aaaafd2478ab79e8de
huangallen <huangallen2008@gmail.com>
2024-12-24 15:33:20 +0000

    235924ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -43,7 +43,7 @@ ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<
 const int mod=998244353;
 const int maxn=5;
 const int maxb=8;
-const int inf=(1ll<<62);
+const int inf=1e14;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
