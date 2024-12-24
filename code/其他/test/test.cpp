00710d7192eb42e0a0e91f86027c1b5da3fc180a
huangallen <huangallen2008@gmail.com>
2024-12-24 15:39:20 +0000

    235924ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -75,7 +75,7 @@ struct SEG {
         build(w<<1,l,m,a);
         build(w<<1|1,m+1,r,a);
         pull(s[w],s[w<<1],s[w<<1|1]);
-        op(w)oparr(s[w])oparr(s[w<<1])oparr(s[w<<1|1])
+        op(w)oparr(s[w])
     }
     void init(int _n,Vi a) {
         n=_n;
