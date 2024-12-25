d0b56df854d9722678c6b786b27b147ee1dfa007
huangallen <huangallen2008@gmail.com>
2024-12-25 06:55:31 +0000

    336025ing

--- code/CSacademy/library_book/library_book.cpp
+++ code/CSacademy/library_book/library_book.cpp
@@ -48,7 +48,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-typename ff() {
+int ff(typename s) {
 
 }
 signed main() {
