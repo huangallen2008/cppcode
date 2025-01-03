bd66f3cb9adeab998b2191c7e710c5c5419cdadf
huangallen <huangallen2008@gmail.com>
2025-01-03 17:32:40 +0000

    644ing

--- code/CSacademy/road-trips/road-trips.cpp
+++ code/CSacademy/road-trips/road-trips.cpp
@@ -131,7 +131,8 @@ signed main() {
     oparr(leaf)oparr(c)ope(lcas[1][1])
     Vi dp(1<<cl,-inf);dp[0]=0;
     REP(r,cl) {
-        Vi ndp=dp;
+        Vi ndp(1<<cl,-inf);
+        if(r) ndp=dp;
         REP(i,1<<cl) {
             int id=r;
             REP(j,cl) {
