3721ddacc3c3c72a9a732ab0bf6fd485a1a68257
huangallen <huangallen2008@gmail.com>
2025-01-05 07:28:38 +0000

    755ing

---
+++ code/其他/temp/APCS202501/D.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) (int)((x).size())
+#define SQ(x) (x)*(x)
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+const int mod=998244353;
+const int maxn=5;
+const int maxb=64;
+const int inf=1e9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+signed main() {
+    IOS();
+    int n,k;
+    cin>>n>>k;
+    Vi a(n+1),p(n+1);
+    REP1(i,n) cin>>a[i];
+    sort(1+ALL(a));
+    REP1(i,n) p[i]=p[i-1]+a[i];
+    auto cc=[&](int l,int r) {
+        int med=a[l+r>>1];
+        int id=l+r>>1;
+        return p[r]-p[id]-(r-id+1)*med+(id-l)*med-(p[id-1]-p[l-1]);
+    } ;
+    Vi cost(n+1);
+    for(int i=k;i<=n;i++) cost[i]=cc(i-k+1,i);
+    Vi d1(n+1,inf),d2(n+1,inf);
+    for(int i=k;i<=n;i++) d1[i]=min(d1[i-1],cost[i]);
+    for(int i=k;i<=n;i++) d2[i]=min(d2[i-1],d1[i-k]+cost[i]);
+    cout<<d2[n]<<'\n';
+    return 0;
+}
