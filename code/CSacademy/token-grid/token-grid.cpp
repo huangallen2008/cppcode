c564742275f2a70386c262fdedce78405f3e188e
huangallen <huangallen2008@gmail.com>
2025-01-11 03:25:58 +0000

    61111ing

--- code/CSacademy/token-grid/token-grid.cpp
+++ code/CSacademy/token-grid/token-grid.cpp
@@ -87,8 +87,8 @@ signed main() {
         ope(i)
         REP(c,26) {
             ope(c)
-            op(a[j][c])
-            for(int j=a[j][c];j>0;j=(j-1)&a[j][c]) {
+            op(a[i][c])
+            for(int j=a[i][c];j>0;j=(j-1)&a[i][c]) {
                 ope(j)
                 d[j]=1;
             }
