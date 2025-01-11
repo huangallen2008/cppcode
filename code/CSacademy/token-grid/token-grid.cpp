d877ebe03a1b571713c76be018da59d929f6fdc2
huangallen <huangallen2008@gmail.com>
2025-01-11 04:02:20 +0000

    61111ing

--- code/CSacademy/token-grid/token-grid.cpp
+++ code/CSacademy/token-grid/token-grid.cpp
@@ -85,7 +85,7 @@ signed main() {
     REP(i,m) {
         Vi d(1<<n);
         d[0]=1;
-        REP(c,26) for(int j=a[i][c];j>0;j=(j-1)&a[i][c]) d[j]=1
+        REP(c,26) for(int j=a[i][c];j>0;j=(j-1)&a[i][c]) d[j]=1;
         dp=merge(dp,trans(d));
     }
     dp=trans(dp,-1);
