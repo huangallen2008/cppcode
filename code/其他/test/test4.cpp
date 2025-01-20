25df818f3419b41b68e2326c92b2bce45da1417a
huangallen <huangallen2008@gmail.com>
2025-01-20 11:16:00 +0000

    12020ing

--- code/其他/test/test4.cpp
+++ code/其他/test/test4.cpp
@@ -1,31 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
-signed main() {
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    .
-    .
-    .
-}
-    int n, k;
-    cin >> n >> k;
-    vector<pair<int, int>> ve;
-    for (int i=0;i<n;i++) {
-        int x;
-        cin >> x;
-        ve.push_back({x, i});
-    }
-    sort(ve.begin(), ve.end());
-    reverse(ve.begin(), ve.end());
-    cout << ve[k-1].first << "\n";
-    vector<int> idx;
-    for (int i=0;i<k;i++) {
-        idx.push_back(ve[i].second);
-    }
-    sort(idx.begin(), idx.end());
-    for (int i:idx) {
-        cout << i+1 << " ";
-    }
-    cout << "\n";
+int main() {
+    printf("%Well"," how dangerous is this program?\n");
 }
