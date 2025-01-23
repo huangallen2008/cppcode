612969a2bede290acafb08bb38b680a5721804b1
huangallen <huangallen2008@gmail.com>
2025-01-23 12:06:52 +0000

    42323ing

--- code/IOICAMP/2025/Day4/B.cpp
+++ code/IOICAMP/2025/Day4/B.cpp
@@ -60,7 +60,6 @@ int rd(int l,int r) {
 Vi mnf(maxv);
 void init_isp() {
     for(int i=2;i<maxv;i++) if(mnf[i]==0) for(int j=i;j<maxv;j+=i) if(mnf[j]==0) mnf[j]=i;
-    ope("ok")
 }
 struct BIT {
     int n;
@@ -97,6 +96,7 @@ signed main() {
     Vi mnp,cnt1(maxv);
     for(int i=2;i<V;i++) if(mnf[i]==i) {
         if(i<V) {
+        ope("ok")
             bit[mnp.size()].init(25);
             mnp.pb(i);
         }
