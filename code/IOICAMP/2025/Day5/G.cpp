0b9651c44c7403536a8975431a6ed66f9b3a406b
huangallen <huangallen2008@gmail.com>
2025-01-24 03:50:38 +0000

    52424ing

--- code/IOICAMP/2025/Day5/G.cpp
+++ code/IOICAMP/2025/Day5/G.cpp
@@ -71,7 +71,7 @@ signed main() {
     for(int i = 1; i <= n; i++) cin >> b[i];
     vector<vector<int> > dp(n+1, vector<int>(m+1));
     vector<vector<int> > xdp(n+1, vector<int>(m+1));
-    vector<Vpii> stk(n+1{{0,0}});
+    vector<Vpii> stk(n+1,{{0,0}});
     REP1(i,n) xdp[i][0]=0;
     Vi mdp(m+1);
     for(int j = 1; j <= m; j++) {
