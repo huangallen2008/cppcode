8e799daee9eb84d4b939c2afb64c6f7d6632878d
huangallen <huangallen2008@gmail.com>
2025-01-26 02:35:50 +0000

    72626ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -67,7 +67,8 @@ int pw(int x,int p) {
 }
 signed main() {
     IOS();
-    double a=(double)5/3;
+    // double a=(double)5/3;
+    ld a=(ld)5/3;
     cout<<fixed<<setprecision(50)<<a<<'\n';
     return 0;
 }
