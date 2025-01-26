b46064001a6f8a308f27d1599403b60b602350bf
huangallen <huangallen2008@gmail.com>
2025-01-26 09:22:24 +0000

    72626ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/三模/C.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/三模/C.cpp
@@ -145,6 +145,67 @@ struct SEG {
         return _qu(1,0,n-1,l,r);
     }
 }seg;
+struct SEG {
+    struct Seg {
+        int mx,ta;
+    };
+    void addtaga(Seg &a,int ta) {
+        a.mx+=ta;
+        a.ta+=ta;
+    }
+    void push(Seg &a,Seg &b,Seg &c) {
+        addtaga(b,a.ta);
+        addtaga(c,a.ta);
+        a.ta=0,a.tx=0;
+    }
+    void pull(Seg &a,Seg &b,Seg &c) {
+        a.mx=max(b.mx,c.mx);
+    }
+    int n;
+    vector<Seg> s;
+    void init(int _n) {
+        n=_n;
+        s=vector<Seg>(n<<2);
+    }
+    void _ud_x(int w,int l,int r,int u,int v) {
+        if(l==r) {
+            chmax(s[w],v);
+            return;
+        }
+        int m=l+r>>1;
+        push(s[w],s[w<<1],s[w<<1|1]);
+        if(u<=m)_ud_x(w<<1,l,m,u,v);
+        else _ud_x(w<<1|1,m+1,r,u,v);
+        pull(s[w],s[w<<1],s[w<<1|1]);
+    }
+    void ud_x(int u,int v) {
+        _ud_x(1,0,n-1,u,v);
+    }void _ud_a(int w,int l,int r,int ql,int qr,int v) {
+        if(ql<=l&&r<=qr) {
+            addtaga(s[w],v);
+            return;
+        }
+        if(ql>r||qr<l) return;
+        int m=l+r>>1;
+        push(s[w],s[w<<1],s[w<<1|1]);
+        _ud_a(w<<1,l,m,ql,qr,v);
+        _ud_a(w<<1|1,m+1,r,ql,qr,v);
+        pull(s[w],s[w<<1],s[w<<1|1]);
+    }
+    void ud_a(int l,int r,int v) {
+        _ud_a(1,0,n-1,l,r,v);
+    }
+    int _qu(int w,int l,int r,int ql,int qr) {
+        if(ql<=l&&r<=qr) return s[w].mx;
+        if(ql<r||qr<l) return -inf;
+        int m=l+r>>1;
+        push(s[w],s[w<<1],s[w<<1|1]);
+        return max(_qu(w<<1,l,m,ql,qr),_qu(w<<1|1,m+1,r,ql,qr));
+    }
+    int qu(int l,int r) {
+        return _qu(1,0,n-1,l,r);
+    }
+}seg;
 signed main() {
     IOS();
     int n,d;
@@ -161,7 +222,7 @@ signed main() {
         mv[i]=dp[i]-bit.qu(a[i]-d,a[i]);
         bit.ud(a[i],1);
         seg.ud_a(a[i],a[i]+d,1);
-        seg.ud_x(a[i],a[i],mv[i]);
+        seg.ud_x(a[i],mv[i]);
     }
     // oparr(dp)oparr(mv)
     cout<<dp[n]<<'\n';
