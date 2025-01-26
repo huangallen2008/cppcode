662c7a5878ed55052b389214d49771a212ab1104
huangallen <huangallen2008@gmail.com>
2025-01-26 09:00:30 +0000

    72626ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/三模/C.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/三模/C.cpp
@@ -49,7 +49,7 @@ istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;ret
 template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=998244353;
-const int maxn=1e6+5;
+const int maxv=2e6+5;
 // const int maxn=1e3+5;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
@@ -78,6 +78,11 @@ struct BIT {
 }bit;
 signed main() {
     IOS();
+    #ifdef LOCAL
+    bit.init(5);
+    bit.ud(1,3);
+    ope(bit.qu(-1,-1));
+    #endif
     int n,d;
     cin>>n>>d;
     Vi a(n+1);
