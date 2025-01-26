97d4d0ccc35b692e9b0dc3401c188daed324eb44
huangallen <huangallen2008@gmail.com>
2025-01-26 11:46:13 +0000

    72626ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/三模/A.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/三模/A.cpp
@@ -107,7 +107,7 @@ signed main() {
         ope(an)
         REP1(j,i) {
             //j+1 chains and 
-            an=(an+(C(i,j)*cal(j+1)%mod))%mod;
+            an=(an+(pw(-1,j+1)*cal(j+1)%mod))%mod;
         }
         an=(an+mod)%mod;
         cout<<an<<'\n';
