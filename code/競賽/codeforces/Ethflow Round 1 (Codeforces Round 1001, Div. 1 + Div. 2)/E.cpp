8827eb38745afe079c788dc5433accc5db017fd6
huangallen <huangallen2008@gmail.com>
2025-01-27 07:04:39 +0000

    12727ing

--- code/競賽/codeforces/Ethflow Round 1 (Codeforces Round 1001, Div. 1 + Div. 2)/E.cpp
+++ code/競賽/codeforces/Ethflow Round 1 (Codeforces Round 1001, Div. 1 + Div. 2)/E.cpp
@@ -129,7 +129,7 @@ void solve() {
     REP(i,n) {
     // ope(i)
         int ret=max(st_mx(0,in[i]-1),st_mx(out[i]+1,n*2));
-        op(i)ope(ret)
+        // op(i)ope(ret)
         if(ret>a[i]) an.pb(i);
     }
     // REP(i,n) {
