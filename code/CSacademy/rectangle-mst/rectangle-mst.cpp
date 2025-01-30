cf66ed4f731143cdb7589bb4ea04fb49d3465afa
huangallen <huangallen2008@gmail.com>
2025-01-30 15:02:02 +0000

    43030ing

--- code/CSacademy/rectangle-mst/rectangle-mst.cpp
+++ code/CSacademy/rectangle-mst/rectangle-mst.cpp
@@ -190,7 +190,7 @@ signed main() {
         REP(i,n) {
             for(auto oo:qu[i]) opqu(oo);
             pii ret=seg.qu(i);
-            ope(i)
+            op(i)op(ret.f)ope(ret.s)
             if(ret.f!=-1) dsu.merge(i,ret.f,ret.s);
         }
     }
