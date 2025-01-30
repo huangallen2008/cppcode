eb33e50653ea34a09bfa07952fe526692729bc42
huangallen <huangallen2008@gmail.com>
2025-01-30 15:55:44 +0000

    43030ing

--- code/CSacademy/rectangle-mst/rectangle-mst.cpp
+++ code/CSacademy/rectangle-mst/rectangle-mst.cpp
@@ -108,14 +108,14 @@ struct SEG {
             a.mid=b.mid;
             a.mp2=min(b.mp2,b.sum+(sg(b.mid,c.mid)?c.mp2:c.mp));
             if(b.mp2==a.mp2) a.mid2=b.mid2;
-            else if(!sg(b.mid,c.mid)&&a.mp2==c.mp) a.mid2=c.mid;
+            else if(!sg(b.mid,c.mid)&&a.mp2==b.sum+c.mp) a.mid2=c.mid;
             else a.mid2=c.mid2;
         }
         else {
             a.mid=c.mid;
             a.mp2=min(sg(c.mid,b.mid)?b.mp2:b.mp,b.sum+c.mp2);
             if(!sg(c.mid,b.mid)&&a.mp2==b.mp)a.mid2=b.mid;
-            else if(c.mp2==a.mp2)a.mid2=c.mid2;
+            else if(b.sum+c.mp2==a.mp2)a.mid2=c.mid2;
             else a.mid2=b.mid2;
         }
         return a;
