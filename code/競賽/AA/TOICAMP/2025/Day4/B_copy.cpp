40c5f5d05c6eb0b119f3bf04a79c6e024b6466e1
huangallen <huangallen2008@gmail.com>
2025-02-06 08:33:34 +0000

    4376ing

--- code/競賽/AA/TOICAMP/2025/Day4/B_copy.cpp
+++ code/競賽/AA/TOICAMP/2025/Day4/B_copy.cpp
@@ -71,14 +71,16 @@ void solve() {
         int rt=d-p[i-1]*n,now=n*(i-1);
         // int cnt=min(n,rt/(p[m]-p[i-1]));
         int mxcnt=min(n,rt/(p[m]-p[i-1]));
-        for(int cnt:Vi{0,mxcnt}) {
+        auto run=[&](int cnt)->void{
             int now2=now;
             int rt2=rt;
             now2+=cnt*(m-i+1+k);
             rt2-=cnt*(p[m]-p[i-1]);
             now2+=min(rt2/a[i],n-cnt);
             chmax(an,now2);
-        }
+        };
+        for(int cnt=0;cnt<mxcnt&&cnt<200;cnt++) run(cnt);
+        for(int cnt=mxcnt;cnt>=0&&cnt>=mxcnt-200;cnt--) run(cnt);
         int c2= min(n,(d-p[i-1]*n)/a[i]);
         chmax(an,n*(i-1)+c2+(c2==n&&i!=m?((d-p[i]*n)/(p[m]-p[i]))*(m-i+k):0));
     }
