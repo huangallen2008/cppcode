95aa53f1e9147e89e0f2f046be5eefb80d64382e
huangallen <huangallen2008@gmail.com>
2025-02-06 06:35:20 +0000

    4376ing

--- code/競賽/AA/TOICAMP/2025/Day4/B.cpp
+++ code/競賽/AA/TOICAMP/2025/Day4/B.cpp
@@ -69,11 +69,20 @@ void solve() {
         //1. a[i]->1  2. p[m]-p[i-1] -> m-i+1+k
         if(p[i-1]*n>d) break;
         int rt=d-p[i-1]*n,now=n*(i-1);
+        int cnt=rt/(p[m]-p[i-1]);
+        now+=cnt*(m-i+1+k);
+        rt-=cnt*(p[m]-p[i-1]);
+        now+=rt/a[i];
+        chmax(an,now);
+        /*
+        
+        int rt=d-p[i-1]*n,now=n*(i-1);
         int cnt=min(n,rt/(p[m]-p[i-1]));
         now+=cnt*(m-i+1+k);
         rt-=cnt*(p[m]-p[i-1]);
         now+=min(rt/a[i],n-cnt);
         chmax(an,now);
+        */
     }
     cout<<an<<'\n';
 }
