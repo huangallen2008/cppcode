9a958b8f7309ef4a094d85e90505f53648cdec20
huangallen <huangallen2008@gmail.com>
2025-02-16 15:30:56 +0000

    74716ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/五模/C.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/五模/C.cpp
@@ -78,7 +78,7 @@ void solve() {
     vector<Vi> mdx(n,Vi(n));
     REP(j,n) {
         REP(i,j) {
-            mdx[i][j]=upper_bound(ALL(a),pii{midx(a[j],a[i]),-inf})-a.begin();
+            mdx[i][j]=upper_bound(ALL(a),pii{midx(a[j],a[i]),inf})-a.begin()-1;
         }
     }
     vector<Vi> dp(k+1,Vi(n,-inf));
