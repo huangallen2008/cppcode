003f2e48de373c3e90f99b9c0c14c5a06cb82dde
huangallen <huangallen2008@gmail.com>
2025-02-16 15:11:08 +0000

    74716ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/五模/C.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/五模/C.cpp
@@ -92,11 +92,11 @@ void solve() {
     };
     REP(i,k+1) {
         REP(j,n) {
-            op(i)ope(j)
             int ht0=s[j][j];
             if(ht0<=i) chmax(dp[i][j],cnsum(a[j].s+1,a[j].s+a[j].f-1));
             op(i)op(ht0)ope(cnsum(a[j].s+1,a[j].s+a[j].f-1))
             REP(k,j) {
+                op(i)op(j)ope(k)
                 int mid=midx(a[k],a[j]);
                 int ht=s[j][j]-s[j][mid]+d[k][mid-1]-d[k][k];
                 if(ht<=i) chmax(dp[i][j],dp[i-ht][j]+qusum(a[k],a[j]));
