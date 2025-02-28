cd120d7773f1ef3a17b3cffeffe0dddac654bf68
huangallen <huangallen2008@gmail.com>
2025-02-28 09:33:46 +0000

    55928ing

--- code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/六模/B.cpp
+++ code/競賽/AA/AA競程實力養成訓練班模擬考/第三階段/六模/B.cpp
@@ -102,12 +102,14 @@ signed main() {
     // vector<Vi> px(n+1,Vi(n+2)),sx(n+1,Vi(n+2));
     REP1(i,n) {
         auto upd=[&](Vpii &a,pii x)->void {
+            op(a)op(x)
             REP(i,3) {
                 if(x.f>a[i].f) {
                     for(int j=2;j>i;j--) a[j]=a[j-1];
                 }
                 a[i]=x;
             }
+            ope(a)
         };
         REP1(j,n) {
             // ope(j)
