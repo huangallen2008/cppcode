9eb4bbfc3e56d8b2356cc798a6d9ea89ab1c370d
huangallen <huangallen2008@gmail.com>
2025-03-01 15:21:50 +0000

    6601ing

--- code/競賽/AA/TOICAMP/2025/Day2/E.cpp
+++ code/競賽/AA/TOICAMP/2025/Day2/E.cpp
@@ -56,45 +56,6 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-// #ifdef LOCAL
-#define GC _getchar_nolock()
-#define PC _putchar_nolock
-// #else 
-// #define GC getchar_unlocked()
-// #define PC putchar_unlocked
-// #endif
-inline int read()
-{
-    int x=0;
-    bool neg=0;
-    char c=GC;
-    while(c<'0'||c>'9'){if(c=='-') neg=1;c=GC;}
-    while(c>='0'&&c<='9') x=(x<<3)+(x<<1)+(c^48),c=GC;
-    if(neg) x=-x;
-    return x;
-}
-inline string reads()
-{
-    char c=GC;
-    string s;
-    while(c==' '||c=='\n')c=GC;
-    while(c!=' '&&c!='\n'&&c!=EOF) s+=c,c=GC;
-    return s;
-}
-inline void out(int x) {
-    if(x<0) {
-        PC('-');
-        x=-x;
-    }
-    char str[18];
-	auto it=str;
-    do { 
-        *it=x%10+'0',it++;
-        x/=10;
-    } while(x);
-    for(it--;it>=str;it--) PC(*it);
-    // PC('\n');
-}
 int r,c,k;
 vector<Vi> a;
 vector<pipii> s;
@@ -102,8 +63,6 @@ Vpii dir={{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0}};
 bool ok(int x,int y) {
     return !(x<0||x>=r||y<0||y>=c);
 }
-
-
 int check(int m) {
     vector<Vi> d=a;
     int it=1;
@@ -129,10 +88,10 @@ int check(int m) {
     return an;
 }
 void solve() {
-    r=read(),c=read(),k=read();
-    // cin>>r>>c>>k;
+    // r=read(),c=read(),k=read();
+    cin>>r>>c>>k;
     a=vector<Vi>(r,Vi(c));
-    REP(i,r) REP(j,c) a[i][j]=read(),s.pb({a[i][j],{i,j}});
+    REP(i,r) REP(j,c) cin>>a[i][j],s.pb({a[i][j],{i,j}});
     sort(ALL(s),greater<pipii>());
     int l=0,r=maxv,m;
     while(l<r) {//first m: check(m)<=k
