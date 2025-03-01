3b3b6b27f8f74361d4862b81dd28c075bed7d432
huangallen <huangallen2008@gmail.com>
2025-03-01 07:40:59 +0000

    6601ing

--- code/競賽/AA/TOICAMP/2025/Day2/E.cpp
+++ code/競賽/AA/TOICAMP/2025/Day2/E.cpp
@@ -102,7 +102,7 @@ signed main() {
     int l=0,r=maxv,m;
     while(l<r) {//first m: check(m)<=k
         m=l+r>>1;
-        op(m)ope(check(m))
+        // op(m)ope(check(m))
         if(check(m)<=k) r=m;
         else l=m+1;
     }
