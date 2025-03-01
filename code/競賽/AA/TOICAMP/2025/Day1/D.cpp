751aea55be2170e5da03018cadde12a5f395d099
huangallen <huangallen2008@gmail.com>
2025-03-01 05:46:25 +0000

    6601ing

--- code/競賽/AA/TOICAMP/2025/Day1/D.cpp
+++ code/競賽/AA/TOICAMP/2025/Day1/D.cpp
@@ -64,7 +64,7 @@ signed main() {
     Vi a(n);
     REP(i,n) cin>>a[i];
     cout<<n<<'\n';
-    cout<<"1 "<<n<<" 1";
+    cout<<"1 "<<n<<" 1\n";
     Vi p(n),b(n,1);
     REP(i,n) p[i]=i+1;
     for(int i=n-1;i>0;i--) {
