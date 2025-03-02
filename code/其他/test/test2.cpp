605a9845392cd45b3d7d2f96f9ee71830c6887f4
huangallen <huangallen2008@gmail.com>
2025-03-02 03:15:25 +0000

    7612ing

--- code/其他/test/test2.cpp
+++ code/其他/test/test2.cpp
@@ -1,199 +1,138 @@
 #include <bits/stdc++.h>
 using namespace std;
-// #pragma GCC optimize("O3,unroll-loops")
-// #pragma GCC target("avx2,sse4,bmi2,popcnt")
-#define int long long
-#define REP(i, n) for (int i = 0; i < (n); i++)
-#define REP1(i, n) for (int i = 1; i <= (n); i++)
-#define RREP(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define ll long long
+// #define int ll
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define RREP(i, n) for (int i = (n)-1; i >= 0; i--)
 #define RREP1(i, n) for (int i = (n); i >= 1; i--)
+#define REP1(i, n) FOR(i, 1, n+1)
+#define pii pair<int, int>
+#define ppi pair<pii, int>
+#define pip pair<int, pii>
 #define f first
 #define s second
 #define pb push_back
 #define ALL(x) (x).begin(), (x).end()
 #define SZ(x) (int)((x).size())
-#define SQ(x) (x) * (x)
-#define pii pair<int, int>
-#define pipii pair<int, pii>
-#define Graph vector<vector<int>>
-#define Graphw vector<vector<pii>>
-#define IOS() ios::sync_with_stdio(0), cin.tie(0)
-#define md(x) (((x) % (mod) + (mod)) % (mod))
-#define MD(x, M) (((x) % (M) + (M)) % (M))
-#define ld long double
-#define pdd pair<ld, ld>
-#define chmax(x, y) x = max(x, y)
-#define chmin(x, y) x = min(x, y)
-#define addmod(x, y) x = ((x + (y)) % mod)
-#define Vi vector<int>
-#define Vpii vector<pii>
-#ifdef LOCAL
-#define op(x) cout << (#x) << "=" << (x) << ", ";
-#define ope(x) cout << (#x) << "=" << (x) << endl;
-#define oparr(x)                                \
-	{                                           \
-		cout << (#x) << ":";                    \
-		for (auto allen : (x))                  \
-			cout << allen << " ";               \
-		cout << " size=" << (x).size() << endl; \
-	}
-#define entr cout << endl;
-#else
-#define op(x) ;
-#define ope(x) ;
-#define oparr(x) ;
-#define entr ;
-#endif
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, pair<T1, T2> p) { return os << '{' << p.f << ',' << p.s << '}'; }
-template <typename T1, typename T2>
-istream &operator>>(istream &os, pair<T1, T2> &p) { return os >> p.f >> p.s; }
-template <typename S>
-ostream &operator<<(ostream &os, vector<S> p)
-{
-	for (auto allen : p)
-		os << allen << ' ';
-	return os << '\n';
+#define endl '\n'
+#define IOS() ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+
+const ll maxn = 1005;
+const ll mod = 998244353;
+const ll inf = (1ll<<60);
+
+ll pw(ll x, ll p, ll m){
+    ll ret = 1;
+    while(p > 0){
+        if (p & 1){
+            ret *= x;
+            ret %= m;
+        }
+        x *= x;
+        x %= m;
+        p >>= 1;
+    }
+    return ret;
 }
-template <typename S>
-istream &operator>>(istream &os, vector<S> &p)
-{
-	for (auto &allen : p)
-		os >> allen;
-	return os;
+ll inv(ll x, ll m){
+    return pw(x, m-2, m);
 }
-template <typename T1, typename T2>
-pair<T1, T2> operator+(pair<T1, T2> p1, pair<T1, T2> p2) { return pair<T1, T2>(p1.f + p2.f, p1.s + p2.s); }
-const int mod = 998244353;
-const int maxn = 5;
-const int maxb = 64;
-const int inf = 1ll << 60;
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-int rd(int l, int r)
-{
-	return uniform_int_distribution<int>(l, r)(rng);
+
+int n, m, k;
+
+char arr[maxn][maxn];
+int smr[maxn], smc[maxn];
+int smt = 0;
+vector<int> A, B;
+
+int mp[maxn][maxn];
+bool check(int a, int b){
+    A.clear(); B.clear();
+    if (a >= n || b >= m) return 0;
+    int ndr = smt / (a+1), ndc = smt / (b+1), ndcell = smt / ((a+1)*(b+1));
+
+    int cur = 0;
+    REP1(i, n){
+        cur += smr[i];
+        if (cur > ndr) return 0;
+        if (cur == ndr && SZ(A) < a){
+            A.pb(i);
+            cur = 0;
+        }
+    }
+    if (cur != ndr) return 0;
+    A.pb(n);
+
+    cur = 0;
+    REP1(i, m){
+        cur += smc[i];
+        if (cur > ndc) return 0;
+        if (cur == ndc && SZ(B) < b){
+            B.pb(i);
+            cur = 0;
+        }
+    }
+    if (cur != ndc) return 0;
+    B.pb(m);
+
+    REP(i, SZ(A)) REP(j, SZ(B)) mp[i][j] = 0;
+
+    int Aptr = 0;
+    REP1(i, n){
+        int Bptr = 0;
+        REP1(j, m){
+            mp[Aptr][Bptr] += (arr[i][j] == '0');
+            if (B[Bptr] == j) Bptr++;
+        }
+        if (A[Aptr] == i) Aptr++;
+    }
+
+    REP(i, SZ(A)) REP(j, SZ(B)) if (mp[i][j] != ndcell) return 0;
+    return 1;
 }
-void solve()
-{
-	int n, m, k;
-	cin >> n >> m >> k;
-	vector<Vi> a(n + 2, Vi(m + 2));
-	vector<Vi> p(n + 2, Vi(m + 2));
-	REP1(i, n)
-	REP1(j, m)
-	{
-		char ch;
-		cin >> ch;
-		a[i][j] = ch == '0';
-	}
-	REP1(i, n)
-	REP1(j, m) p[i][j] = p[i - 1][j] + p[i][j - 1] - p[i - 1][j - 1] + a[i][j];
-	auto cnt = [&](int r1, int r2, int c1, int c2)
-	{
-		return p[r2][c2] - p[r2][c1 - 1] - p[r1 - 1][c2] + p[r1 - 1][c1 - 1];
-	};
-	if (p[n][m] == 0)
-	{
-		REP1(i, k)
-			cout << i << ' ';
-		cout << '\n';
-		return;
-	}
-	Vi an(k, inf);
-	REP1(rc, k + 1)
-	{
-		bool ok = 1;
-		int cc = k + 2 - rc;
-		if (rc > n || cc > m)
-			continue;
-		// op(rc)op(cc)ope(p[n][m])
-		if (p[n][m] % ((rc) * (cc)))
-			continue;
-		int x = p[n][m] / rc / cc;
-		int now = 0;
-		Vi rid(rc + 1);
-		Vi cid(cc + 1);
-		int rit = 1;
-		int cit = 1;
-		// op(rc)ope(cc)
-		REP1(i, n)
-		{
-			// ope(i)
-			now += p[i][n] - p[i - 1][n];
-			if (now == x * cc)
-			{
-				rid[rit++] = i;
-				now = 0;
-			}
-			else if (now > x * cc)
-			{
-				ok = 0;
-				break;
-			}
-		}
-		if (rit != rc + 1)
-			continue;
-		if (!ok)
-			continue;
-		rid[rc] = n;
-		now = 0;
-		REP1(i, m)
-		{
-			now += p[n][i] - p[n][i - 1];
-			if (now == x * rc)
-			{
-				cid[cit++] = i;
-				now = 0;
-			}
-			else if (now > x * rc)
-			{
-				ok = 0;
-				break;
-			}
-		}
-		cid[cc] = m;
-		if (cit != cc + 1)
-			continue;
-		if (!ok)
-			continue;
-		REP1(i, rc)
-		{
-			REP1(j, cc)
-			{
-				if (cnt(rid[i - 1] + 1, rid[i], cid[j - 1] + 1, cid[j]) != x)
-				{
-					ok = 0;
-					break;
-				}
-			}
-			if (!ok)
-				break;
-		}
-		if (!ok)
-			continue;
-		// oparr(rid)oparr(cid)
-		Vi res;
-		REP1(i, rc - 1)
-		res.pb(rid[i]);
-		REP1(i, cc - 1)
-		res.pb(n - 1 + cid[i]);
-		chmin(an, res);
-	}
-	if (an[0] == inf)
-	{
-		cout << "Impossible\n";
-		return;
-	}
-	REP(i, k)
-		cout << an[i] << ' ';
-	cout << '\n';
-}
-signed main()
-{
-	IOS();
-	int T;
-	cin >> T;
-	while (T--)
-		solve();
-	return 0;
+signed main(){
+    IOS();
+    int T; cin>>T;
+    while(T--){
+        cin>>n>>m>>k;
+        REP1(i, n) smr[i] = 0;
+        REP1(i, m) smc[i] = 0;
+
+        smt = 0;
+        REP1(i, n){
+            REP1(j, m){
+                cin>>arr[i][j];
+                smr[i] += (arr[i][j] == '0');
+                smc[j] += (arr[i][j] == '0');
+                smt += (arr[i][j] == '0');
+            }
+        }
+
+        vector<vector<int>> cands;
+        for (int pa = k; pa >= 0; pa--){
+            int pb = k-pa;
+            if (smt % ((pa+1)*(pb+1)) != 0) continue;
+            if (check(pa, pb)){
+                vector<int> tmp;
+                REP(i, pa){
+                    tmp.pb(A[i]);
+                }
+                REP(i, pb){
+                    tmp.pb(B[i]+n-1);
+                }
+                cands.pb(tmp);
+            }
+        }
+
+        sort(ALL(cands));
+        if (SZ(cands) == 0){
+            cout<<"Impossible"<<endl;
+        }
+        else {
+            REP(i, k) cout<<cands[0][i]<<' ';
+            cout<<endl;
+        }
+    }
+
 }
