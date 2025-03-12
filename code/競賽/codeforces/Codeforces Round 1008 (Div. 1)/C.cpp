918d87d3e0a3b8272e0d7203df6a8fc2ff7eb82f
huangallen <huangallen2008@gmail.com>
2025-03-12 02:28:28 +0000

    37112ing

--- code/競賽/codeforces/Codeforces Round 1008 (Div. 1)/C.cpp
+++ code/競賽/codeforces/Codeforces Round 1008 (Div. 1)/C.cpp
@@ -49,126 +49,60 @@ istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;ret
 template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=998244353;
-const int maxn=5;
+const int maxn=2e5+5;
 const int maxb=64;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-struct SEG {
-    struct Seg {
-        int len=0,s=0,ts=0,t=0;
-    };
-    void addtag(Seg &s,int v) {
-        s.ts=(s.ts+2*v*s.s+v*v*s.len)%mod;
-        s.s=(s.s+v*s.len)%mod;
-        s.t=(s.t+v)%mod;
+int pw(int x,int p) {
+    int r=1;
+    while(p>0) {
+        if(p&1) r=r*x%mod;
+        x=x*x%mod;
+        p>>=1;
     }
-    void push(Seg &a,Seg &b,Seg &c) {
-        addtag(b,a.t);
-        addtag(c,a.t);
-        a.t=0;
-    }
-    Seg merge(Seg b,Seg c) {
-        Seg a;
-        a.len=b.len+c.len;
-        a.s=(b.s+c.s)%mod;
-        a.ts=(b.ts+c.ts)%mod;
-        return a;
-    }
-    void pull(Seg &a,Seg &b,Seg &c) {
-        a=merge(b,c);
-    }
-    int n;
-    vector<Seg> s;
-    int ttt=0;
-    void build(int w,int l,int r,Vi &ps,int __=0) {
-        // op(w)op(l)op(r)ope(__)ope(ttt)
-        // if(ttt++>10) exit(0);
-        if(l==r) {
-            s[w].s=ps[l];
-            s[w].ts=(ps[l]*ps[l])%mod;
-            s[w].t=0;
-            return;
-        }
-        int m=l+r>>1;
-        build(w<<1,l,m,ps,__+1);
-        build(w<<1|1,m+1,r,ps,__+1);
-        pull(s[w],s[w<<1],s[w<<1|1]);
-    }
-    void init(int _n,Vi &ps) {
-        // ttt=0;
-        n=_n;
-        s=vector<Seg>(n<<2);
-        build(1,0,n-1,ps);
-    }
-    void _ud(int w,int l,int r,int ql,int qr,int v) {
-        if(ql<=l&&r<=qr) {
-            addtag(s[w],v);
-            return;
-        }
-        if(ql>r||qr<l) return;
-        int m=l+r>>1;
-        push(s[w],s[w<<1],s[w<<1|1]);
-        _ud(w<<1,l,m,ql,qr,v);
-        _ud(w<<1|1,m+1,r,ql,qr,v);
-        pull(s[w],s[w<<1],s[w<<1|1]);
-    }
-    void ud(int l,int r,int v) {
-        _ud(1,0,n-1,l,r,v);
-    }
-    Seg _qu(int w,int l,int r,int ql,int qr) {
-        if(ql<=l&&r<=qr) return s[w];
-        if(ql>r||qr<l) return {0,0,0,0};
-        int m=l+r>>1;
-        push(s[w],s[w<<1],s[w<<1|1]);
-        return merge(_qu(w<<1,l,m,ql,qr),_qu(w<<1|1,m+1,r,ql,qr));
-    }
-    pii qu(int l,int r) {
-        Seg ret=_qu(1,0,n-1,l,r);
-        return {ret.s,ret.ts};
-    }
-};
-int f(int x) {
-    if(x<0) x=-x;
-    return (x>>1)*(x+1>>1); 
+    return r;
 }
-int updv(int s,int d,int n,int k) {
-    int k1=k>>1;
-    return (s+k1*d+(k1-1)*k1*n)%mod;
+int inv(int x) { return pw(x,mod-2); }
+Vi fac(maxn),infac(maxn);
+void com_init() {
+    fac[0]=1;
+    REP1(i,maxn-1) fac[i]=fac[i-1]*i%mod;
+    infac[maxn-1]=inv(fac[mxn-1]);
+    RREP(i,maxn-1) infac[i]=infac[i+1]*(i+1)%mod;
+}
+int C(int n,int k) {
+    return (fac[n]*infac[k]%mod)*infac[n-k]%mod;
+}
+int cost(int x) {
+    x=abs(x);
+    return (x>>1)*(x+1>>1);
+}
+int cost_sum(int x,int y) {
+    int an=0;
+    REP(i,x+1) REP(j,y+1) an+=cost(i-j);
+    return an;
 }
 void solve() {
     int n,q;
     cin>>n>>q;
-    ope(n)ope(q)
-    string s;
-    cin>>s;
-    ope(s)
-    Vi ps(n);
-    ps[0]=s[0]=='1'?1:-1;
-    REP1(i,n-1) ps[i]=ps[i-1]+(s[i]=='1'?1:-1);
-    SEG seg;
-    seg.init(n,ps);
-    Vi psum(n),pds(n);
-    REP(i,n) {
-        psum[i]=((i?psum[i-1]:0)+f(ps[i]))%mod;
-        pds[i]=(i?pds[i-1]:0)+f(ps[i]+2)-f(ps[i]);
-    }
-    oparr(ps)
-    oparr(psum)
-    oparr(pds)
-    int an=0;
-    REP1(i,n-1) {
-        addmod(an,updv(psum[i-1],pds[i-1],i,-ps[i]));
-        ope(an)
-    }
-    ope(an)
+    string s0;
+    cin>>s0;
+    Vi s(n);
+    REP(i,n) s[i]=s0[i]-'0';
+    Vi cnt(2);
+    REP(i,n) cnt[s[i]]++;
     REP(i,q) {
-        int x;
-        cin>>x;
+        int u;
+        cin>>u,u--;
+        cnt[s[u]]--;
+        s[u]^=1;
+        cnt[s[u]]++;
+        if(n<4) cout<<cost_sum(cnt[0],cnt[1])<<'\n';
+        else cout<<(pw(2,n-4)*((cnt[0]-cnt[1])*(cnt[0]-cnt[1])+n-2)%mod)<<'\n';
     }
-    ope("ok")
 }
 signed main() {
     IOS();
