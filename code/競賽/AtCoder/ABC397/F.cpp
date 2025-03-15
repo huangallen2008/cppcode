1772e5b3db57ee0c4b07208757615d01e4625555
huangallen <huangallen2008@gmail.com>
2025-03-15 12:48:54 +0000

    67415ing

---
+++ code/競賽/AtCoder/ABC397/F.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
+const int mod=998244353;
+const int maxn=2e5+5;
+const int maxv=1e7+5;
+const int inf=1ll<<60;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+struct SEG {
+    struct Seg {
+        int mx,t;
+    };
+    void addtag(Seg &x,int t) {
+        x.mx+=t;
+        x.t+=t;
+    }
+    void push(Seg &a,Seg &b,Seg &c) {
+        addtag(b,a.t);
+        addtag(c,a.t);
+        a.t=0;
+    }
+    void pull(Seg &a,Seg &b,Seg &c) {
+        a.mx=max(b.mx,c.mx);
+    }
+    int n;
+    vector<Seg> s;
+    void init(int _n) {
+        n=_n;
+        s=vector<Seg>(n<<2);
+    }
+    void _ud(int w,int l,int r,int ql,int qr,int v) {
+        if(ql<=l&&r<=qr) {
+            addtag(s[w],v);
+            return;
+        }
+        if(ql>r||qr<l) return;
+        int m=l+r>>1;
+        push(s[w],s[w<<1],s[w<<1|1]);
+        _ud(w<<1,l,m,ql,qr,v);
+        _ud(w<<1|1,m+1,r,ql,qr,v);
+        pull(s[w],s[w<<1],s[w<<1|1]);
+    }
+    void ud(int l,int r,int v) {
+        _ud(1,0,n-1,l,r,v);
+    }
+    void _qu(int w,int l,int r,int ql,int qr) {
+        if(ql<=l&&r<=qr) return s[w].mx;
+        if(ql>r||qr<l) return 0;
+        int m=l+r>>1;
+        push(s[w],s[w<<1],s[w<<1|1]);
+        return max(_qu(w<<1,l,m,ql,qr),_qu(w<<1|1,m+1,r,ql,qr));
+    }
+    void qu(int l,int r) {
+        return _qu(1,0,n-1,l,r,v);
+    }
+}seg;
+signed main() {
+    IOS();
+    int n;
+    cin>>n;
+    Vi a(n);
+    REP(i,n) cin>>a[i];
+    return 0;
+}
