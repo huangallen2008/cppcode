edc886ca098ddd481606b0b2229fe1eb48a70067
huangallen <huangallen2008@gmail.com>
2025-03-21 01:07:21 +0000

    58021ing

---
+++ code/CSacademy/fake-coins/fake-coins.cpp
@@ -0,0 +1,150 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
+const int mod=998244353;
+const int maxn=2e5+5;
+const int maxv=2e5+5;
+const int mx=1e9+5;
+const int inf=1ll<<60;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+int qur(Vi a,Vi b) {
+    cout<<"Q "<<SZ(a)<<' '<<SZ(b)<<' ';
+    for(int x:a) cout<<x<<' ';
+    for(int x:b) cout<<x<<' ';
+    cout<<'\n'<<flush;
+    int ret;
+    cin>>ret;
+    return ret;
+}
+void ans(int a,int b) {
+    cout<<"A "<<a<<' '<<b<<'\n'<<flush;
+    exit(0);
+}
+int solve(Vi a,bool big) {
+    // oparr(a)
+    if(SZ(a)==1) return a[0]; 
+    // if(SZ(a)==2) {
+    //     return qur({a[0]},{a[1]})==big?a[1]:a[0]; 
+    // }
+    Vi v0,v1,v2;
+    int n=a.size();
+    int nn=(n+1)/3;
+    REP(i,n) {
+        if(i<nn) v0.pb(a[i]);
+        else if(i<nn*2) v1.pb(a[i]);
+        else v2.pb(a[i]); 
+    }
+    int ret=qur(v0,v1);
+    if(ret==0) return solve(v2,big);
+    else if(big?ret==-1:ret==1) return solve(v1,big);
+    else return solve(v0,big);
+}
+signed main() {
+    IOS();
+    int n;
+    cin>>n;
+    if(n&1) {
+        int r1=qur({1},{2});
+        if(r1!=0) {
+            int r2=qur({1},{3});
+            if(r1==-1&&r2==1) {
+                ans(3,2);
+            }else if(r1==1&&r2==-1) {
+                ans(2,3);
+            }else if(r1==1&&r2==1) {
+                Vi id;
+                REP1(i,n) if(i!=1) id.pb(i);
+                ans(solve(id,0),1);
+            }else if(r1==-1&&r2==-1) {
+                Vi id;
+                REP1(i,n) if(i!=1) id.pb(i);
+                ans(1,solve(id,1));
+            }
+            // else {
+            //     int r3=qur({2},{3});
+            //     if(r1==-1&&r3==-1) {
+            //         ans(1,3);
+            //     }else if(r1==1&&r2==1) {
+            //         ans(3,1);
+            //     }else if(r1==-1&&r2==1) {
+            //         Vi id;
+            //         REP1(i,n) if(i!=2) id.pb(i);
+            //         ans(solve(id,0),2);
+            //     }else if(r1==1&&r2==-1) {
+            //         Vi id;
+            //         REP1(i,n) if(i!=2) id.pb(i);
+            //         ans(1,solve(id,2));
+            //     }
+            // }
+        }
+    }
+    Vi id;
+    for(int i=(n&1)+1;i<=n;i++) id.pb(i);
+    int cnt=0;
+    while(1) {
+        shuffle(ALL(id),rng);
+        int nn=n/2;
+        Vi v1,v2;
+        REP(i,n) {
+            if(i<nn) v1.pb(id[i]);
+            else v2.pb(id[i]);
+        }
+        int ret=qur(v1,v2);
+        cnt++;
+        assert(cnt<=11);
+        if(ret==0) continue;
+        if(ret==-1) ans(solve(v1,0),solve(v2,1));
+        if(ret==1) ans(solve(v2,0),solve(v1,1));
+    }
+    return 0;
+}
--- code/CSacademy/permutation-towers/permutation-tower_3.cpp
+++ code/CSacademy/permutation-towers/permutation-tower_3.cpp
@@ -48,36 +48,63 @@ template<typename S>
 istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
 template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
-const int mod=998244353;
+// const int mod=998244353;
 const int maxn=2e5+5;
-const int mx=1e9+5;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+int n,x,mod;
+int pw(int x,int p) {
+    int r=1;
+    while(p>0) {
+        if(p&1) r=r*x%mod;
+        x=x*x%mod;
+        p>>=1;
+    }
+    return r;
+}
+int inv(int x) { return pw(x,mod-2); }
+Vi fac(maxn),infac(maxn);
+void com_init() {
+    fac[0]=1;
+    REP1(i,maxn-1) fac[i]=fac[i-1]*i%mod;
+    infac[maxn-1]=inv(fac[maxn-1]);
+    RREP(i,maxn-1) infac[i]=infac[i+1]*(i+1)%mod;
+}
+int C(int n,int k) {
+    return (fac[n]*infac[k]%mod)*infac[n-k]%mod;
+}
+int H(int n,int k) { return C(n+k-1,k); }
 signed main() {
     IOS();
-    int T;
-    cin>>T;
-    while(T--) {
-        int n,m,k;
-        cin>>n>>m>>k;
-        Vi a(n),b(m);
-        REP(i,n) cin>>a[i];
-        REP(i,m) cin>>b[i];
-        // auto lcm=[&](int a,int b) { return a*b/__gcd(a,b); };
-        // ope(lcm(8,8))
-        int c1=n,c2=m;
-        
-        REP(i,n) if(a[i]%k==0||(a[i]>=k&&__gcd(k,a[i])!=a[i]&&__gcd(k,a[i])!=1)) c1--;
-        REP(i,m) if(b[i]%k==0||(b[i]>=k&&__gcd(k,b[i])!=b[i]&&__gcd(k,b[i])!=1)) c2--;
-        // REP(i,m) if(b[i]%k==0||__gcd(k,b[i])!=b[i]) c2--;
-        // REP(i,n) if(lcm(k,b[i])!=b[i]) c2--;
-        cout<<c1*c2<<'\n';
+    cin>>n>>x>>mod;
+    com_init();
+    vector<Vi> dp(1,Vi(2));
+    dp[0][0]=1;
+    REP1(i,n) {
+        int K=(i+1)*(i+2),pK=i*(i+1);
+        int J=i+1,pJ=i;
+        vector<Vi> ndp(J,Vi(K));
+        REP(j,pJ) {
+            REP(k,pK) {
+                if(j+1<J)addmod(ndp[j+1][k],dp[j][k]);
+                if(j-1>=0&&k+i*2<K)addmod(ndp[j-1][k+i*2],dp[j][k]*j*(j-1));
+                // op(i)op(j)ope(k)op(J)ope(K)
+                if(k+i<K)addmod(ndp[j][k+i],dp[j][k]*j*2);
+            }
+        }
+        swap(ndp,dp);
+        // ope(i)oparr(dp)
     }
+    int an=0;
+    int N=n*n;
+    // ope(N)
+    for(int i=0;i<N&&i<x;i++) {
+        addmod(an,dp[1][i]*H(n+1,x-i-1));
+    }
+    an=(an+mod)%mod;
+    cout<<an<<'\n';
     return 0;
 }
-/*
-
-*/
