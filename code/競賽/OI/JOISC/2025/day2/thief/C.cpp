bc1472871828192fffcfffa543de5ffde5a9cd9d
huangallen <huangallen2008@gmail.com>
2025-03-22 06:31:41 +0000

    68122ing

--- code/競賽/OI/JOISC/2025/day2/thief/C.cpp
+++ code/競賽/OI/JOISC/2025/day2/thief/C.cpp
@@ -69,7 +69,7 @@ void solve(int n, int m, std::vector<int> U, std::vector<int> V) {
             Vi vv(n-1);
             REP(i,m) vv[i]=i>=mb;
             if(!r1) reverse(ALL(vv));
-            op(mb)ope(query(vv))
+            // op(mb)ope(query(vv))
             if(query(vv))rb=mb;
             else lb=mb+1;
         }
@@ -82,7 +82,7 @@ void solve(int n, int m, std::vector<int> U, std::vector<int> V) {
             Vi vv(n-1);
             REP(i,n-1) vv[i]=i<mb;
             if(!r1) reverse(ALL(vv));
-            op(mb)ope(query(vv))
+            // op(mb)ope(query(vv))
             if(query(vv))lb=mb;
             else rb=mb-1;
         }
