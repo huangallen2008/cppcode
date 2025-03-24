75fdd8675343b1588180dd43405f8c96edf3ed4e
huangallen <huangallen2008@gmail.com>
2025-03-24 08:05:14 +0000

    18324ing

--- code/競賽/OI/TOI/2025/mock1/1D (1)/1D/cpp/1D.cpp
+++ code/競賽/OI/TOI/2025/mock1/1D (1)/1D/cpp/1D.cpp
@@ -94,6 +94,7 @@ signed maximum_length(const std::vector<signed> &UX, const std::vector<signed> &
         oparr(tx)oparr(ax)oparr(bx)
         int mx=0,an=0;
         REP(i,N-1) chmax(mx,ax[i]+bx[i]);
+        ope(mx)
         REP(i,N-1) if(ax[i]+bx[i]==mx) an+=tx[i+1]-tx[i];
         op(dd)ope(an)
         return an;
