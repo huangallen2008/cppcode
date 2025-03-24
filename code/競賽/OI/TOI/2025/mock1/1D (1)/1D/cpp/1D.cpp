4e785a24f6ed9377054be92c0e6f06cf15ad5145
huangallen <huangallen2008@gmail.com>
2025-03-24 07:43:51 +0000

    18324ing

--- code/競賽/OI/TOI/2025/mock1/1D (1)/1D/cpp/1D.cpp
+++ code/競賽/OI/TOI/2025/mock1/1D (1)/1D/cpp/1D.cpp
@@ -1,17 +1,6 @@
-#include <vector>
-
-int maximum_length(const std::vector<int> &UX, const std::vector<int> &UY, 
-                   const std::vector<int> &DX, const std::vector<int> &DY) {
-    int n = UX.size() / 2;
-    int m = DX.size() / 2;
-
-    return 4;
-}
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,avx,bmi,bmi2,abm,sse2,sse3,sse4.1,sse4.2")
-// #define int long long
+#define int long long
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define REP1(i,n) for(int i=1;i<=(n);i++)
 #define pb push_back
@@ -60,7 +49,7 @@ signed maximum_length(const std::vector<signed> &UX, const std::vector<signed> &
     clock_t st=clock();
     Vi ux(n*2),uy(n*2),dx(m*2),dy(m<<1);
     REP(i,n*2) ux[i]=UX[i],uy[i]=UY[i];
-    // Vpii b(m*2);
+    Vpii b(m*2);
     REP(i,m*2) dx[i]=DX[i],dy[i]=DY[i];
     int umxy=*max_element(ALL(uy)),dmxy=-*min_element(ALL(dy));
     int umnx=ux[0],umxx=ux.back();
@@ -104,60 +93,5 @@ signed maximum_length(const std::vector<signed> &UX, const std::vector<signed> &
     };
     int an=0;
     for(int dd=-dn;dd<=un;dd++) chmax(an,cal(dd));
-    // auto va=[&](int x) {
-    //     if(x<0) return -inf;
-    //     return prev(lower_bound(ALL(a),pii{x,inf}))->s;
-    // };
-    // auto vb=[&](int x) {
-    //     if(x<0) return -inf;
-    //     return prev(lower_bound(ALL(b),pii{x,inf}))->s;
-    // };
-    // oparr(a)oparr(b)
-    // Vpii apple;
-    // REP(i,n+1) REP(j,m+1) apple.pb({i,j});
-    // shuffle(ALL(apple),rng);
-    // int ccc=0;
-    // int mxcc=150000000/(n+m);
-    // for(auto [i,j]:apple) {
-    //     if(ccc++>mxcc) break;
-    //     // ope((double)clock()/CLOCKS_PER_SEC)
-    //     // if((long double)(clock()-st)/CLOCKS_PER_SEC>=0.9) break;
-    //     // op(i)ope(j)
-    //         // #ifdef LOCAL
-    //         // if(i||j) continue;
-    //         // #endif
-    //         int cnt=0;
-    //         Vi tx;
-    //         REP(k,n+1) tx.pb(a[k].f-a[i].f);
-    //         REP(k,m+1) tx.pb(b[k].f-b[j].f);
-    //         sort(ALL(tx));
-    //         // oparr(tx)
-    //         int mx=0;
-    //         int nn=SZ(tx);
-    //         Vi va(nn-1),vb(nn-1);
-    //         int it=0;
-    //         REP(k,nn-1) {
-    //             int val=tx[k]+a[i].f;
-    //             while(it<n+1&&a[it].f<=val) it++;
-    //             if(it==0||it==n+1) va[k]=-inf;
-    //             else va[k]=a[it-1].s;
-    //         }
-    //         it=0;
-    //         REP(k,nn-1) {
-    //             int val=tx[k]+b[j].f;
-    //             while(it<m+1&&b[it].f<=val) it++;
-    //             if(it==0||it==m+1) va[k]=-inf;
-    //             else vb[k]=b[it-1].s;
-    //         }
-    //         REP(k,nn-1) {
-    //             chmax(mx,va[k]+vb[k]);
-    //             // op(k)op(va(tx[k]+a[i].f))ope(vb(tx[k]+b[j].f));
-    //         }
-    //         REP(k,nn-1) {
-    //             if(va[k]+vb[k]==mx) cnt+=tx[k+1]-tx[k];
-    //         }
-    //         chmax(an,cnt);
-    //         // op(i)op(j)ope(cnt)
-    // }
     return an;
 }
