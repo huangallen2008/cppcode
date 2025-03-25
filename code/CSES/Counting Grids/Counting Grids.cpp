c3f95902ada8d85900c4dcd14e966ddcaaada65b
huangallen <huangallen2008@gmail.com>
2025-03-25 03:18:48 +0000

    28425ing

--- code/CSES/Counting Grids/Counting Grids.cpp
+++ code/CSES/Counting Grids/Counting Grids.cpp
@@ -1,29 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
 #define int long long
-#define REP(i,n) for(int i=0;i<n;i++)
-#define REP1(i,n) for(int i=1;i<=n;i++)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
 #define RREP1(i,n) for(int i=(n);i>=1;i--)
 #define f first
 #define s second
 #define pb push_back
-#define ALL(x) x.begin(),x.end()
-#define SZ(x) (int)(x.size())
-#define SQ(x) (x)*(x)
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
 #define pii pair<int,int>
 #define pipii pair<int,pii>
 #define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
 #define IOS() ios::sync_with_stdio(0),cin.tie(0)
 #define md(x) (((x)%(mod)+(mod))%(mod))
 #define MD(x,M) (((x)%(M)+(M))%(M))
 #define ld long double
 #define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
 #ifdef LOCAL
 #define op(x) cout<<(#x)<<"="<<(x)<<", ";
 #define ope(x) cout<<(#x)<<"="<<(x)<<endl;
-#define oparr(x) cout<<(#x)<<":";REP(allen,(x).size()) cout<<x[allen]<<" ";cout<<" size="<<(x).size()<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
 #define entr cout<<endl;
 #else
 #define op(x) ;
@@ -31,10 +38,20 @@ using namespace std;
 #define oparr(x) ;
 #define entr ;
 #endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=1e9+7;
-const int maxn=2e5+5;
-const int maxv=1e5+5;
-const int inf=(1ll<<62);
+const int maxn=2e6+5;
+const int mx=1e9+5;
+const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
@@ -42,26 +59,23 @@ int rd(int l,int r) {
 int pw(int x,int p) {
     int r=1;
     while(p>0) {
-        if(p&1) {
-            r*=x;
-            r%=mod;
-        }
-        x*=x;
-        x%=mod;
+        if(p&1) r=r*x%mod;
+        x=x*x%mod;
         p>>=1;
     }
     return r;
-}
-int inv(int x) {
-    return pw(x,mod-2);
 }
 signed main() {
     IOS();
     int n;
     cin>>n;
-    int N=n*n;
-    int a=pw(2,N-2),b=pw(2,N-3>>1),c=pw(2,N-5>>2);
-    int an=md((a+(b<<1)+c*3));
+    if(n==1) {
+        cout<<"2\n";
+        return 0;
+    }
+    int x=(n>>1)*(n+1>>1);
+    int an=pw(2,n*n-2)+pw(2,x*2-2)+pw(2,x-1);
+    an=(an+mod)%mod;
     cout<<an<<'\n';
     return 0;
 }
