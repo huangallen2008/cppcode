d5936674ad8d40c29a347251abf96e16f2ddca88
huangallen <huangallen2008@gmail.com>
2025-03-25 08:52:48 +0000

    28425ing

--- code/CSES/Monotone Subsequences/Monotone Subsequences.cpp
+++ code/CSES/Monotone Subsequences/Monotone Subsequences.cpp
@@ -56,16 +56,21 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-signed main() {
-    IOS();
+void solve() {
     int n,k;
     cin>>n>>k;
     if(k<=(n-1>>1)) {
         cout<<"IMPOSSIBLE\n";
-        return 0;
+        return;
     }
     for(int i=n-k+1;i<=n;i++) cout<<i<<' ';
     for(int i=1;i<=n-k;i++) cout<<i<<' ';
     cout<<'\n';
+}
+signed main() {
+    IOS();
+    int T;
+    cin>>T;
+    while(T--) solve();
     return 0;
 }
