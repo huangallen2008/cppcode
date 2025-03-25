4d0d19a4ea7070b5138b365ea59d2bd0634bc7a9
huangallen <huangallen2008@gmail.com>
2025-03-25 01:48:14 +0000

    28425ing

---
+++ code/Cpp_template/recursive_lambda_function.cpp
@@ -0,0 +1,11 @@
+template<class T>
+struct Func {
+    T func;
+    Func(const T &func):func(func) {}
+    template<class... Args>
+    constexpr decltype(auto) operator()(Args &&... args) {
+        return func(*this, forward<Args>(args)...);
+    }
+};
+template<class T>
+Func(const T &) -> Func<T>;
