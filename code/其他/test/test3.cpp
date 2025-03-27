e07b0f4b9910f48676ed86289b925e808c3c9bc4
huangallen <huangallen2008@gmail.com>
2025-03-27 10:56:17 +0000

    48627ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -69,9 +69,7 @@ signed main() {
     int ans=0;
     REP1(i,n-1) {
         int mx=d[0][i]*2;
-        REP(j,i) {
-            assert((d[0][i]-d[0][j]+d[i][j])%2==0);
-            chmin(mx,(d[0][i]-d[0][j]+d[i][j]));
+        REP(j,i) chmin(mx,(d[0][i]-d[0][j]+d[i][j]));
         }
         ans+=mx;
     }
