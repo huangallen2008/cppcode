bc669eaa56ad901c266d7bdc036c3296849531ac
huangallen <huangallen2008@gmail.com>
2025-03-27 10:43:42 +0000

    48627ing

--- code/其他/test/test3.cpp
+++ code/其他/test/test3.cpp
@@ -62,11 +62,7 @@ signed main() {
     cin>>n;
     vector<Vi> d(n,Vi(n));
     REP(i,n) REP(j,n) cin>>d[i][j];
-    Vi x(3);
-    x[0]=d[0][1],x[1]=d[0][2],x[2]=d[1][2];
-    sort(ALL(x));
     int ban=d[0][1]+d[0][2]+d[1][2];
-    
     int ans=0;
     REP1(i,n-1) {
         int mx=d[0][i];
