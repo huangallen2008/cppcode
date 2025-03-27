1b9323a39bbb3dc7d7744ed0969f421c463bb895
huangallen <huangallen2008@gmail.com>
2025-03-27 02:53:52 +0000

    48627ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -77,9 +77,11 @@ signed main() {
     Vpii v1,v2;
     REP(i,n) {
         while(SZ(v1)>=1&&cros(v1.back()-v1[SZ(v1)-1],a[i]-v1[SZ(v1)-1])) v1.pop_back();
+        v1.pb(a[i]);
     }
     RREP(i,n) {
         while(SZ(v2)>=1&&cros(v2.back()-v2[SZ(v2)-2],a[i]-v2[SZ(v2)-2])) v2.pop_back();
+        v2.pb(a[i]);
     }
     oparr(v1)oparr(v2)
     return 0;
