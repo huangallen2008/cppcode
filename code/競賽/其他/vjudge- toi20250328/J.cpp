32c9e7911ef2dedcdb8027e2a35c68a4cbd50fd6
huangallen <huangallen2008@gmail.com>
2025-03-28 02:27:17 +0000

    58728ing

---
+++ code/競賽/其他/vjudge- toi20250328/J.cpp
@@ -0,0 +1,128 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
+const int mod=998244353;
+const int maxn=2e5+5;
+const int maxv=1e6+5;
+const int inf=1e9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+struct DSU {
+    int n;
+    Vi p,v,sz;
+    void init(int _n) {
+        n=_n;
+        p=v=Vi(n);
+        sz=Vi(n,1);
+        REP(i,n) p[i]=i;
+    }
+    int find(int u) {
+        return p[u]==u?u:p[u]=find(p[u]);
+    }
+    void merge(int a,int b) {
+        int x=find(a),y=find(b);
+        if(x==y) return;
+        if(sz[x]>sz[y]) swap(x,y);
+        p[x]=y;
+        sz[y]+=sz[x];
+        v[y]+=v[x];
+    }
+    void addv(int u,int x) {
+        v[find(u)]+=x;
+    }
+    void addsz(int u,int x) { 
+        sz[find(u)]+=x;
+    }
+    pii getan(int u) {
+        int x=find(u);
+        return {sz[x],v[x]};
+    }
+    bool same(int a,int b) { return find(a)==find(b); }
+}dsu;
+signed main() {
+    IOS();
+    int n,m;
+    while(cin>>n>>m) {
+        dsu.init(n+m+5);
+        int nid=n+1;
+        REP1(i,n) dsu.addv(i,i);
+        Vi id(n+1);
+        REP1(i,n) id[i]=i;
+        REP(i,m) {
+            int opt;
+            cin>>opt;
+            if(opt==1) {
+                int p,q;
+                cin>>p>>q;
+                dsu.merge(id[p],id[q]);
+            }else if(opt==2) {
+                int p,q;
+                cin>>p>>q;
+                if(dsu.same(id[p],id[q])) continue;
+                dsu.addv(id[p],-p);
+                dsu.addsz(id[p],-1);
+                id[p]=nid++;
+                dsu.addv(id[p],p);
+                dsu.merge(id[p],id[q]);
+            }else {
+                int p;
+                cin>>p;
+                pii an=dsu.getan(id[p]);
+                cout<<an.f<<' '<<an.s<<'\n';
+            }
+            // oparr(dsu.p)
+            // oparr(dsu.v)
+            // oparr(dsu.sz)
+        }
+    }
+    return 0;
+}
