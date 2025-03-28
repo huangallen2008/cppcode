f41ce627bc3116d35d8fb4020ce2c0b50eebd623
huangallen <huangallen2008@gmail.com>
2025-03-28 03:35:57 +0000

    58728ing

--- code/競賽/其他/vjudge- toi20250328/D.cpp
+++ code/競賽/其他/vjudge- toi20250328/D.cpp
@@ -128,13 +128,13 @@ void solve(int n) {
             for(int j=idl;j<=idr;j++) dsu.merge(i,v[j].s);
         }
         {
+            ope(i)
             auto &v=ml[x2];
             int idr=lower_bound(ALL(v),piii{{y2,inf},inf})-v.begin();
             auto &v2=mlb[x2];
             int idl=lower_bound(ALL(v2),piii{{y1,-inf},-inf})-v.begin();
             for(int j=idl;j<=idr;j++) dsu.merge(i,v[j].s);
         }
-        ope(i)
         {
             auto &v=md[y1];
             int idr=lower_bound(ALL(v),piii{{x2,inf},inf})-v.begin();
