994a23a01598fa270507ce2c997c37032e3e115b
huangallen <huangallen2008@gmail.com>
2025-03-28 03:38:37 +0000

    58728ing

--- code/競賽/其他/vjudge- toi20250328/D.cpp
+++ code/競賽/其他/vjudge- toi20250328/D.cpp
@@ -141,7 +141,6 @@ void solve(int n) {
             }
             // entr
         }
-        ope(i)
         {
             auto &v=md[y1];
             int idr=lower_bound(ALL(v),piii{{x2,inf},inf})-v.begin();
@@ -149,6 +148,7 @@ void solve(int n) {
             int idl=lower_bound(ALL(v2),piii{{x1,-inf},-inf})-v.begin();
             for(int j=idl;j<idr;j++) dsu.merge(i,v[j].s);
         }
+        ope(i)
         {
             auto &v=mu[y2];
             int idr=lower_bound(ALL(v),piii{{x2,inf},inf})-v.begin();
