cac6b74d45efaf47f5fd92313296f5106bd6526f
huangallen <huangallen2008@gmail.com>
2025-03-28 03:21:16 +0000

    58728ing

--- code/競賽/其他/vjudge- toi20250328/D.cpp
+++ code/競賽/其他/vjudge- toi20250328/D.cpp
@@ -56,7 +56,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-#define piii pia
+#define piii pair<pii,int>
 void solve(int n) {
     
 }
