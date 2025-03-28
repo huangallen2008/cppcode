eb708621627c16e7b9d86ac5707cfbccfe33a05e
huangallen <huangallen2008@gmail.com>
2025-03-28 02:29:21 +0000

    58728ing

--- code/競賽/其他/vjudge- toi20250328/J.cpp
+++ code/競賽/其他/vjudge- toi20250328/J.cpp
@@ -63,7 +63,10 @@ void solve() {
 }
 signed main() {
     IOS();  
-    
+    Vi f(40);
+    f[0]=f[1]=1;
+    for(int i=2;i<40;i++) f[i]=f[i-1]+f[i-2];
+    oparr(f)
     int T;
     cin>>T;
     while(T--) solve();
