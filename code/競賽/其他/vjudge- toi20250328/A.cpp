e896bcbaab050dc82ea24aa598a8f79f51926323
huangallen <huangallen2008@gmail.com>
2025-03-28 01:20:18 +0000

    58728ing

--- code/競賽/其他/vjudge- toi20250328/A.cpp
+++ code/競賽/其他/vjudge- toi20250328/A.cpp
@@ -56,8 +56,13 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+void oslve() {
+
+}
 signed main() {
     IOS();
-
+    int T;
+    cin>>T;
+    while(T--) solve();
     return 0;
 }
