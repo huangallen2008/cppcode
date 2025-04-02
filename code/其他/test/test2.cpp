5ea305f0ae5857d6726bbf0b87f8382672b63ca4
huangallen <huangallen2008@gmail.com>
2025-04-02 14:59:40 +0000

    3922ing

--- code/其他/test/test2.cpp
+++ code/其他/test/test2.cpp
@@ -1,78 +1,51 @@
-#include<bits/stdc++.h>
-using namespace std;
-// #pragma GCC optimize("O3,unroll-loops")
-// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#include <bits/stdc++.h>
+#include <queue>
+#include <vector>
+#include <cmath>
 #define int long long
-#define REP(i,n) for(int i=0;i<(n);i++)
-#define REP1(i,n) for(int i=1;i<=(n);i++)
-#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
-#define RREP1(i,n) for(int i=(n);i>=1;i--)
-#define f first
-#define s second
-#define pb push_back
-#define ALL(x) (x).begin(),(x).end()
-#define SZ(x) ((int)((x).size()))
-#define SQ(x) ((x)*(x))
-#define pii pair<int,int>
-#define pipii pair<int,pii>
-#define Graph vector<vector<int>>
-#define Graphw vector<vector<pii>>
-#define IOS() ios::sync_with_stdio(0),cin.tie(0)
-#define md(x) (((x)%(mod)+(mod))%(mod))
-#define MD(x,M) (((x)%(M)+(M))%(M))
-#define ld long double
-#define pdd pair<ld,ld>
-#define chmax(x,y) x=max(x,y)
-#define chmin(x,y) x=min(x,y)
-#define addmod(x,y) x=((x+(y))%mod)
-#define Vi vector<int>
-#define Vpii vector<pii>
-#ifdef LOCAL
-#define op(x) cout<<(#x)<<"="<<(x)<<", ";
-#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
-#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
-#define entr cout<<endl;
-#else
-#define op(x) ;
-#define ope(x) ;
-#define oparr(x) ;
-#define entr ;
-#endif
-template<typename T1,typename T2>
-ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
-template<typename T1,typename T2>
-istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
-template<typename S>
-ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
-template<typename S>
-istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
-template<typename T1,typename T2>
-pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
-const int mod=998244353;
-const int maxn=2e5+5;
-const int maxb=64;
-const int inf=1ll<<60;
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-int rd(int l,int r) {
-    return uniform_int_distribution<int>(l,r)(rng);
-}
-
+using namespace std;
+const int N = 1e5+7;
+int n, m, a, b;
+vector<int> adj[N], path[N];
+bool ready[N];
+ 
 signed main() {
-    IOS();
-    int mask,n;
-    cin>>mask>>n;
-        // if(mask!=504) continue;
-    int it=0;
-    Graph g(n);
-    REP(i,n) {
-        REP(j,i) {
-            // op(i)op(j)op(it)ope(mask>>it&1)
-            if(mask>>(it++)&1) {
-                g[i].pb(j);
-                g[j].pb(i);
+    //input, fill
+    cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        cin >> a >> b;
+        adj[a].emplace_back(b);
+        adj[b].emplace_back(a);
+    }
+    for(int i = 2; i <= n; i++){
+        ready[i] = false;
+    }
+ 
+    //bfs
+    queue<int> q;
+    q.push(1);
+    path[1].push_back(1);
+    ready[1] = true;
+    while(!q.empty()){
+        int i = q.front(); q.pop();
+        for(int j: adj[i]){
+            if(!ready[j]){
+                ready[j] = true;
+                path[j] = path[i];
+                path[j].emplace_back(j);
+                q.push(j);
             }
         }
     }
-    oparr(g)
+ 
+    //consider
+    if(!ready[n]) cout << "IMPOSSIBLE" << '\n';
+    else{
+        cout << path[n].size() << '\n';
+        cout << 1;
+        for(int k: path[n]) if(k != 1) cout << ' ' << k;
+        cout << '\n';
+    }
+ 
     return 0;
 }
