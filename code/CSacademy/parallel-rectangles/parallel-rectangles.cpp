7985d058b33400562db078400d29d92369b0585f
huangallen <huangallen2008@gmail.com>
2025-04-02 14:21:51 +0000

    3922ing

--- code/CSacademy/parallel-rectangles/parallel-rectangles.cpp
+++ code/CSacademy/parallel-rectangles/parallel-rectangles.cpp
@@ -83,6 +83,25 @@ signed main() {
             an+=cnt*(cnt-1)>>1;
         }
     }
-    
+    for(int x:bid) {
+        Vi is(maxv);
+        for(int t:a[x]) is[t]=1;
+        REP(i,maxv) {
+            if(isb[i]) continue;
+            int cnt=0;
+            for(int t:a[x2]) cnt+=is[t];
+            an+=cnt*(cnt-1)>>1;
+        }
+    }
+    map<int,int> mp;
+    REP(i,maxv) {
+        if(isb[i]) continue;
+        REP(j,SZ(a[i])) {
+            REP(k,j) {
+                an+=mp[a[i][k]*maxv+a[i][j]]++;
+            }
+        }
+    }
+    cout<<an<<'\n';
     return 0;
 }
