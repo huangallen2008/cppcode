ae9cb02ed78008883c71f9dc82c599a1d366fa88
huangallen <huangallen2008@gmail.com>
2025-04-02 01:32:34 +0000

    3922ing

--- code/CSacademy/cntgigelmat/cntgigelmat.cpp
+++ code/CSacademy/cntgigelmat/cntgigelmat.cpp
@@ -57,13 +57,13 @@ int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
 struct BIT {
-    
+
 }
 signed main() {
     IOS();
     int n,m;
     cin>>n>>m;
-    vector<Vi> a(n+2,Vi(m+1));
+    vector<Vi> a(n+2,Vi(m+2));
     REP1(i,n) REP1(j,m) cin>>a[i][j];
     vector<Vi> cl,cr,cu,cd,clu,crd,c1,c2;
     cl=cr=cu=cd=clu=crd=c1=c2=vector<Vi>(n+2,Vi(m+2));
