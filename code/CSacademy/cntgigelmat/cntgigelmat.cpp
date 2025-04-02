87b99aa32c007f1012dfe2e25247a11ef02f0aa2
huangallen <huangallen2008@gmail.com>
2025-04-02 01:49:39 +0000

    3922ing

--- code/CSacademy/cntgigelmat/cntgigelmat.cpp
+++ code/CSacademy/cntgigelmat/cntgigelmat.cpp
@@ -118,10 +118,12 @@ signed main() {
     int n,m;
     cin>>n>>m;
     vector<Vi> a(n+2,Vi(m+2));
-    REP1(i,n) REP1(j,m) {
+    REP1(i,n) {
+        REP1(j,m) {
         char c;
         cin>>c;
         a[i][j]=c=='1';
+    }
     }
     vector<Vi> cl,cr,cu,cd,clu,crd,c1,c2;
     cl=cr=cu=cd=clu=crd=c1=c2=vector<Vi>(n+2,Vi(m+2));
