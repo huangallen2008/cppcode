a292bba6fce0dbed5412ad22c236ead076a69922
huangallen <huangallen2008@gmail.com>
2025-04-03 09:06:57 +0000

    4933ing

--- code/CSES/Graph Paths I/Graph Paths I.cpp
+++ code/CSES/Graph Paths I/Graph Paths I.cpp
@@ -57,7 +57,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-#define mat 
+#define Mat vector<Vi>
+
 signed main() {
     IOS();
 
