382f6a06fb81989c53a3b90a7654afce61f3eaf5
huangallen <huangallen2008@gmail.com>
2025-04-03 12:58:02 +0000

    4933ing

--- code/CSES/Moving Robots/Moving Robots.cpp
+++ code/CSES/Moving Robots/Moving Robots.cpp
@@ -61,8 +61,8 @@ int rd(int l,int r) {
 #define Mat vector<Vld>
 Mat operator*(Mat a,Mat b) {
     int n=SZ(a),m=SZ(b),k=SZ(b[0]);
-    Mat c(n,Vi(k));
-    REP(i,n) REP(j,m) REP(l,k) addmod(c[i][l],a[i][j]*b[j][l]);
+    Mat c(n,Vld(k));
+    REP(i,n) REP(j,m) REP(l,k) c[i][l]+=a[i][j]*b[j][l]);
     return c;
 }
 Mat pw(Mat x,int p) {
