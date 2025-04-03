d31f8ba2dd4cbf7ca91f750b408416393a1ae441
huangallen <huangallen2008@gmail.com>
2025-04-03 07:16:56 +0000

    4933ing

--- code/CSacademy/subinterval-division/subinterval-division.cpp
+++ code/CSacademy/subinterval-division/subinterval-division.cpp
@@ -86,7 +86,7 @@ signed main() {
     REP(i,SZ(stk)) {
         an[stk[i].s]=(i==SZ(stk)-1?x:stk[i+1].f)-stk[i].f;
     }
-    oparr(stk)
+    // oparr(stk)
     REP(i,n) cout<<an[i]<<'\n';
     return 0;
 }
