23ce82cf288e6c2ea71ead94ff4336a129bc68bf
huangallen <huangallen2008@gmail.com>
2025-04-03 16:36:16 +0000

    5944ing

--- code/CSES/Inversion Probability/Inversion Probability.cpp
+++ code/CSES/Inversion Probability/Inversion Probability.cpp
@@ -86,7 +86,9 @@ signed main() {
     };
     REP(i,n) trans(a[i]);
     ld an=0;
-    REP1(i,maxv-1) REP(j,n+1) an+=dp[i][j].f*dp[i][j].s;\
+    REP1(i,maxv-1) REP(j,n+1) an+=dp[i][j].f*dp[i][j].s;
+    cout<<fixed<<setprecision(10) <<an<<'\n';
+    // cout<<fixed<<setprecision(20)<<an*(ld)1e6;
     an=(int)(an*(ld)1e8)/1e8;
     an=nearbyintl(an*(ld)1e6)/1e6;
     cout<<fixed<<setprecision(6)<<an<<'\n';
