02ac7b5387de748ec4ca79798eb678fc077c372d
huangallen <huangallen2008@gmail.com>
2025-04-03 14:45:35 +0000

    4933ing

--- code/CSES/Candy Lottery/Candy Lottery.cpp
+++ code/CSES/Candy Lottery/Candy Lottery.cpp
@@ -75,7 +75,7 @@ signed main() {
     }
     ld an=0;
     REP1(i,k) an+=i*dp[i];
-    cout<<fixed<<setprecision(6)<<an<<'\n';
+    cout<<fixed<<setprecision(10)<<an<<'\n';
     ld ans=round(an*1000000)/1000000;
     cout<<fixed<<setprecision(6)<<ans<<'\n';
     return 0;
