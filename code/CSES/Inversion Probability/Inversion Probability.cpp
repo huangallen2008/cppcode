bfc8e21667ee6649a9d270b6522434cb1d5cd5ab
huangallen <huangallen2008@gmail.com>
2025-04-03 16:01:05 +0000

    5944ing

--- code/CSES/Inversion Probability/Inversion Probability.cpp
+++ code/CSES/Inversion Probability/Inversion Probability.cpp
@@ -72,7 +72,7 @@ signed main() {
         vector<Vpd> ndp(maxv,Vpd(n+1));
         REP(i,maxv) {
             REP(j,n+1) {
-                ndp[i][j].f+=(dp[i][j].f+j)*min(i,v)/v;
+                ndp[i][j].f+=(dp[i][j].f+j)/v;
                 ndp[i][j].s+=dp[i][j].s*min(i,v)/v;
                 if(j<n) {
                     ndp[i][j+1].f+=(dp[i][j].f)*(v-min(i,v))/v;
