38c3b8a150bb853d2b111f5635c4aae72cfcc2c7
huangallen <huangallen2008@gmail.com>
2025-04-03 06:54:53 +0000

    4933ing

---
+++ code/CSacademy/subinterval-division/subinterval-division.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
+const int mod=998244353;
+const int maxn=2e5+5;
+const int maxv=1e5+5;
+const int inf=1ll<<60;
+const int S=70;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+signed main() {
+    IOS();
+    int n;
+    cin>>n;
+    vector<Vi> a(maxv);
+    REP(i,n) {
+        int x,y;
+        cin>>x>>y,x--,y--;
+        a[x].pb(y);
+    }
+    REP(i,maxv) sort(ALL(a[i]));
+    Vi bid;
+    Vi isb(maxv);
+    REP(i,maxv) if(SZ(a[i])>S) bid.pb(i),isb[i]=1;
+    int an=0;
+    REP(i,SZ(bid)) {
+        int x=bid[i];
+        Vi is(maxv);
+        for(int t:a[x]) is[t]=1;
+        REP(j,i) {
+            int cnt=0;
+            int x2=bid[j];
+            for(int t:a[x2]) cnt+=is[t];
+            an+=cnt*(cnt-1)>>1;
+        }
+    }
+    for(int x:bid) {
+        Vi is(maxv);
+        for(int t:a[x]) is[t]=1;
+        REP(i,maxv) {
+            if(isb[i]) continue;
+            int cnt=0;
+            for(int t:a[i]) cnt+=is[t];
+            an+=cnt*(cnt-1)>>1;
+        }
+    }
+
+
+    map<int,int> mp;
+    REP(i,maxv) {
+        if(isb[i]) continue;
+        REP(j,SZ(a[i])) {
+            REP(k,j) {
+                an+=mp[a[i][k]*maxv+a[i][j]]++;
+            }
+        }
+    }
+    cout<<an<<'\n';
+    return 0;
+}
