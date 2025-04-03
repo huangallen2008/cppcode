322f2fdc9585a27e6d6588d001ccfb252cb0342c
huangallen <huangallen2008@gmail.com>
2025-04-03 14:37:48 +0000

    4933ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -57,6 +57,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+#include <quadmath.h>
 #define f128 __float128
 void print(f128 r) {
     char buf[64];
