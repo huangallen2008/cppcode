a8bf1f168d475d793b57dba4f9fd1f13b44ddad4
huangallen <huangallen2008@gmail.com>
2025-04-03 14:43:19 +0000

    4933ing

--- code/CSES/Candy Lottery/Candy Lottery.cpp
+++ code/CSES/Candy Lottery/Candy Lottery.cpp
@@ -22,7 +22,6 @@ using namespace std;
 #define md(x) (((x)%(mod)+(mod))%(mod))
 #define MD(x,M) (((x)%(M)+(M))%(M))
 #define ld long double
-#define f128 __float128
 #define pdd pair<ld,ld>
 #define chmax(x,y) x=max(x,y)
 #define chmin(x,y) x=min(x,y)
@@ -63,10 +62,10 @@ signed main() {
     IOS();
     int n,k;
     cin>>n>>k;
-    vector<f128> dp(k+1);
+    Vld dp(k+1);
     dp[0]=1;
     REP(_,n) {
-        vector<f128> ndp(k+1);
+        Vld ndp(k+1);
         REP(i,k+1) {
             ndp[i]+=dp[i]*i;
             for(int j=i+1;j<=k;j++) ndp[j]+=dp[i];
@@ -74,9 +73,9 @@ signed main() {
         REP1(i,k) ndp[i]/=k;
         swap(dp,ndp);
     }
-    f128 an=0;
+    ld an=0;
     REP1(i,k) an+=i*dp[i];
-    
-    cout<<fixed<<setprecision(6)<<(ld)an<<'\n';
+    ld ans
+    cout<<fixed<<setprecision(6)<<an<<'\n';
     return 0;
 }
