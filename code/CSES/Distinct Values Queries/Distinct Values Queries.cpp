db57a627b7243bee59e4d61516c2d415ce90224a
huangallen <huangallen2008@gmail.com>
2025-04-04 06:06:08 +0000

    5944ing

--- code/CSES/Distinct Values Queries/Distinct Values Queries.cpp
+++ code/CSES/Distinct Values Queries/Distinct Values Queries.cpp
@@ -1,74 +1,74 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
 #define int long long
-#define FOR(i,a,b) for(int i=a;i<b;i++)
-#define REP(i,n) FOR(i,0,n)
-#define REP1(i,n) FOR(i,1,(n)+1)
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
 #define f first
 #define s second
 #define pb push_back
-#define ALL(x) x.begin(),x.end()
-#define SZ(x) (int)(x.size())
-#define SQ(x) (x)*(x)
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
 #define pii pair<int,int>
+#define pipii pair<int,pii>
+#define ppi pair<pii,int>
 #define Graph vector<vector<int>>
-#define IOS() cin.sync_with_stdio(0),cin.tie(0),cout.tie(0)
-const int inf=(1ll<<63)-1;
-const int maxn=2e5+5;
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=1e9+7;
-int n,q;
-vector<int> ua(vector<int> a) {
-    unordered_map<int,int> m;
-    int x=1;
-    REP1(i,n) {
-        if(m[a[i]]==0) {
-            m[a[i]]=x++;
-            a[i]=m[a[i]];
-        }
-        else a[i]=m[a[i]];
-    }
-    return a;
+const int maxn=2e5+5;
+const int maxv=105;
+const int inf=1ll<<60;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
 }
-vector<int> bn(vector<int> a) {
-    vector<int> l(n+1,n+1),nxt(n+1);
-    for(int i=n;i>0;i--) {
-        nxt[i]=l[a[i]];
-        l[a[i]]=i;
-    }
-    return nxt;
+#define pdd pair<ld,ld>
+#define Vpd vector<pdd>
+#define Vld vector<ld>
+ld rhe(ld f){
+    const ld r = round(f);
+    const ld d = r - f;
+    if ((d != 0.5f) && (d != -0.5f))return r;
+    if (fmod(r, 2.0) == 0.0)return r;
+    return f - d;
 }
-int bit[maxn]={};
-void ud(int u,int v) {
-    for(;u<=n;u+=u&-u) bit[u]+=v;
-}
-int qu(int u) {
-    int an=0;
-    for(;u>0;u-=u&-u) an+=bit[u];
-    return an;
-}
-signed main()
-{
+signed main() {
     IOS();
-    cin>>n>>q;
-    vector<int> a(n+1);
-    REP1(i,n) cin>>a[i];
-    a=ua(a);
-    vector<int> next;
-    next=bn(a);
-    vector<vector<pii>> b(n+1);
-    REP(i,q) {
-        int x,y;
-        cin>>x>>y;
-        b[x].pb({y,i});
-    }
-    vector<int> an(q);
-    for(int i=n;i>0;i--) {
-        ud(i,1);
-        if(next[i]<=n) ud(next[i],-1);
-        REP(j,b[i].size()) an[b[i][j].s]=qu(b[i][j].f)-qu(i-1);
-    }
-    REP(i,q) cout<<an[i]<<endl;
+    
     return 0;
 }
