b4d6ff66edfdb8378883fcc39440e05b66ec8c21
huangallen <huangallen2008@gmail.com>
2025-04-04 06:58:51 +0000

    5944ing

--- code/CSES/Stair Game/Stair Game.cpp
+++ code/CSES/Stair Game/Stair Game.cpp
@@ -57,10 +57,14 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+void solve() {
+    
+}
 signed main() {
     IOS();
     int T;
     cin>>T;
+    while(T--) solve();
     int n;
     cin>>n;
     int an=0;
