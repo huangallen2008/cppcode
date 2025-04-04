bb5d3d82dc4010b2d2657824798a33b73aefc1b2
huangallen <huangallen2008@gmail.com>
2025-04-04 06:17:13 +0000

    5944ing

--- code/CSES/Stick Game/Stick Game.cpp
+++ code/CSES/Stick Game/Stick Game.cpp
@@ -57,55 +57,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-#define pdd pair<ld,ld>
-#define Vpd vector<pdd>
-#define Vld vector<ld>
-ld rhe(ld f){
-    const ld r = round(f);
-    const ld d = r - f;
-    if ((d != 0.5f) && (d != -0.5f))return r;
-    if (fmod(r, 2.0) == 0.0)return r;
-    return f - d;
-}
 signed main() {
     IOS();
-    fesetround(FE_TONEAREST);
-    int n;
-    cin>>n;
-    Vi a(n);
-    REP(i,n) cin>>a[i];
-    vector<Vpd> dp(maxv,Vpd(n+1));
-    REP(i,maxv) dp[i][0]={0,1};
-    auto upd=[&](pdd &a,pdd b) ->void{
-        if(b.s==0) return;
-        a={(a.f*a.s+b.f*b.s)/(a.s+b.s),a.s+b.s};
-    };
-    auto trans=[&](int v) {
-        vector<Vpd> ndp(maxv,Vpd(n+1));
-        REP1(i,maxv-1) {
-            REP(j,n) {
-                if(i<=v) upd(ndp[i][j],{dp[i][j].f+j,dp[i][j].s/v});
-                upd(ndp[i][j],{dp[i][j].f,dp[i][j].s*(min(i,v)-(i<=v))/v});
-                upd(ndp[i][j+1],{dp[i][j].f,dp[i][j].s*(v-min(i,v))/v});
-            }
-        }
-        swap(dp,ndp);
-    };
-    REP(i,n) trans(a[i]);
-    ld an=0;
-    REP1(i,maxv-1) REP(j,n+1) an+=dp[i][j].f*dp[i][j].s;
-    // auto rhe=[&](ld &an)->void {
-    //     an+=1e-18;
-    //     cout<<fixed<<setprecision(20) <<an<<'\n';
-    //     int tt=an*1e7;
-    //     // ope(tt)
-    //     if(tt%10==5 && (~(int)(tt/10)&1))
-    //         an-=1e-7;
-    // };
-    cout<<fixed<<setprecision(20)<<an;
-    // // an=(int)(an*(ld)1e8)/1e8;
-    // an=rint(an*(ld)1e6)/1e6;
-    an=rhe(an*1e6)/1e6;
-    cout<<fixed<<setprecision(6)<<an<<'\n';
+    
     return 0;
 }
