300692f21d492aeb2efad1ae16470cb1b51bc8ed
huangallen <huangallen2008@gmail.com>
2025-04-05 17:16:36 +0000

    7966ing

--- code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/E.cpp
+++ code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/E.cpp
@@ -143,6 +143,26 @@ Vi MTT(Vi _A,Vi _B) {
 	}
 	return an;
 }
+int pw(int x,int p) {
+    int r=1;
+    while(p>0) {
+        if(p&1) r=r*x%mod;
+        x=x*x%mod;
+        p>>=1;
+    }
+    return r;
+}
+int inv(int x) { return pw(x,mod-2); }
+Vi fac(maxn),infac(maxn);
+void com_init() {
+    fac[0]=1;
+    REP1(i,maxn-1) fac[i]=fac[i-1]*i%mod;
+    infac[maxn-1]=inv(fac[maxn-1]);
+    RREP(i,maxn-1) infac[i]=infac[i+1]*(i+1)%mod;
+}
+int C(int n,int k) {
+    return (fac[n]*infac[k]%mod)*infac[n-k]%mod;
+}
 void solve() {
     int n;
     cin>>n;
@@ -170,7 +190,7 @@ void solve() {
             }
         }
         int ch=i+1-cnt;
-        
+
     }
 }
 signed main() {
