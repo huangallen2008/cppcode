c45595890df8bbfa30d50f4a6e957cd33e407043
huangallen <huangallen2008@gmail.com>
2025-04-05 14:46:45 +0000

    6955ing

--- code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/B.cpp
+++ code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/B.cpp
@@ -65,12 +65,11 @@ void solve() {
     int mnv=*min_element(ALL(a));
     int gcd=0;
     bool fir=1;
-    REP(i,n-1) {
+    REP(i,n) {
         if(a[i]==mnv&&fir) {
             fir=0;
             continue;
         }
-        ope(a[i])
         if(a[i]%mnv==0) gcd=__gcd(gcd,a[i]);
     }
     cout<<(gcd==mnv?"YES":"NO")<<'\n';
