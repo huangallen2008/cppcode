45e8882450693bb02945bad97dd44b79b093061e
huangallen <huangallen2008@gmail.com>
2025-04-05 17:17:56 +0000

    7966ing

--- code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/E.cpp
+++ code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/E.cpp
@@ -172,7 +172,8 @@ void solve() {
     Vi id(n,-1);
     REP(i,n) if(a[i]!=-1) id[i]=i;
     int mn=inf,mx=-inf;
-    int cnt=0,cntm1;
+    int cnt=0,cntm1=0;
+    REP(i,n) cntm1+=a[i]==-1;
     REP(r,n) {
         if(id[r]!=-1) {
             chmin(mn,id[r]);
