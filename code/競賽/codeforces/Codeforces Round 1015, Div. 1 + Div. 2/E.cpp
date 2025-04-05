330db5f1c60fb1415d864a1fb8f64c8d8b0dfbf9
huangallen <huangallen2008@gmail.com>
2025-04-05 17:37:47 +0000

    7966ing

--- code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/E.cpp
+++ code/競賽/codeforces/Codeforces Round 1015, Div. 1 + Div. 2/E.cpp
@@ -187,7 +187,7 @@ void solve() {
         Vi v(cntm1+1);
         ope(ch)
         REP(i,cntm1+1) {
-            if(i<ch) continue;
+            if(i>ch) continue;
             v[i]=(fac[i]*infac[i-ch]%mod)*fac[cntm1-ch]%mod;
         }
         if(mn==inf) {
