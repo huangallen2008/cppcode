615edb70f7055a56157ec536489478ce622620ce
huangallen <huangallen2008@gmail.com>
2025-04-05 12:32:12 +0000

    6955ing

--- code/競賽/AtCoder/ABC400/D.cpp
+++ code/競賽/AtCoder/ABC400/D.cpp
@@ -90,7 +90,9 @@ signed main() {
                 }
             }else {
                 if(dis[nx][ny]>dis[x][y]+1) {
-                    
+                    dis[nx][ny]=dis[x][y]+1;
+                    dq.push_front({nx,ny});
+
                 }
             }
         }
