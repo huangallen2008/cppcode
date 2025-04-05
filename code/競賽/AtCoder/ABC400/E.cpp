c9b5d43ec888f7265ccc557f3f330a7ae743f4b8
huangallen <huangallen2008@gmail.com>
2025-04-05 12:39:17 +0000

    6955ing

--- code/競賽/AtCoder/ABC400/E.cpp
+++ code/競賽/AtCoder/ABC400/E.cpp
@@ -51,7 +51,7 @@ template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=1e9+7;
 const int maxn=2e5+5;
-const int maxv=1300;
+const int maxv=1e6+5;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
@@ -59,6 +59,6 @@ int rd(int l,int r) {
 }
 signed main() {
     IOS();
-    
+    Vi 
     return 0;
 }
