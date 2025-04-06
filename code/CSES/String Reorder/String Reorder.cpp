79fdc7b396a56bf55536627ca8da3538dcb3ed8e
huangallen <huangallen2008@gmail.com>
2025-04-06 07:19:10 +0000

    7966ing

--- code/CSES/String Reorder/String Reorder.cpp
+++ code/CSES/String Reorder/String Reorder.cpp
@@ -57,45 +57,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-int pw(int x,int p) {
-    int r=1;
-    while(p>0) {
-        if(p&1) r=r*x%mod;
-        x=x*x%mod;
-        p>>=1;
-    }
-    return r;
-}
-int inv(int x) { return pw(x,mod-2); }
-Vi fac(maxn),infac(maxn);
-void com_init() {
-    fac[0]=1;
-    REP1(i,maxn-1) fac[i]=fac[i-1]*i%mod;
-    infac[maxn-1]=inv(fac[maxn-1]);
-    RREP(i,maxn-1) infac[i]=infac[i+1]*(i+1)%mod;
-}
-int C(int n,int k) {
-    return (fac[n]*infac[k]%mod)*infac[n-k]%mod;
-}
 signed main() {
     IOS();
-    com_init();
-    int n,m;
-    cin>>n>>m;
-    Vpii a(m+2);
-    REP(i,m) cin>>a[i];
-    a[m]={1,1};
-    a[m+1]={n,n};
-    sort(ALL(a));
-    Vi dp(m+2);
-    dp[0]=mod-1;
-    REP1(i,m+1) {
-        REP(j,i) {
-            if(a[j].s>a[i].s) continue;
-            addmod(dp[i],-dp[j]*C(a[i].f+a[i].s-a[j].f-a[j].s,a[i].f-a[j].f));
-        }
-    }
-    int an=(dp[m+1]+mod)%mod;
-    cout<<an<<'\n';
+    s
     return 0;
 }
