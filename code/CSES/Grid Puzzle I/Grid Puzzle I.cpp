49195e6211a15c1a1952cbff044cfc31af60d91a
huangallen <huangallen2008@gmail.com>
2025-04-06 16:43:03 +0000

    1977ing

--- code/CSES/Grid Puzzle I/Grid Puzzle I.cpp
+++ code/CSES/Grid Puzzle I/Grid Puzzle I.cpp
@@ -86,7 +86,7 @@ struct Dinic {
             q.pop();
             for(auto id:g[u]) {
                 auto [_,v,cap,r]=es[id];
-                
+                if(r==0)
             }
         }
     }
