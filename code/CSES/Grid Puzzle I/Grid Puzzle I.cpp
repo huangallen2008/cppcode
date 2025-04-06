259d50525c08a9a194148c6212a1998e2f71c5f7
huangallen <huangallen2008@gmail.com>
2025-04-06 16:46:28 +0000

    1977ing

--- code/CSES/Grid Puzzle I/Grid Puzzle I.cpp
+++ code/CSES/Grid Puzzle I/Grid Puzzle I.cpp
@@ -97,7 +97,7 @@ struct Dinic {
     int dfs(int u,int cap) {
         if(u==t||cap==0) return cap;
         for(int &i=it[u];i<SZ(g[u]);i++) {
-            auto [_,v,__,r]
+            auto [_,v,_c,r]=es[]
         }
     }
     int mxf(int _s,int _t) {
