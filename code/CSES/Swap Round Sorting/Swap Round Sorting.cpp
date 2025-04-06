7126847d309c0604772673c9eeca7f6754716b7d
huangallen <huangallen2008@gmail.com>
2025-04-06 13:25:47 +0000

    7966ing

--- code/CSES/Swap Round Sorting/Swap Round Sorting.cpp
+++ code/CSES/Swap Round Sorting/Swap Round Sorting.cpp
@@ -71,11 +71,14 @@ signed main() {
         an.pb({});
         Vi vis(n+1);
         REP1(i,n) {
+            if(vis[i]) continue;
             if(a[i]!=i) {
                 Vi v;
                 v.pb(i);
+                vis[i]=1;
                 int t=a[i];
                 while(t!=i) {
+                    vis[t]=1;
                     v.pb(t);
                     t=a[t];
                 }
