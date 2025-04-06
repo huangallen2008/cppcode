5301603669a7d77d0cab9c81cce6744a8de3156a
huangallen <huangallen2008@gmail.com>
2025-04-06 06:52:57 +0000

    7966ing

--- code/CSES/Writing Numbers/Writing Numbers.cpp
+++ code/CSES/Writing Numbers/Writing Numbers.cpp
@@ -68,7 +68,7 @@ signed main() {
         while(x) {
             int v=x/10;
             if(x%10) {
-                if(x%10==1) an+=x0%mul+1;
+                if(x%10==1) an+=v*mul+x0%mul+1;
                 else an+=mul;
             } 
         }
