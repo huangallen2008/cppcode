f359d6da73cd4913e7370fcb6c4e97a2547a0b93
huangallen <huangallen2008@gmail.com>
2025-04-07 06:54:12 +0000

    1977ing

--- code/CSES/Grid Puzzle II/Grid Puzzle II.cpp
+++ code/CSES/Grid Puzzle II/Grid Puzzle II.cpp
@@ -138,6 +138,7 @@ signed main() {
     mcmf.init(n);
     REP(i,n) REP(j,n) mcmf.addedge(i,j+n,-v[i][j],1);
     REP(i,n) mcmf.addedge(n<<1,i,0,c[i]);
-    REP(i,n) mcmf.addedge(i+n,n<<1|1,0,c[i]);
+    REP(i,n) mcmf.addedge(i+n,n<<1|1,0,r[i]);
+    
     return 0;
 }
