18fa5e97e3a50b9e5dd579a7e616f41b0ce3cb13
huangallen <huangallen2008@gmail.com>
2025-04-07 05:46:20 +0000

    1977ing

--- code/CSES/Grid Puzzle II/Grid Puzzle II.cpp
+++ code/CSES/Grid Puzzle II/Grid Puzzle II.cpp
@@ -57,7 +57,7 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-struct Dinic {
+struct MCMF {
     struct E {
         int u,v,cap,r;
     };
