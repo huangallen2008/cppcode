19a991a7a818a47875f72637bdb927d680dae20f
huangallen <huangallen2008@gmail.com>
2025-04-07 14:42:45 +0000

    1977ing

--- code/CSES/Creating Offices/Creating Offices.cpp
+++ code/CSES/Creating Offices/Creating Offices.cpp
@@ -88,7 +88,9 @@ int lca_dep(int a,int b) {
     return st_qu(in[a],in[b]); 
 }
 int qu_dis(int a,int b) { return dep[a]+dep[b]-2*lca_dep(a,b); }
-void bfs
+void bfs() {
+    
+}
 signed main() {
     IOS();
     cin>>n>>d;
