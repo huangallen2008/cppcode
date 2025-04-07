888411a88b124bfca933c5bdfe0aa201e2e8d5d3
huangallen <huangallen2008@gmail.com>
2025-04-07 05:47:13 +0000

    1977ing

--- code/CSES/Grid Puzzle II/Grid Puzzle II.cpp
+++ code/CSES/Grid Puzzle II/Grid Puzzle II.cpp
@@ -76,42 +76,6 @@ struct MCMF {
         g[u].pb(SZ(e)-2);
         g[v].pb(SZ(e)-1);
     }
-    bool bfs() {
-        dis=Vi(n,inf);
-        dis[s]=0;
-        queue<int> q;
-        q.push(s);
-        while(SZ(q)) {
-            int u=q.front();
-            q.pop();
-            for(auto id:g[u]) {
-                auto [_,v,cap,r]=es[id];
-                if(r&&dis[v]>dis[u]+1) {
-                    dis[v]=dis[u]+1;
-                    q.push(v);
-                }
-            }
-        }
-        return dis[t]!=inf;
-    }
-    int dfs(int u,int cap) {
-        if(u==t||cap==0) return cap;
-        int an=0;
-        for(int &i=it[u];i<SZ(g[u]);i++) {
-            int id=g[u][i];
-            auto [_,v,_c,r]=es[id];
-            if(dis[v]!=dis[u]+1) continue;
-            int ret=dfs(v,min(cap,r));
-            if(ret>0) {
-                es[id].r-=ret;
-                es[id^1].r+=ret;
-                an+=ret;
-                cap-=ret;
-                if(cap==0) break;
-            }
-        }
-        return an;
-    }
     int mxf(int _s,int _t) {
         s=_s,t=_t;
         int an=0,mf;
