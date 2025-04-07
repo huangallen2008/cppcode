fd44b6bf55036e8eabc3983de51c1ef8138a138b
huangallen <huangallen2008@gmail.com>
2025-04-07 14:55:58 +0000

    1977ing

--- code/CSES/Creating Offices/Creating Offices.cpp
+++ code/CSES/Creating Offices/Creating Offices.cpp
@@ -127,7 +127,7 @@ signed main() {
     auto gdis=[&](int x) {
         int mn=dis[x];
         for(int u:v2) chmin(mn,qu_dis(x,u));
-        op(x)ope(mn)
+        op(x)oparr(v2)ope(mn)
         return mn;
     };
     // REP(i,n) REP(j,n) {
