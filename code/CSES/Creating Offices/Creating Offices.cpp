bc0b51c0f5e688993a32033b4035948c1a2b13ea
huangallen <huangallen2008@gmail.com>
2025-04-07 14:33:30 +0000

    1977ing

--- code/CSES/Creating Offices/Creating Offices.cpp
+++ code/CSES/Creating Offices/Creating Offices.cpp
@@ -57,9 +57,10 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+int n,d;
+Graph g;
 signed main() {
     IOS();
-    int n,d;
     cin
     return 0;
 }
