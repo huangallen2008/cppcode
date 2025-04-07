35292d7809b423cd8179b8913e44a85332207318
huangallen <huangallen2008@gmail.com>
2025-04-07 15:03:10 +0000

    1977ing

--- code/CSES/Creating Offices/Creating Offices.cpp
+++ code/CSES/Creating Offices/Creating Offices.cpp
@@ -53,7 +53,7 @@ const int mod=1e9+7;
 const int maxn=4e5+5;
 const int maxb=19;
 const int inf=1ll<<60;
-const int sn=300;
+const int sn=200;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
