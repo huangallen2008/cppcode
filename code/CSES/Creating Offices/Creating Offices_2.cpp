9e23f8bd75629dcd0b6c5843a8f46714dd528994
huangallen <huangallen2008@gmail.com>
2025-04-08 02:12:01 +0000

    2988ing

--- code/CSES/Creating Offices/Creating Offices_2.cpp
+++ code/CSES/Creating Offices/Creating Offices_2.cpp
@@ -82,6 +82,10 @@ signed main() {
         g[v].pb(u);
     }
     Vpii dp(n);
-    
+    Func dfs=[&](auto dfs,int u,int fa) {
+        for(int v:g[u]) {
+            if(v==fa) continue;
+        }
+    }
     return 0;
 }
