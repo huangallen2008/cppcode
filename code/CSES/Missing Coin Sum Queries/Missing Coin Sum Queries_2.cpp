f5b58e665d5208844ece45f357134bf2374224c0
huangallen <huangallen2008@gmail.com>
2025-04-08 11:36:09 +0000

    2988ing

--- code/CSES/Missing Coin Sum Queries/Missing Coin Sum Queries_2.cpp
+++ code/CSES/Missing Coin Sum Queries/Missing Coin Sum Queries_2.cpp
@@ -58,12 +58,13 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+
 signed main() {
     IOS();
     int n,q;
     cin>>n>>q;
     Vi a(n);
     REP(i,n) cin>>a[i];
-    
+
     return 0;
 }
