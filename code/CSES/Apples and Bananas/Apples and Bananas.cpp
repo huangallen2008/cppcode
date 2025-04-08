724f2ca64228b5cc73cb60fae943ec8aed3919c6
huangallen <huangallen2008@gmail.com>
2025-04-08 15:57:43 +0000

    2988ing

--- code/CSES/Apples and Bananas/Apples and Bananas.cpp
+++ code/CSES/Apples and Bananas/Apples and Bananas.cpp
@@ -94,6 +94,7 @@ namespace FFT {
         while(SZ(a)<t) a.pb(0);
         while(SZ(b)<t) b.pb(0);
         r=Vi(t);
+        op(t)ope(lt)
         REP(i,t) r[i]=r[i>>1]>>1|(i&1)<<lt-1;
         _fft(a),_fft(b);
         REP(i,t) a[i]=a[i]*b[i];
