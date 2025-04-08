ad0bcb0285d587175ed751233d0cce3b9bfa1ed0
huangallen <huangallen2008@gmail.com>
2025-04-08 11:37:22 +0000

    2988ing

--- code/CSES/Missing Coin Sum Queries/Missing Coin Sum Queries_2.cpp
+++ code/CSES/Missing Coin Sum Queries/Missing Coin Sum Queries_2.cpp
@@ -64,7 +64,9 @@ int st[lgv][maxb][maxn];
 void st_init() {
     REP(i,lgv) REP(j,n) st[i][0][j]=inf;
     REP(i,n) st[__lg(a[i])][0][i]=a[i];
-    
+    REP(i,lgv) {
+        REP1(j,maxb) 
+    }
 }
 signed main() {
     IOS();
