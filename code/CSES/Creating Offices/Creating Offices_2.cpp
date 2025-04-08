720614b35b1ee570183009a8d585e012bd7c9452
huangallen <huangallen2008@gmail.com>
2025-04-08 02:11:44 +0000

    2988ing

--- code/CSES/Creating Offices/Creating Offices_2.cpp
+++ code/CSES/Creating Offices/Creating Offices_2.cpp
@@ -58,6 +58,17 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+template<class T>
+struct Func {
+    T func;
+    Func(const T &func):func(func) {}
+    template<class... Args>
+    constexpr decltype(auto) operator()(Args &&... args) {
+        return func(*this, forward<Args>(args)...);
+    }
+};
+template<class T>
+Func(const T &) -> Func<T>;
 signed main() {
     IOS();
     int n,d;
