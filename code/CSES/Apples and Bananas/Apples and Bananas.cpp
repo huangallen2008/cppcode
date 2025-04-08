14599137e0241dba2286c23952ace66f9298ece3
huangallen <huangallen2008@gmail.com>
2025-04-08 14:51:57 +0000

    2988ing

--- code/CSES/Apples and Bananas/Apples and Bananas.cpp
+++ code/CSES/Apples and Bananas/Apples and Bananas.cpp
@@ -60,8 +60,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-complex<ld>
 namespace FFT {
+    complex<ld>
     
 };
 signed main() {
