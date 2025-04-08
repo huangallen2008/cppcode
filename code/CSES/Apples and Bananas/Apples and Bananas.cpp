f76434e158d3d63a5b0c0c37bf6407067b3769e3
huangallen <huangallen2008@gmail.com>
2025-04-08 15:35:40 +0000

    2988ing

--- code/CSES/Apples and Bananas/Apples and Bananas.cpp
+++ code/CSES/Apples and Bananas/Apples and Bananas.cpp
@@ -74,7 +74,9 @@ namespace FFT {
             Cd wn(cosl(2*PI/m),sinl(on*2*PI/m));
             for(int l=0;l<t;l+=m) {
                 Cd w=1;
-                for()
+                for(int k=l;k<l+m;k++) {
+                    
+                }
             }
         }
     }
