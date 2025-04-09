548b2fb33404f4d830925a392807cbc3d8626c47
huangallen <huangallen2008@gmail.com>
2025-04-09 04:14:40 +0000

    3999ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -57,7 +57,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-#include<extc++.h>
+#include<bits/extc++.h>
+
 signed main() {
     IOS();
     cout<<dd(200000);
