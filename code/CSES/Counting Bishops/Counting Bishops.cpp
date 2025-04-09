70604b3e8d50f118ad2bbbe475bd77aa2ef48deb
huangallen <huangallen2008@gmail.com>
2025-04-09 01:36:02 +0000

    3999ing

--- code/CSES/Counting Bishops/Counting Bishops.cpp
+++ code/CSES/Counting Bishops/Counting Bishops.cpp
@@ -50,7 +50,7 @@ istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;ret
 template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=1e9+7;
-const int maxn=1000+5;
+const int maxn=500+5;
 const int maxb=18;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
