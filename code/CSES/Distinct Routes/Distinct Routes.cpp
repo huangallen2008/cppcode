a03e206ebfdf86afc03c9a14113227cb246118d2
huangallen <huangallen2008@gmail.com>
2025-04-09 19:12:23 +0000

    410010ing

--- code/CSES/Distinct Routes/Distinct Routes.cpp
+++ code/CSES/Distinct Routes/Distinct Routes.cpp
@@ -149,8 +149,7 @@ signed main() {
     int ans=dinic.mxf(0,n-1);
     Graph g(n);
     REP(i,m) if(dinic.es[i<<1].r==0) g[dinic.es[i<<1].u].pb(dinic.es[i<<1].v);
+    Vi path;
     Func dfs=[&](int u) ->void {
-        
-    }
-    return 0;
-}
+        path.pb(u);
+    }
