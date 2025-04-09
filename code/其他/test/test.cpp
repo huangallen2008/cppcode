7b65f28066eb050b1e22721f19e5fc865607ba3e
huangallen <huangallen2008@gmail.com>
2025-04-09 04:19:57 +0000

    3999ing

--- code/其他/test/test.cpp
+++ code/其他/test/test.cpp
@@ -68,7 +68,7 @@ signed main() {
     t.append(4);
     t.append(4);
 
-    for(int x:s) cout<<x<<' ';
+    for(int x:s) cout<<x<<' ';entr
     s.replace(1,t);
     for(int x:s) cout<<x<<' ';
     return 0;
