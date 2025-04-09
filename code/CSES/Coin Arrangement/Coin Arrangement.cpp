9ca2f728c9ea6b7d41f54d419a39d1f635637e47
huangallen <huangallen2008@gmail.com>
2025-04-09 05:51:58 +0000

    3999ing

--- code/CSES/Coin Arrangement/Coin Arrangement.cpp
+++ code/CSES/Coin Arrangement/Coin Arrangement.cpp
@@ -69,8 +69,8 @@ signed main() {
     int it=0;
     REP(i,n) {
         if(a[0][i]>1||a[1][i]>1) {
-            int c0=max(0ll,a[0][i]-1),c2=max(0ll,a[1][i]-1);
-            
+            int c0=max(0ll,a[0][i]-1),c1=max(0ll,a[1][i]-1);
+            for(int )
         }
     }
     return 0;
