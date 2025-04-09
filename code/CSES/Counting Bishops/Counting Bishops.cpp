0390afa1b6e904e015575d45428bdf7b3659642c
huangallen <huangallen2008@gmail.com>
2025-04-09 01:51:30 +0000

    3999ing

--- code/CSES/Counting Bishops/Counting Bishops.cpp
+++ code/CSES/Counting Bishops/Counting Bishops.cpp
@@ -110,7 +110,7 @@ signed main() {
         an2=Vi(k+1);
         REP1(i,k) an2[i]=(dp2[n2][i]+dp2[n2][i-1]*(n-i+1));
     }
-    REP(i,k+1) addmod(an,an1[i]*nan2[k-i]);
+    REP(i,k+1) addmod(an,an1[i]*an2[k-i]);
     cout<<an<<'\n';
     return 0;
 }
