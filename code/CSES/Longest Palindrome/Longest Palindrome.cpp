dbb46eecad227a9de7b8319a80796c212e6da213
huangallen <huangallen2008@gmail.com>
2025-04-10 06:43:01 +0000

    410010ing

--- code/CSES/Longest Palindrome/Longest Palindrome.cpp
+++ code/CSES/Longest Palindrome/Longest Palindrome.cpp
@@ -69,6 +69,7 @@ signed main() {
     REP(i,N) {
         p[i]=r>i?min(r-i,p[m*2-i]):1;
         while(i-p[i]>=0&&i+p[i]<N&&t[i+p[i]]==t[i-p[i]]) p[i]++;
+        if(i+p[i]>m) m=i+p[i]
     }
     return 0;
 }
