e7c6758b6080e24f3b9474515c878ae2fca8b827
huangallen <huangallen2008@gmail.com>
2025-04-10 06:55:01 +0000

    410010ing

--- code/CSES/Longest Palindrome/Longest Palindrome.cpp
+++ code/CSES/Longest Palindrome/Longest Palindrome.cpp
@@ -83,7 +83,7 @@ signed main() {
         op(i)ope(i&1?p[i]-(p[i]+1&1):p[i]&~1)
         if(anl==(i&1?p[i]-(p[i]+1&1):p[i]&~1)) {
             string an;
-            for(int j=i-p[i]+1;j<=i+p[i]-1;j++) an+=s[j];
+            for(int j=i-p[i]+1;j<=i+p[i]-1;j++) an+=t[j];
             for(char c:an) if(c!='.') ans+=c;
             break;
         }
