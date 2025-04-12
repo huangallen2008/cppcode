6d9e5d5b14da869e46814f82fdb0a78b7ff6f3cf
huangallen <huangallen2008@gmail.com>
2025-04-12 12:20:07 +0000

    610212ing

--- code/競賽/AtCoder/ABC401/D.cpp
+++ code/競賽/AtCoder/ABC401/D.cpp
@@ -70,7 +70,7 @@ signed main() {
     REP(i,SZ(id)-1) cnt+=id[i+1]-id[i]-(s[id[i]]=='o')-(s[id[i+1]]=='o')-2>>1;
     if(cnt==k) {
         REP(i,SZ(id)-1) {
-            if()
+            if(id[i+1]-id[i]-(s[id[i]]=='o')-(s[id[i+1]]=='o')&1) continue;
             for(int j=id[i]+(s[id[i]]=='o')+1;j<=id[i+1]-1-(s[id[i+1]]=='o');j+=2) {
 
             }
