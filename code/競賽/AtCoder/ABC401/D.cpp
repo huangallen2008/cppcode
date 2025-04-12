155e8ca58ced3abcc54efc96a631f1b79ce1f15b
huangallen <huangallen2008@gmail.com>
2025-04-12 12:21:24 +0000

    610212ing

--- code/競賽/AtCoder/ABC401/D.cpp
+++ code/競賽/AtCoder/ABC401/D.cpp
@@ -67,7 +67,7 @@ signed main() {
     Vi id;
     REP(i,n+2) if(s[i]!='?') id.pb(i);
     int cnt=0;
-    REP(i,SZ(id)-1) cnt+=id[i+1]-id[i]-(s[id[i]]=='o')-(s[id[i+1]]=='o')-2>>1;
+    REP(i,SZ(id)-1) cnt+=id[i+1]-id[i]-(s[id[i]]=='o')-(s[id[i+1]]=='o')+1>>1;
     ope(cnt)
     if(cnt==k) {
         string an=s;
