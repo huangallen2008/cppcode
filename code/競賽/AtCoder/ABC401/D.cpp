15a551a1ee934169017d491ab1eae8e8be3ccda6
huangallen <huangallen2008@gmail.com>
2025-04-12 12:37:47 +0000

    610212ing

--- code/競賽/AtCoder/ABC401/D.cpp
+++ code/競賽/AtCoder/ABC401/D.cpp
@@ -85,6 +85,20 @@ signed main() {
         string ans;
         REP1(i,n) ans+=an[i];
         cout<<ans<<'\n';
+    }if(cnt+cnto==k-1) {
+        string an=s;
+        REP(i,SZ(id)-1) {
+            if(id[i+1]-id[i]-(s[id[i]]=='o')-(s[id[i+1]]=='o')&1) continue;
+            for(int j=id[i]+(s[id[i]]=='o')+1;j<=id[i+1]-1-(s[id[i+1]]=='o');j+=2) {
+                an[j]='o';
+            }
+        }
+        REP1(i,n) {
+            if(an[i]=='?'&&(an[i-1]=='o'||an[i+1]=='o')) an[i]='.';
+        }
+        string ans;
+        REP1(i,n) ans+=an[i];
+        cout<<ans<<'\n';
     }else {
         string an=s;
         REP1(i,n) {
