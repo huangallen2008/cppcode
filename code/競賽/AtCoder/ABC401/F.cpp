9b8d7e7edad8706da6e899b5835abace957f051d
huangallen <huangallen2008@gmail.com>
2025-04-12 13:29:55 +0000

    610212ing

--- code/競賽/AtCoder/ABC401/F.cpp
+++ code/競賽/AtCoder/ABC401/F.cpp
@@ -57,6 +57,17 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+template<class T>
+struct Func {
+    T func;
+    Func(const T &func):func(func) {}
+    template<class... Args>
+    constexpr decltype(auto) operator()(Args &&... args) {
+        return func(*this, forward<Args>(args)...);
+    }
+};
+template<class T>
+Func(const T &) -> Func<T>;
 struct BIT {
     int n;
     Vi b;
@@ -74,9 +85,9 @@ struct BIT {
     }
     int qu(int l,int r) { return pre(r)-pre(l-1); }
 };
-Vpii get
+Vpii get_g() {
+    int n,m
+}
 signed main() {
     IOS();
-    
-    return 0;
-}
+  
