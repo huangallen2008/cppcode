ed609bade159aa117781dcdbf86066b83bc6ae33
huangallen <huangallen2008@gmail.com>
2025-04-12 12:52:01 +0000

    610212ing

--- code/競賽/AtCoder/ABC401/E.cpp
+++ code/競賽/AtCoder/ABC401/E.cpp
@@ -57,8 +57,13 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+struct DSU {
+    int n;
+    Vi p,sz;
+    void init(int _n) {}
+}
 signed main() {
     IOS();
-    
+
     return 0;
 }
