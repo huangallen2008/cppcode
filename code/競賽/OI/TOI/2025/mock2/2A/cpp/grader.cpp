e5ecd63aa8f24b523cb043aa0d9f9a32d12e0599
huangallen <huangallen2008@gmail.com>
2025-04-15 01:22:49 +0000

    210515ing

--- code/競賽/OI/TOI/2025/mock2/2A/cpp/grader.cpp
+++ code/競賽/OI/TOI/2025/mock2/2A/cpp/grader.cpp
@@ -1,23 +1,24 @@
 #include <cassert>
 #include <cstdio>
 #include <vector>
-#include "2A.cpp"
 std::vector<long long> maximum_profit(const std::vector<int> &A,
-                                      const std::vector<int> &Q);
-
-int main() {
-  int N, M;
-  assert(scanf("%d %d", &N, &M) == 2);
-  std::vector<int> A(N), Q(M);
-  for (int i = 0; i < N; ++i) {
-    assert(scanf("%d", &A[i]) == 1);
+  const std::vector<int> &Q);
+  
+  int main() {
+    int N, M;
+    assert(scanf("%d %d", &N, &M) == 2);
+    std::vector<int> A(N), Q(M);
+    for (int i = 0; i < N; ++i) {
+      assert(scanf("%d", &A[i]) == 1);
+    }
+    for (int i = 0; i < M; ++i) {
+      assert(scanf("%d", &Q[i]) == 1);
+    }
+    auto ans = maximum_profit(A, Q);
+    for (int i = 0; i < M; ++i) {
+      printf("%lld%c", ans[i], " \n"[i + 1 == M]);
+    }
+    return 0;
   }
-  for (int i = 0; i < M; ++i) {
-    assert(scanf("%d", &Q[i]) == 1);
-  }
-  auto ans = maximum_profit(A, Q);
-  for (int i = 0; i < M; ++i) {
-    printf("%lld%c", ans[i], " \n"[i + 1 == M]);
-  }
-  return 0;
-}
+  
+  #include "2A.cpp"
