be9a9aa821ac9ac8b7a09f9d582e46a4e88150a6
huangallen <huangallen2008@gmail.com>
2025-04-15 02:22:23 +0000

    210515ing

--- code/競賽/OI/TOI/2025/mock2/2C/cpp/2C.cpp
+++ code/競賽/OI/TOI/2025/mock2/2C/cpp/2C.cpp
@@ -107,7 +107,7 @@ void construct_logic(const std::string &s, signed K) {
             }
             // oparr(ids)
             if(K<T){
-                REP(i,1<<K) lids2
+                REP(i,1<<K) ids2[i]=adds
                 swap(lids,ids);
                 swap(lids2,ids2);
             }
