6fa0794737970988993dee95f003369536d5dea4
huangallen <huangallen2008@gmail.com>
2025-04-15 08:10:14 +0000

    210515ing

--- code/CSES/Coin Arrangement/Coin Arrangement.cpp
+++ code/CSES/Coin Arrangement/Coin Arrangement.cpp
@@ -68,7 +68,9 @@ signed main() {
     REP(i,n) {
         v0+=a[0][i],v1+=a[1][i];
         if(v1*v1<0) {
-            
+            if(abs(v1)<abs(v2)) {
+                
+            }
         }
     }
     cout<<an<<'\n';
