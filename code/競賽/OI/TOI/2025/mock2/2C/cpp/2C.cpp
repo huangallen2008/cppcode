501741575843fae784bcafd3d933db4af6a4e986
huangallen <huangallen2008@gmail.com>
2025-04-15 02:35:05 +0000

    210515ing

--- code/競賽/OI/TOI/2025/mock2/2C/cpp/2C.cpp
+++ code/競賽/OI/TOI/2025/mock2/2C/cpp/2C.cpp
@@ -89,7 +89,10 @@ void construct_logic(const std::string &s, signed K) {
         Vi ids=lids;
         for(int K=2;K<=T;K*=2) {
             ids=Vi(1<<K);
-            Vi lids2(1<<())
+            Vi lids2(1<<(K>>1));
+            REP(i,1<<(K>>1)) {
+                
+            }
             REP(i,1<<K) {
                 if(K==T&&cnt[i]==0) continue;
                 int nid=lids[i&((1<<(K>>1))-1)];
