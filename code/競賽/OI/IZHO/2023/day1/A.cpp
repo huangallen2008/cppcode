9adc5fdfd16b3182363c5530319f1d5fd03e5c3b
huangallen <huangallen2008@gmail.com>
2025-04-18 06:23:12 +0000

    510818ing

--- code/競賽/OI/IZHO/2023/day1/A.cpp
+++ code/競賽/OI/IZHO/2023/day1/A.cpp
@@ -84,7 +84,7 @@ signed main() {
                 now-=asum(stk.back().f+1,i,stk[SZ(stk)-2].s+1,stk.back().s);
                 stk.pop_back();
             }
-            now+=asum()
+            now+=asum(mxx[j]+1,i,)
         }
     }
     return 0;
