5a8fe1fbd3acaf169b4d255655f8eff774a3c049
huangallen <huangallen2008@gmail.com>
2025-04-18 06:25:40 +0000

    510818ing

--- code/競賽/OI/IZHO/2023/day1/A.cpp
+++ code/競賽/OI/IZHO/2023/day1/A.cpp
@@ -93,7 +93,7 @@ signed main() {
         Vi sa(m+1);
         RREP1(j,m) {
             while(SZ(stk)&&stk.back().f<mxx[j]) {
-                now-=asum(stk.back().f)
+                now-=asum(stk.back().f+1,i,)
             }
         }
     }
