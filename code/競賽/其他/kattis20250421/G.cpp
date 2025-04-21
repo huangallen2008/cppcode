54d048b3b9822122dd2f8281c9a0d77b52d2accf
huangallen <huangallen2008@gmail.com>
2025-04-21 02:10:06 +0000

    111121ing

--- code/競賽/其他/kattis20250421/G.cpp
+++ code/競賽/其他/kattis20250421/G.cpp
@@ -66,6 +66,9 @@ bool dif_side(pii a,pii b,pii c,pii d) {return sign(cros(to(a,b),to(a,c)))*sign(
 bool inters(pii a,pii b,pii c,pii d) { return !onl(a,b,c)&&!onl(a,b,d)&&!onl(c,d,a)&&!onl(c,d,b)&&dif_side(a,b,c,d)&&dif_side(c,d,a,b); }
 pii operator-(pii a,pii b) { return {b.f-a.f,b.s-a.s}; }
 pii operator+(pii a,pii b) { return {b.f+a.f,b.s+a.s}; }
+void solve() {
+    
+}
 signed main() {
     IOS();
     int n;
