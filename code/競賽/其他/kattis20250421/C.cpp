52247b0e07153ac787a16ea65455f572df745971
huangallen <huangallen2008@gmail.com>
2025-04-21 04:49:25 +0000

    111121ing

--- code/競賽/其他/kattis20250421/C.cpp
+++ code/競賽/其他/kattis20250421/C.cpp
@@ -105,14 +105,10 @@ void solve() {
         A0+=w*h;
         pdd v1=pdd{w*cosl((-v)*PI/180)/2,w*sinl(-v*PI/180)/2};
         pdd v2=pdd{h*cosl((90-v)*PI/180)/2,h*sinl((90-v)*PI/180)/2};
-        // p.pb({x,y});
-        // p.pb({x+v1,y});
-        // p.pb({x,y+v2});
-        // p.pb({x+v1,y+v2});
         p.pb(po+v1+v2);
         p.pb(po+v1-v2);
-        p.pb(po+v1-v2);
-        p.pb(po+v1-v2);
+        p.pb(po-v1+v2);
+        p.pb(po-v1-v2);
     }
     oparr(p)
     ld A=convex_area(p);
