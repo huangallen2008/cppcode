e0583ffd847015b1993d35c6d5fba1b270817e34
huangallen <huangallen2008@gmail.com>
2025-04-21 03:00:29 +0000

    111121ing

--- code/競賽/其他/kattis20250421/B.cpp
+++ code/競賽/其他/kattis20250421/B.cpp
@@ -85,7 +85,7 @@ ld seg_dis(pii a,pii b,pii c) {
 void solve() {
     pii a,b,c,d;
     cin>>a>>b>>c>>d;
-    if()
+    if(onl(a,b,c)&&onl(a,b,d)&&onl(c,d,a)&&)
     ld an=min({seg_dis(a,b,c),seg_dis(a,b,d),seg_dis(c,d,a),seg_dis(c,d,b)});
     cout<<fixed<<setprecision(2)<<an<<'\n';
 }
