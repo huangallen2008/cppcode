8fb2591c7b94e01c73cb5b0f050a820f3a3af827
huangallen <huangallen2008@gmail.com>
2025-04-21 02:42:38 +0000

    111121ing

--- code/競賽/其他/kattis20250421/A.cpp
+++ code/競賽/其他/kattis20250421/A.cpp
@@ -90,7 +90,9 @@ void solve() {
         else cout<<p1.f<<' '<<p1.s<<' '<<p2.f<<' '<<p2.s<<'\n';
     }else {
         
-        pdd an=inters_p(a,b,c,d);
+        pdd an;
+        
+        inters_p(a,b,c,d);
         cout<<an.f<<' '<<an.s<<'\n';
     }
 }
