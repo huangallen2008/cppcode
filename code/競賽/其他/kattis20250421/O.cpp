44b45ba4490dac1b39430e3d6dcb9977ba73b787
huangallen <huangallen2008@gmail.com>
2025-04-21 06:27:06 +0000

    111121ing

--- code/競賽/其他/kattis20250421/O.cpp
+++ code/競賽/其他/kattis20250421/O.cpp
@@ -115,8 +115,8 @@ void solve() {
     pii v2=p[0]-o;
     op(o)ope(v2)op(v2.f+v2.s)ope(v2.s-v2.f)
     np.pb(inters_p(p[id2],p[id2==n-1?0:id2+1],o,o+40000*(pdd{v2.f+v2.s,v2.s-v2.f})));
-    ld A2=np.back().f*np[0].s-np;
-    REP(i,SZ(np)-1) A2+=np[i],np[i+1]);
+    ld A2=np.back().f*np[0].s-np.back().s*np[0].f;
+    REP(i,SZ(np)-1) A2+=np[i].f*np[i+1].s-;
     oparr(p)oparr(np)op(id1)ope(id2)
     op(A)ope(A2)
     ld an=A2/A;
