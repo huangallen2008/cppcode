7d508a8a483cd5e609c028835a5dac11eaf3b87f
huangallen <huangallen2008@gmail.com>
2025-04-21 06:10:28 +0000

    111121ing

--- code/競賽/其他/kattis20250421/O.cpp
+++ code/競賽/其他/kattis20250421/O.cpp
@@ -108,6 +108,6 @@ signed main() {
     Vpii np;
     np.pb(o);
     pii v1=p[1]-o;
-    np.pb(inters_p(p[id1-1],p[id1],o,o+))
+    np.pb(inters_p(p[id1-1],p[id1],o,o+40000*(pdd{})))
     return 0;
 }
