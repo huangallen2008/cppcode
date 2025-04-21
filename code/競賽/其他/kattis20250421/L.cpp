bdbcd1afa11a3e36716971d4524a654e7021ee6d
huangallen <huangallen2008@gmail.com>
2025-04-21 01:51:31 +0000

    111121ing

--- code/競賽/其他/kattis20250421/L.cpp
+++ code/競賽/其他/kattis20250421/L.cpp
@@ -71,9 +71,9 @@ signed main() {
     pii a,b,c;
     cin>>a>>b>>c;
     pii an;
-    if(dot(b-a,c-a)==0) an=a-b-c;
-    else if(dot(a-b,c-b)==0) an=b-a-c;
-    else an=c-a-b;
+    if(dot(b-a,c-a)==0) an=b+c-a;
+    else if(dot(a-b,c-b)==0) an=a+c-b;
+    else an=a+b-c;
     cout<<an.f<<' '<<an.s<<'\n';
     return 0; 
 }
