39b5b0ff6a216361bbb4f344c857a74da41a8818
huangallen <huangallen2008@gmail.com>
2025-04-21 02:09:55 +0000

    111121ing

--- code/競賽/其他/kattis20250421/G.cpp
+++ code/競賽/其他/kattis20250421/G.cpp
@@ -80,7 +80,7 @@ signed main() {
             an.f+=sinl(d)*l;
             an.s+=cosl(d)*l;
         }
-        cout<<fixed<<setprecision()
+        cout<<fixed<<setprecision(6)<<an.f<<' '<<an.s<<'\n';
     }
     return 0;
 }
