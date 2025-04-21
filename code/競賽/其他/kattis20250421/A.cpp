e6207b2a05773f061d2ddf9fbec77cc5d1a0d2e1
huangallen <huangallen2008@gmail.com>
2025-04-21 02:26:28 +0000

    111121ing

--- code/競賽/其他/kattis20250421/A.cpp
+++ code/競賽/其他/kattis20250421/A.cpp
@@ -64,7 +64,9 @@ int sign(int x) { if(x>0) return 1;else if(x<0) return -1;else return 0; }
 bool onl(pii l1,pii l2,pii p) {return cros(to(l1,l2),to(l1,p))==0&&dot(to(l1,l2),to(l1,p))>=0&&dot(to(l2,l1),to(l2,p))>=0;}
 bool dif_side(pii a,pii b,pii c,pii d) {return sign(cros(to(a,b),to(a,c)))*sign(cros(to(a,b),to(a,d)))<0;}
 bool inters(pii a,pii b,pii c,pii d) { return !onl(a,b,c)&&!onl(a,b,d)&&!onl(c,d,a)&&!onl(c,d,b)&&dif_side(a,b,c,d)&&dif_side(c,d,a,b); }
-bool inters_p(pii a,pii b,pii c,pii d) { return !onl(a,b,c)&&!onl(a,b,d)&&!onl(c,d,a)&&!onl(c,d,b)&&dif_side(a,b,c,d)&&dif_side(c,d,a,b); }
+bool inters_p(pii a,pii b,pii c,pii d) { 
+    
+}
 pii operator-(pii a,pii b) { return {b.f-a.f,b.s-a.s}; }
 pii operator+(pii a,pii b) { return {b.f+a.f,b.s+a.s}; }
 void solve() {
