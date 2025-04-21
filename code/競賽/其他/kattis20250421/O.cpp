1e079fefd98b079ff26a9ef883e3defb89561b43
huangallen <huangallen2008@gmail.com>
2025-04-21 06:30:35 +0000

    111121ing

--- code/競賽/其他/kattis20250421/O.cpp
+++ code/競賽/其他/kattis20250421/O.cpp
@@ -64,8 +64,8 @@ pii operator*(pii a,int b) { return {b*a.f,b*a.s}; }
 pii operator*(int b,pii a) { return {b*a.f,b*a.s}; }
 pdd operator/(pii a,int b) { return {(ld)a.f/b,(ld)a.s/b}; }
 pii to(pii a,pii b) { return {b.f-a.f,b.s-a.s}; }
-int cros(pii a,pii b) { return a.f*b.s-a.s*b.f; }
-int dot(pii a,pii b) { return a.f*b.f+a.s*b.s; }
+ld cros(pii a,pii b) { return a.f*b.s-a.s*b.f; }
+ld dot(pii a,pii b) { return a.f*b.f+a.s*b.s; }
 int sign(int x) { if(x>0) return 1;else if(x<0) return -1;else return 0; }
 bool onl(pii l1,pii l2,pii p) { return l1==l2?l1==p:cros(to(l1,l2),to(l1,p))==0&&dot(to(l1,l2),to(l1,p))>=0&&dot(to(l2,l1),to(l2,p))>=0;}
 bool dif_side(pii a,pii b,pii c,pii d) {return sign(cros(to(a,b),to(a,c)))*sign(cros(to(a,b),to(a,d)))<0;}
