615b455722752592957544272b71e57656be6a7f
huangallen <huangallen2008@gmail.com>
2025-04-21 02:38:20 +0000

    111121ing

--- code/競賽/其他/kattis20250421/A.cpp
+++ code/競賽/其他/kattis20250421/A.cpp
@@ -70,6 +70,7 @@ bool dif_side(pii a,pii b,pii c,pii d) {return sign(cros(to(a,b),to(a,c)))*sign(
 bool inters(pii a,pii b,pii c,pii d) { return onl(a,b,c)||onl(a,b,d)||onl(c,d,a)||onl(c,d,b)||(dif_side(a,b,c,d)&&dif_side(c,d,a,b)); }
 pdd inters_p(pii a,pii b,pii c,pii d) { 
     int x=abs(cros(b-a,c-a)),y=abs(cros(b-a,d-a));
+    
     return (c*y+d*x)/(x+y);
 }
 void solve() {
