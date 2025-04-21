ff91f92b3b0b28930d27e156f74743105bb176ff
huangallen <huangallen2008@gmail.com>
2025-04-21 02:12:26 +0000

    111121ing

--- code/競賽/其他/kattis20250421/G.cpp
+++ code/競賽/其他/kattis20250421/G.cpp
@@ -77,10 +77,10 @@ void solve() {
         ld x,l;
         cin>>x>>l;
         d+=x;
-        op(d)op(sinl(d))ope(cosl(d))
-        an.f+=sinl(d*PI/180)*l;
-        an.s+=cosl(d*PI/180)*l;
-        op(an.f)ope(an.s)
+        // op(d)op(sinl(d))ope(cosl(d))
+        an.f+=cosl(d*PI/180)*l;
+        an.s+=sinl(d*PI/180)*l;
+        // op(an.f)ope(an.s)
     }
     cout<<fixed<<setprecision(6)<<an.f<<' '<<an.s<<'\n';
 }
