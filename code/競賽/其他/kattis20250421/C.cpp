635c68d5ce0ecf339d126aa3576b67e90d96f696
huangallen <huangallen2008@gmail.com>
2025-04-21 04:01:32 +0000

    111121ing

--- code/競賽/其他/kattis20250421/C.cpp
+++ code/競賽/其他/kattis20250421/C.cpp
@@ -95,7 +95,7 @@ void solve() {
         ld x,y,w,h,v;
         cin>>x>>y>>w>>h>>v;
         A0+=w*h;
-        pdd v1={w*cosl(-v),w*sinl(-v)};
+        pdd v1={w*cosl((-v)*180),w*sinl(-v)};
         pdd v2={cosl(90-v),sinl(90-v)};
         p.pb({x,y});
         p.pb({x,y});
