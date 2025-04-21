eedcf67d22f36b33748a83f9bc82086dc9643745
huangallen <huangallen2008@gmail.com>
2025-04-21 02:31:14 +0000

    111121ing

--- code/競賽/其他/kattis20250421/A.cpp
+++ code/競賽/其他/kattis20250421/A.cpp
@@ -83,7 +83,7 @@ void solve() {
     }
     cout<<fixed<<setprecision(2);
     if(cros(b-a,d-c)==0) {
-        pii p1=max(),p2;
+        pii p1=max(a,c),p2;
     }else {
         pdd an=inters_p(a+a-b,b+b-a,c+c-d,d+d-c);
         cout<<an.f<<' '<<an.s<<'\n';
