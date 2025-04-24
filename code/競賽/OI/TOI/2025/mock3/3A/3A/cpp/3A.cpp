9f5da000fc10d0ecd8ed50a6b2060fddf71a23a0
huangallen <huangallen2008@gmail.com>
2025-04-24 05:28:17 +0000

    411424ing

--- code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
+++ code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
@@ -98,6 +98,8 @@ signed minimum_garbage(const std::vector<signed> &L, const std::vector<signed> &
     REP(i,m) b[i]=lower_bound(ALL(t),b[i])-t.begin();
     REP(i,k) c[i]=lower_bound(ALL(t),c[i])-t.begin();
     REP(i,n) a[i]={lower_bound(ALL(t),L[i])-t.begin(),upper_bound(ALL(t),R[i])-t.begin()-1};
+    SEG seg;
+    seg.init(N);
     
     return 0;
 }
