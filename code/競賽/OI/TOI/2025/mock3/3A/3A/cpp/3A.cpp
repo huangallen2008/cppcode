2237a908189481f4d5d938316f355a3606a7134b
huangallen <huangallen2008@gmail.com>
2025-04-24 08:40:34 +0000

    411424ing

--- code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
+++ code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
@@ -109,7 +109,7 @@ signed minimum_garbage(const std::vector<signed> &L, const std::vector<signed> &
     int N=SZ(t);
     for(auto [l,r]:a) {
         int l2=*prev(upper_bound(ALL(b),l));
-        chmin(dp[r],seg.qu(l2))
+        chmin(dp[r],seg.qu(l2,r)+)
         seg.ud(r,seg.qu())
     }
     return 0;
