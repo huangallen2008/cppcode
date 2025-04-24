af6724fe1aee9c9d8eaf3dab821b1a66c641c836
huangallen <huangallen2008@gmail.com>
2025-04-24 05:14:42 +0000

    411424ing

--- code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
+++ code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
@@ -75,6 +75,7 @@ struct SEG {
         s[w]=min(s[w<<1],s[w<<1|1]);
     }
     void ud(int u,int v) { _ud(1,0,n-1,u,v); }
+    int _qu()
 }
 signed minimum_garbage(const std::vector<signed> &L, const std::vector<signed> &R,const std::vector<signed> &J, const std::vector<signed> &Q) {
     int n = L.size();
