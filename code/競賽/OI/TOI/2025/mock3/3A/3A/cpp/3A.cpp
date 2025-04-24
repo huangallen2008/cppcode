2daf96e96bf49a4da20cc1855a90c7f27d9a0dc7
huangallen <huangallen2008@gmail.com>
2025-04-24 05:24:59 +0000

    411424ing

--- code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
+++ code/競賽/OI/TOI/2025/mock3/3A/3A/cpp/3A.cpp
@@ -90,10 +90,10 @@ signed minimum_garbage(const std::vector<signed> &L, const std::vector<signed> &
     int N=m+k;
     Vpii a(n);
     Vi t,b(m),c(k);
-    REP(i,m) b[i]=J[i];
+    REP(i,m) b[i]=J[i],t.pb(b[i]);
     REP(i,k) c[i]=Q[i];
     sort(ALL(b));
     sort(ALL(c));
-    
+
     return 0;
 }
