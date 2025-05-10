d38a7ca4d7520cf30ea7747f727b80597c32b60a
huangallen <huangallen2008@gmail.com>
2025-05-10 06:27:00 +0000

    613010ing

--- code/競賽/AA/2025YTP分組賽驗題/E.cpp
+++ code/競賽/AA/2025YTP分組賽驗題/E.cpp
@@ -68,9 +68,9 @@ void solve() {
     int an=0,cnt=0;
     REP(i,n) {
         int x=s[i]-'0';
+        ope(x)
         if(x>=2) an++;
         else if(x==0) {
-            ope("o")
             if(cnt>0)an++,cnt--;
         }else {
             if(cnt<ss[i+1]) cnt++;
