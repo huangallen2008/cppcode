6f687d756463885f02a9875db17abc22d9af6817
huangallen <huangallen2008@gmail.com>
2025-05-10 07:41:57 +0000

    613010ing

--- code/競賽/AA/2025YTP分組賽驗題/L.cpp
+++ code/競賽/AA/2025YTP分組賽驗題/L.cpp
@@ -67,7 +67,16 @@ int pw(int x,int p) {
     return r;
 }
 int inv(int x) { return pw(x,mod-2); }
-
+Vi fac(maxn),infac(maxn);
+void com_init() {
+    fac[0]=1;
+    REP1(i,maxn-1) fac[i]=fac[i-1]*i%mod;
+    infac[maxn-1]=inv(fac[maxn-1]);
+    RREP(i,maxn-1) infac[i]=infac[i+1]*(i+1)%mod;
+}
+int C(int n,int k) {
+    return (fac[n]*infac[k]%mod)*infac[n-k]%mod;
+}
 signed main() {
     IOS();
 
