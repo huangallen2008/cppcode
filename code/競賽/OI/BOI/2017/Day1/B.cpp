2ad789dc297bc61a9797bfceca72022d99c91588
huangallen <huangallen2008@gmail.com>
2025-05-11 09:24:04 +0000

    713111ing

--- code/競賽/OI/BOI/2017/Day1/B.cpp
+++ code/競賽/OI/BOI/2017/Day1/B.cpp
@@ -143,6 +143,7 @@ signed main() {
         sort(ALL(a),[&](int x,int y) {return in[x]<in[y]; });
         Vi b;b.pb(a[0]);
         REP1(j,s-1) {
+            op(i)ope(j)
             b.pb(qu_lca(a[j-1],a[j]));
             b.pb(a[j]);
         }
