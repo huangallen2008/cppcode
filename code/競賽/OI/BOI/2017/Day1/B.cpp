0541063674a056666bad76dbe305c7e9b1960b6a
huangallen <huangallen2008@gmail.com>
2025-05-11 09:02:24 +0000

    713111ing

--- code/競賽/OI/BOI/2017/Day1/B.cpp
+++ code/競賽/OI/BOI/2017/Day1/B.cpp
@@ -50,7 +50,7 @@ istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;ret
 template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=998244353;
-const int maxn=3e3+5;
+const int maxn=2e5+5;
 const int maxb=17;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
