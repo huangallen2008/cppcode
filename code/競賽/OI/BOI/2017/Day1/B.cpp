f30fcffcb7c7bcd592ab5db3ebdc184b1a2b5d9e
huangallen <huangallen2008@gmail.com>
2025-05-11 09:13:19 +0000

    713111ing

--- code/競賽/OI/BOI/2017/Day1/B.cpp
+++ code/競賽/OI/BOI/2017/Day1/B.cpp
@@ -79,12 +79,9 @@ struct BIT {
 };
 int n,m,k;
 Graphw g;
-Vi dfn,dfnp,dep;
-Vpii dfn2;
+Vi dfn,dep;
 Vi in,out,pe;
 void dfs(int u,int fa) {
-    dfnp[u]=SZ(dfn2);
-    dfn2.pb({dep[u],u});
     for(auto [v,id]:g[u]) {
         if(v==fa) continue;
         pe[v]=id;
