6c16e6a5ad0a2afe6e1515d1f6c290369019c1cf
huangallen <huangallen2008@gmail.com>
2025-05-13 14:34:56 +0000

    213313ing

--- code/CSES/Visible Buildings Queries/Visible Buildings Queries.cpp
+++ code/CSES/Visible Buildings Queries/Visible Buildings Queries.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 // #pragma GCC optimize("O3,unroll-loops")
 // #pragma GCC target("avx2,sse4,bmi2,popcnt")
-#define int long long
+// #define int long long
 #define REP(i,n) for(int i=0;i<(n);i++)
 #define REP1(i,n) for(int i=1;i<=(n);i++)
 #define RREP(i,n) for(int i=(n)-1;i>=0;i--)
@@ -139,6 +139,6 @@ signed main() {
         stk.pb({a[i],i});
         for(auto [l,id]:qu[i]) an[id]=bit.qu(l);
     }
-    REP(i,q) cout<<an[i]<<'\n';
+    REP(i,q) out(an[i]),PC('\n');
     return 0;
 }
