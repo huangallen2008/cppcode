f79eb03fc71a0b49d4f0b621f0668fafb943a384
huangallen <huangallen2008@gmail.com>
2025-05-13 14:29:06 +0000

    213313ing

---
+++ code/CSES/Visible Buildings Queries/Visible Buildings Queries.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,sse4,bmi2,popcnt")
+#define int long long
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,n) for(int i=1;i<=(n);i++)
+#define RREP(i,n) for(int i=(n)-1;i>=0;i--)
+#define RREP1(i,n) for(int i=(n);i>=1;i--)
+#define f first
+#define s second
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)((x).size()))
+#define SQ(x) ((x)*(x))
+#define pii pair<int,int>
+#define pipii pair<int,pii>
+#define ppi pair<pii,int>
+#define Graph vector<vector<int>>
+#define Graphw vector<vector<pii>>
+#define IOS() ios::sync_with_stdio(0),cin.tie(0)
+#define md(x) (((x)%(mod)+(mod))%(mod))
+#define MD(x,M) (((x)%(M)+(M))%(M))
+#define ld long double
+#define pdd pair<ld,ld>
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+#define addmod(x,y) x=((x+(y))%mod)
+#define Vi vector<int>
+#define Vpii vector<pii>
+#ifdef LOCAL
+#define op(x) cout<<(#x)<<"="<<(x)<<", ";
+#define ope(x) cout<<(#x)<<"="<<(x)<<endl;
+#define oparr(x) {cout<<(#x)<<":";for(auto allen:(x)) cout<<allen<<" ";cout<<" size="<<(x).size()<<endl;}
+#define entr cout<<endl;
+#else
+#define op(x) ;
+#define ope(x) ;
+#define oparr(x) ;
+#define entr ;
+#endif
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,pair<T1,T2> p) { return os<<'{'<<p.f<<','<<p.s<<'}'; }
+template<typename T1,typename T2>
+istream& operator>>(istream& os,pair<T1,T2> &p) { return os>>p.f>>p.s; }
+template<typename S>
+ostream& operator<<(ostream& os,vector<S> p) { for(auto allen:p) os<<allen<<' ';return os<<'\n'; }
+template<typename S>
+istream& operator>>(istream& os,vector<S> &p) { for(auto &allen:p) os>>allen;return os; }
+template<typename T1,typename T2>
+pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
+const int mod=1e9+7;
+const int maxn=1e6+5;
+const int maxv=1000;
+const int inf=1ll<<60;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rd(int l,int r) {
+    return uniform_int_distribution<int>(l,r)(rng);
+}
+signed main() {
+    IOS();
+    int n,m;
+    cin>>n>>m;
+    Vi a(n+1),b(m+1);
+    REP1(i,n) cin>>a[i];
+    REP1(i,m) cin>>b[i];
+    vector<Vi> dp(n+1,Vi(m+1)),pre(n+1,Vi(m+1));
+    REP1(i,n) REP1(j,m) {
+        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        if(a[i]==b[j]) chmax(dp[i][j],dp[i-1][j-1]+1);
+
+        if(a[i]==b[j]&&dp[i-1][j-1]+1==dp[i][j]) pre[i][j]=0;
+        else if(dp[i-1][j]==dp[i][j]) pre[i][j]=1;
+        else pre[i][j]=2;
+    }
+    Vi an;
+    int nx=n,ny=m;
+    while(nx>0&&ny>0) {
+        if(pre[nx][ny]==0) {
+            an.pb(a[nx]);
+            nx--,ny--;
+        }else if(pre[nx][ny]==1) nx--;
+        else ny--;
+    }
+    reverse(ALL(an));
+    cout<<SZ(an)<<'\n';
+    for(int x:an) cout<<x<<' ';
+    return 0;
+}
