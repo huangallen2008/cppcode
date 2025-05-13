57e48dde1dee8c328277849c0879a2ec0d5029a1
huangallen <huangallen2008@gmail.com>
2025-05-13 15:45:06 +0000

    213313ing

--- code/CSES/Sliding Window Mode/Sliding Window Mode.cpp
+++ code/CSES/Sliding Window Mode/Sliding Window Mode.cpp
@@ -57,6 +57,9 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+struct cmp {
+    bool operator()()
+}
 signed main() {
     IOS();
     int n,k;
