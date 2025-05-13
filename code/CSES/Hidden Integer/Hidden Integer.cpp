e07372fd70e3060a0ce9a84ae294acd2adcf6b92
huangallen <huangallen2008@gmail.com>
2025-05-13 16:38:24 +0000

    313414ing

--- code/CSES/Hidden Integer/Hidden Integer.cpp
+++ code/CSES/Hidden Integer/Hidden Integer.cpp
@@ -57,7 +57,9 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-
+bool qur(int x) {
+    cout<<"? "<<x<<fl
+}
 signed main() {
     IOS();
 
