ad1f89e789acc013e508fd385cfc5a73c0baa725
huangallen <huangallen2008@gmail.com>
2025-05-13 15:45:17 +0000

    213313ing

--- code/CSES/Sliding Window Mode/Sliding Window Mode.cpp
+++ code/CSES/Sliding Window Mode/Sliding Window Mode.cpp
@@ -58,7 +58,7 @@ int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
 struct cmp {
-    bool operator()()
+    bool operator()(pii a,pii b) { return a.}
 }
 signed main() {
     IOS();
