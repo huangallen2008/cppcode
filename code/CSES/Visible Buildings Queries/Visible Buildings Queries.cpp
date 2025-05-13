615445e86f192e956ce45bbe5af54a813b42b8df
huangallen <huangallen2008@gmail.com>
2025-05-13 14:32:04 +0000

    213313ing

--- code/CSES/Visible Buildings Queries/Visible Buildings Queries.cpp
+++ code/CSES/Visible Buildings Queries/Visible Buildings Queries.cpp
@@ -95,7 +95,7 @@ signed main() {
     bit.init(n);
     REP(i,n) {
         while(SZ(stk)&&a[i]>stk.back().f) stk.pop_back();
-        
+        bit.
         stk.pb({a[i],i});
     }
     return 0;
