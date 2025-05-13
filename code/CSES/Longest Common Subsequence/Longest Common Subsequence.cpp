c68a870106b824332070cf647af6f4687806dee8
huangallen <huangallen2008@gmail.com>
2025-05-13 12:59:20 +0000

    213313ing

--- code/CSES/Longest Common Subsequence/Longest Common Subsequence.cpp
+++ code/CSES/Longest Common Subsequence/Longest Common Subsequence.cpp
@@ -71,7 +71,7 @@ signed main() {
 
 
 
-        
+        \
         if(a[i]==b[j]&&dp[i-1][j-1]+1==dp[i][j]) pre[i][j]=0;
         else if(dp[i-1][j]==dp[i][j]) pre[i][j]=1;
         else pre[i][j]=2;
