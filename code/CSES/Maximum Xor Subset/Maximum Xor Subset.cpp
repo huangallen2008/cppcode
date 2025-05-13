7a53b8e2efbdbc54001f2e4031ecb81aecdbe813
huangallen <huangallen2008@gmail.com>
2025-05-13 16:06:31 +0000

    313414ing

--- code/CSES/Maximum Xor Subset/Maximum Xor Subset.cpp
+++ code/CSES/Maximum Xor Subset/Maximum Xor Subset.cpp
@@ -60,7 +60,11 @@ int rd(int l,int r) {
 Vi bas(maxb);
 void add(int x) {
     RREP(i,maxb) if(x>>i&1) {
-        if(b)
+        if(bas[i]==0) {
+            bas[i]=x;
+            return;
+        }
+        
     }
 }
 signed main() {
