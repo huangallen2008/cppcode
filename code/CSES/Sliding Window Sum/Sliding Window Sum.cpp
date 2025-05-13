6a2e460067d155a111eeee2d4fcfd6752c3d7498
huangallen <huangallen2008@gmail.com>
2025-05-13 15:17:23 +0000

    213313ing

--- code/CSES/Sliding Window Sum/Sliding Window Sum.cpp
+++ code/CSES/Sliding Window Sum/Sliding Window Sum.cpp
@@ -57,62 +57,8 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-struct SEG {
-    struct Seg {
-        int sum,mp,ms,ma;
-    };
-    const Seg zr={0,0,0,0};
-    Seg merge(Seg b,Seg c) {
-        Seg a;
-        a.sum=b.sum+c.sum;
-        a.mp=max(b.mp,b.sum+c.mp);
-        a.ms=max(c.ms,c.sum+b.ms);
-        a.ma=max({b.ma,c.ma,b.ms+c.mp});
-        return a;
-    }
-    void pull(Seg &a,Seg &b,Seg &c) {
-        a=merge(b,c);
-    }
-    int n;
-    vector<Seg> s;
-    void build(int w,int l,int r,Vi &a) {
-        if(l==r) {
-            s[w].sum=a[l];
-            s[w].mp=s[w].ms=s[w].ma=max(a[l],0ll);
-            return;
-        }
-        int m=l+r>>1;
-        build(w<<1,l,m,a);
-        build(w<<1|1,m+1,r,a);
-        pull(s[w],s[w<<1],s[w<<1|1]);
-    }
-    void init(int _n,Vi &a) {
-        n=_n;
-        s=vector<Seg>(n<<2);
-        build(1,0,n-1,a);
-    }
-    Seg _qu(int w,int l,int r,int ql,int qr) {
-        if(ql<=l&&r<=qr) return s[w];
-        if(ql>r||qr<l) return zr;
-        int m=l+r>>1;
-        return merge(_qu(w<<1,l,m,ql,qr),_qu(w<<1|1,m+1,r,ql,qr));
-    }
-    int qu(int l,int r) {
-        return _qu(1,0,n-1,l,r).ma;
-    }
-}seg;
 signed main() {
     IOS();
-    int n,q;
-    cin>>n>>q;  
-    Vi a(n);
-    REP(i,n) cin>>a[i];
-    seg.init(n,a);
-    REP(i,q) {
-        int l,r;
-        cin>>l>>r,l--,r--;
-        int an=seg.qu(l,r);
-        cout<<an<<'\n';
-    }
+
     return 0;
 }
