a2a2d49a5209647fc7c7167c1a481f7974ff4eda
huangallen <huangallen2008@gmail.com>
2025-05-13 15:32:48 +0000

    213313ing

--- code/CSES/Sliding Window Or/Sliding Window Or.cpp
+++ code/CSES/Sliding Window Or/Sliding Window Or.cpp
@@ -57,7 +57,9 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
-
+struct mystk {
+    
+}
 signed main() {
     IOS();
     int n,k;
