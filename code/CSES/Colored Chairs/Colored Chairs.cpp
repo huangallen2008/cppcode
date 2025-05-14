03599bad05470a3dfb8446117350692aa43b6f16
huangallen <huangallen2008@gmail.com>
2025-05-14 01:29:21 +0000

    313414ing

--- code/CSES/Colored Chairs/Colored Chairs.cpp
+++ code/CSES/Colored Chairs/Colored Chairs.cpp
@@ -51,14 +51,29 @@ template<typename T1,typename T2>
 pair<T1,T2> operator+(pair<T1,T2> p1,pair<T1,T2> p2) { return pair<T1,T2>(p1.f+p2.f,p1.s+p2.s); }
 const int mod=1e9+7;
 const int maxn=1e6+5;
-const int maxv=1000;
+const int maxb=30;
 const int inf=1ll<<60;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+bool qur(int x) {
+    cout<<"? "<<x<<endl;
+    string ret;
+    cin>>ret;
+    return ret[0]=='Y';
+}
+void ans(int x) {
+    cout<<"! "<<x<<endl;
+}
 signed main() {
     IOS();
-    
+    int l=1,r=1e9,m;
+    while(l<r) {
+        m=l+r>>1;
+        if(!qur(m)) r=m;
+        else l=m+1;
+    }
+    ans(l);
     return 0;
 }
