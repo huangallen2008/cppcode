680fac6f12d12cb6eb9a648ddd160404cec75b76
huangallen <huangallen2008@gmail.com>
2025-05-14 00:41:59 +0000

    313414ing

--- code/CSES/Mountain Range/Mountain Range.cpp
+++ code/CSES/Mountain Range/Mountain Range.cpp
@@ -66,7 +66,7 @@ signed main() {
     Vpii stk;
     Vi h(n,1);
     REP(i,n) {
-        while(SZ(stk)&&a[i]>stk.back().f) {
+        while(SZ(stk)&&a[i]>=stk.back().f) {
             if(SZ(stk)>1) chmax(h[stk[SZ(stk)-2].s],h[stk.back().s]+1);
             chmax(h[i],h[stk.back().s]+1);
             stk.pop_back();
