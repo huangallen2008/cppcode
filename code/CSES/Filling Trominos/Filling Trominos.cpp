a9287cf412b96bb900169c2b50e56b98d86efc23
huangallen <huangallen2008@gmail.com>
2025-05-15 12:10:47 +0000

    413515ing

--- code/CSES/Filling Trominos/Filling Trominos.cpp
+++ code/CSES/Filling Trominos/Filling Trominos.cpp
@@ -57,6 +57,11 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+void solve() {
+    int n,m;
+    cin>>n>>m;
+    if((n*m)%3!=0)
+}
 signed main() {
     IOS();
     int T;
