9ac10eb5acf9e7c4b16f3786aa5945d7f4b3d15c
huangallen <huangallen2008@gmail.com>
2025-05-23 03:19:09 +0000

    514323ing

--- code/python/test.py
+++ code/python/test.py
@@ -1,23 +1,23 @@
-def get_grade(score):
-    if not isinstance(score, (int, float)) or score < 0 or score > 100:
-        return 'Invalid score'
-    if 90 <= score <= 100:
-            return '優'
-    elif 80 <= score < 90:
-        return '甲'
-    elif 70 <= score < 80:
-        return '乙'
-    elif 60 <= score < 70:
-        return '丙'
-    elif 50 <= score < 60:
-        return '丁'
-    elif 40 <= score < 50:
-        return '不及格'
-    else:
-        return '掰掰你被死當了'
+# def get_grade(score):
+#     if not isinstance(score, (int, float)) or score < 0 or score > 100:
+#         return 'Invalid score'
+#     if 90 <= score <= 100:
+#             return '優'
+#     elif 80 <= score < 90:
+#         return '甲'
+#     elif 70 <= score < 80:
+#         return '乙'
+#     elif 60 <= score < 70:
+#         return '丙'
+#     elif 50 <= score < 60:
+#         return '丁'
+#     elif 40 <= score < 50:
+#         return '不及格'
+#     else:
+#         return '掰掰你被死當了'
 try:
     w = float(input().split(' ')) 
     s = float(input().split(' ')) 
-    
+
 except ValueError:
     print("輸入的不是有效的數字。")
