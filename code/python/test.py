fb940c8c86c4d1a19ec066e90cb386c13757b6d1
huangallen <huangallen2008@gmail.com>
2025-05-23 03:39:20 +0000

    514323ing

--- code/python/test.py
+++ code/python/test.py
@@ -11,10 +11,8 @@ results = {'chai': 0, 'juice': 0, 'coffee': 0, 'coldrink': 0, 'others': 0}
 
 filtered_df = df[df['Date'] == '1/3/2023']
 
-# 使用 groupby 計算各商品的總銷量
 grouped = filtered_df.groupby('Item')['Amount'].sum()
 
-# 更新 results 字典
 for item in results:
     if item in grouped:
         results[item] = grouped[item]
