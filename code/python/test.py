0dfc3e72995c8d45675302eee857d674264578f6
huangallen <huangallen2008@gmail.com>
2025-05-23 03:44:44 +0000

    514323ing

--- code/python/test.py
+++ code/python/test.py
@@ -1,20 +1,22 @@
 import pandas as pd
 
-data = {
-    'Date': ['1/3/2023', '1/3/2023', '1/3/2023', '1/3/2023', '1/3/2023', '1/3/2023', '1/3/2023', '1/3/2023', '2/3/2023', '2/3/2023'],
-    'Item': ['chai', 'chai', 'juice', 'coffee', 'coffee', 'chai', 'coldrink', 'others', 'chai', 'juice'],
-    'Amount': [7, 20, 15, 12, 12, 25, 10, 10, 7, 15]
-}
-df = pd.DataFrame(data)
+json_data = [
+  { "HotelName": "丰居旅店忠孝館", "Description": "位於臺北市的旅館", "ServiceInfo": "接駁服務、健身房、SPA", "Price": 2420, "PostalAddress": { "City": "臺北市", "Town": "大安區", "StreetAddress": "復興南路1段126巷1號3樓" } },
+  { "HotelName": "松河璞旅", "Description": "位於臺北市的旅館", "ServiceInfo": "接駁服務、健身房", "Price": 2600, "PostalAddress": { "City": "臺北市", "Town": "松山區", "StreetAddress": "松河街112號1樓至8樓" } },
+  { "HotelName": "二十輪旅店大安店", "Description": "優雅質感生活", "ServiceInfo": "SPA、游泳池、酒吧", "Price": 4500, "PostalAddress": { "City": "臺北市", "Town": "大安區", "StreetAddress": "大安路一段183號" } },
+  { "HotelName": "城市商旅南東館", "Description": "時尚沉穩設計", "ServiceInfo": "健身房、酒吧、會議室", "Price": 3000, "PostalAddress": { "City": "臺北市", "Town": "松山區", "StreetAddress": "南京東路五段411號" } },
+  { "HotelName": "在家行旅", "Description": "鄰近台北火車站", "ServiceInfo": "接駁服務、健身房", "Price": 1000, "PostalAddress": { "City": "臺北市", "Town": "中山區", "StreetAddress": "中山北路二段65巷2弄3號" } }
+]
 
-results = {'chai': 0, 'juice': 0, 'coffee': 0, 'coldrink': 0, 'others': 0}
+df = pd.DataFrame(json_data)
 
-filtered_df = df[df['Date'] == '1/3/2023']
+money_input = int(input())
 
-grouped = filtered_df.groupby('Item')['Amount'].sum()
+# 過濾符合條件的資料（Price >= money_input）
+filtered_df = df[df['Price'] >= money_input]
 
-for item in results:
-    if item in grouped:
-        results[item] = grouped[item]
-
-print(results)
+# 輸出符合條件的旅館資訊
+for _, row in filtered_df.iterrows():
+    hotel_name = row['HotelName']
+    city = row['PostalAddress']['City']
+    print(f"旅館名稱: {hotel_name}, 城市: {city}")
