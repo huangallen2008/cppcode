993a7286ad2d59634e75d42e357876db2adfe0f5
huangallen <huangallen2008@gmail.com>
2025-05-23 02:57:28 +0000

    514323ing

--- code/python/test.py
+++ code/python/test.py
@@ -3,5 +3,8 @@ def chat_bot():
     "hello": {"French": "bonjour", "Korean": "안녕하세요", "Chinese": "你好"},
     "goodbye": {"French": "au revoir", "Korean": "안녕히 가세요", "Chinese": "再見"},
     "thank you": {"French": "merci", "Korean": "감사합니다", "Chinese": "謝謝"},
-}
-target_language = input("")
+    }
+    target_language = input("")
+    if target_language not in ["French", "Korean", "Chinese"]:
+            print("Sorry, that language is not supported.")
+            return
