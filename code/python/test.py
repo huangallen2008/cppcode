4b3b711b1b24f8eeecedcc0e1f2bf725131d2c58
huangallen <huangallen2008@gmail.com>
2025-05-23 02:54:29 +0000

    514323ing

--- code/python/test.py
+++ code/python/test.py
@@ -18,6 +18,6 @@ def get_grade(score):
 try:
     score = float(input()) 
     grade = get_grade(score) 
-    print(f"你的成績等第是 {grade}。")  # 顯示結果
+    print(f"你的成績等第是 {grade}。")
 except ValueError:
     print("輸入的不是有效的數字。")
