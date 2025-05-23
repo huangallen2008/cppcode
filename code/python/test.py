3da5958fa6bf5f1a42b6d6c0d928a9f5c33a8696
huangallen <huangallen2008@gmail.com>
2025-05-23 03:15:40 +0000

    514323ing

--- code/python/test.py
+++ code/python/test.py
@@ -1,6 +1,23 @@
-def chat_bot():
-    translations = {
-    "hello": {"French": "bonjour", "Korean": "안녕하세요", "Chinese": "你好"},
-    "goodbye": {"French": "au revoir", "Korean": "안녕히 가세요", "Chinese": "再見"},
-    "thank you": {"French": "merci", "Korean": "감사합니다", "Chinese": "謝謝"},
-}
+def get_grade(score):
+    if not isinstance(score, (int, float)) or score < 0 or score > 100:
+        return 'Invalid score'
+    if 90 <= score <= 100:
+            return '優'
+    elif 80 <= score < 90:
+        return '甲'
+    elif 70 <= score < 80:
+        return '乙'
+    elif 60 <= score < 70:
+        return '丙'
+    elif 50 <= score < 60:
+        return '丁'
+    elif 40 <= score < 50:
+        return '不及格'
+    else:
+        return '掰掰你被死當了'
+try:
+    score = float(input()) 
+    grade = get_grade(score) 
+    print(f"你的成績等第是 {grade}。")
+except ValueError:
+    print("輸入的不是有效的數字。")
