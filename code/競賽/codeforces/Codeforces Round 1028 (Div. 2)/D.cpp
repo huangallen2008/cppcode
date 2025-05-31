cbe336784e4413988e1c62c9b8ed6cd0f0ea163b
huangallen <huangallen2008@gmail.com>
2025-05-31 16:03:52 +0000

    71521ing

--- code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/D.cpp
+++ code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/D.cpp
@@ -64,7 +64,7 @@ struct SCC {
     Graph ng,ngb;
     Graph g,gb;
     int an=0;
-    void init(int n,Graph &_g,Vi vs) {
+    int init(int n,Graph &_g,Vi vs) {
         g=_g;
         sccid=vector<int>(n);
         dfn.clear();
@@ -95,7 +95,7 @@ struct SCC {
         val=Vi(sccc,inf);
         REP(i,SZ(vs)) {
             if(val[sccid[i]]==inf) val[sccid[i]]=vs[i];
-            else 
+            else return -1;
         }
         // dis=val;
         // REP(u,sccc) {
