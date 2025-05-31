76f75182bbf8bf7e4b82ad7ce42e0791ba32a461
huangallen <huangallen2008@gmail.com>
2025-05-31 14:38:36 +0000

    615131ing

--- code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/A.cpp
+++ code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/A.cpp
@@ -57,6 +57,9 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rd(int l,int r) {
     return uniform_int_distribution<int>(l,r)(rng);
 }
+void solve() {
+    int 
+}
 signed main() {
     IOS();
     int T;
