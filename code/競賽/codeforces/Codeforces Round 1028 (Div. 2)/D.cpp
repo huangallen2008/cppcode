19e8eb0f5d4e99cb347dd4f676abaf0c4be7c4b0
huangallen <huangallen2008@gmail.com>
2025-05-31 16:34:30 +0000

    71521ing

--- code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/D.cpp
+++ code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/D.cpp
@@ -170,10 +170,11 @@ void solve() {
             if(z==y) swap(x,y);
             //now z==x is true
             add(id[z],id[y]);
-            
+            if(x!=y) {
             int _=id[z];
             id[z]=node_id++;
             add(_,id[z]);
+            }
         }
     }
     // ope("ok")
