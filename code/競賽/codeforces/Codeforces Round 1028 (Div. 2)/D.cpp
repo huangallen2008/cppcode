dc5b462d492e49adcd880440c8dbf446da0eb603
huangallen <huangallen2008@gmail.com>
2025-05-31 16:07:13 +0000

    71521ing

--- code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/D.cpp
+++ code/競賽/codeforces/Codeforces Round 1028 (Div. 2)/D.cpp
@@ -142,11 +142,11 @@ void solve() {
     Vi vs=a;
     sort(ALL(vs));
     vs.erase(unique(ALL(vs)),vs.begin());
-    ope("ok")
     int N=n+q+vs.size();
     int node_id=vs.size();
     Vi id(n);
     REP(i,n) id[i]=lower_bound(ALL(vs),a[i])-vs.begin();
+    ope("ok")
     struct S {
         int x,y,z;
     };
